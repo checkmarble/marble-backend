92d87f2702aa630e00ad6d2a21d269145c8e806d
Thomas Lathuiliere <thomas@checkmarble.com>
2023-03-31 17:26:38 +0200

    refactor(migration): merge files

--- migrations/20230313103809_init_marble.sql
+++ migrations/20230313103809_init_marble.sql
@@ -61,6 +61,18 @@ VALUES (
     'token12345'
   );
 
+-- scenarios table
+CREATE TABLE scenarios(
+  id uuid DEFAULT uuid_generate_v4(),
+  org_id uuid NOT NULL,
+  name VARCHAR NOT NULL,
+  description VARCHAR NOT NULL,
+  trigger_object_type VARCHAR NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_scenarios_org FOREIGN KEY(org_id) REFERENCES organizations(id)
+);
+
 -- decisions
 -- Outcomes
 CREATE TYPE decision_outcome AS ENUM (
