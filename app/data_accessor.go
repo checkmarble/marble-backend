c6dbaff22109c47f305f5bce32e461076141a8f3
pdelange <pascal.delange@polytechnique.org>
2023-04-05 22:42:04 +0200

    pass app pointer in data accessor to access repository

--- app/data_accessor.go
+++ app/data_accessor.go
@@ -3,11 +3,12 @@ package app
 type DataAccessorImpl struct {
 	DataModel DataModel
 	Payload   Payload
+	app       *App
 }
 
 func (d *DataAccessorImpl) GetPayloadField(fieldName string) (interface{}, error) {
 	return nil, nil
 }
 func (d *DataAccessorImpl) GetDBField(path []string, fieldName string) (interface{}, error) {
-	return nil, nil
+	return d.app.repository.GetDBField(path, fieldName, d.DataModel, d.Payload)
 }
