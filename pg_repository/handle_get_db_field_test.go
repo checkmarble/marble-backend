58e23c8a607202b381ffc59d2849cd309409c6f0
Thomas Lathuiliere <thomas@checkmarble.com>
2023-04-13 15:25:57 +0200

    test(get_db_field): comment out TestMain

--- pg_repository/handle_get_db_field_test.go
+++ pg_repository/handle_get_db_field_test.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/Masterminds/squirrel"
 	"github.com/google/go-cmp/cmp"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 	"github.com/pashagolub/pgxmock/v2"
 
 	"marble/marble-backend/app"
@@ -42,140 +47,140 @@ const (
 
 var dbpool *pgxpool.Pool
 
-// func TestMain(m *testing.M) {
-// 	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
-// 	pool, err := dockertest.NewPool("")
-// 	if err != nil {
-// 		log.Fatalf("Could not construct pool: %s", err)
-// 	}
+func TestMain(m *testing.M) {
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
 
-// 	err = pool.Client.Ping()
-// 	if err != nil {
-// 		log.Fatalf("Could not connect to Docker: %s", err)
-// 	}
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
 
-// 	// pulls an image, creates a container based on it and runs it
-// 	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-// 		Repository: "postgres",
-// 		Tag:        "15",
-// 		Env: []string{
-// 			fmt.Sprintf("POSTGRES_PASSWORD=%s", testPassword),
-// 			fmt.Sprintf("POSTGRES_USER=%s", testUser),
-// 			fmt.Sprintf("POSTGRES_DB=%s", testDbName),
-// 			"listen_addresses = '*'",
-// 		},
-// 	}, func(config *docker.HostConfig) {
-// 		// set AutoRemove to true so that stopped container goes away by itself
-// 		config.AutoRemove = true
-// 		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-// 	})
-// 	if err != nil {
-// 		log.Fatalf("Could not start resource: %s", err)
-// 	}
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "15",
+		Env: []string{
+			fmt.Sprintf("POSTGRES_PASSWORD=%s", testPassword),
+			fmt.Sprintf("POSTGRES_USER=%s", testUser),
+			fmt.Sprintf("POSTGRES_DB=%s", testDbName),
+			"listen_addresses = '*'",
+		},
+	}, func(config *docker.HostConfig) {
+		// set AutoRemove to true so that stopped container goes away by itself
+		config.AutoRemove = true
+		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
+	})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
 
-// 	resource.Expire(testDbLifetime) // Tell docker to hard kill the container in testDbLifetime seconds
+	resource.Expire(testDbLifetime) // Tell docker to hard kill the container in testDbLifetime seconds
 
-// 	pool.MaxWait = testDbLifetime * time.Second
+	pool.MaxWait = testDbLifetime * time.Second
 
-// 	hostAndPort := resource.GetHostPort("5432/tcp") // docker container will bind to another port than 5432 if already taken
-// 	databaseUrl := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", testUser, testPassword, hostAndPort, testDbName)
-// 	dbpool, err = pgxpool.New(context.Background(), databaseUrl)
-// 	if err != nil {
-// 		log.Fatalf("Could not connect to database: %s", err)
-// 	}
+	hostAndPort := resource.GetHostPort("5432/tcp") // docker container will bind to another port than 5432 if already taken
+	databaseUrl := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", testUser, testPassword, hostAndPort, testDbName)
+	dbpool, err = pgxpool.New(context.Background(), databaseUrl)
+	if err != nil {
+		log.Fatalf("Could not connect to database: %s", err)
+	}
 
-// 	if err = pool.Retry(func() error {
-// 		log.Printf("DB connection pool created. Stats: %+v\n", dbpool.Stat())
-// 		err = dbpool.Ping(context.Background())
-// 		if err != nil {
-// 			log.Printf("Could not ping database: %s", err)
-// 			return err
-// 		}
-// 		return nil
-// 	}); err != nil {
-// 		log.Fatalf("Could not connect to db: %s", err)
-// 	}
+	if err = pool.Retry(func() error {
+		log.Printf("DB connection pool created. Stats: %+v\n", dbpool.Stat())
+		err = dbpool.Ping(context.Background())
+		if err != nil {
+			log.Printf("Could not ping database: %s", err)
+			return err
+		}
+		return nil
+	}); err != nil {
+		log.Fatalf("Could not connect to db: %s", err)
+	}
 
-// 	createTablesSql := `
-// 	CREATE SCHEMA testschema;
+	createTablesSql := `
+	CREATE SCHEMA testschema;
 
-// 	GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA testschema TO test_user;
+	GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA testschema TO test_user;
 
-// 	ALTER DATABASE test_db
-// 	SET search_path TO testschema,
-// 	public;
+	ALTER DATABASE test_db
+	SET search_path TO testschema,
+	public;
 
-// 	ALTER ROLE test_user
-// 	SET search_path TO testschema,
-// 	public;
+	ALTER ROLE test_user
+	SET search_path TO testschema,
+	public;
 
-// 	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-// 	CREATE TABLE transactions(
-// 		id uuid DEFAULT uuid_generate_v4(),
-// 		object_id VARCHAR NOT NULL,
-// 		updated_at TIMESTAMP NOT NULL,
-// 		account_id VARCHAR,
-// 		title VARCHAR,
-// 		value FLOAT,
-// 		isValidated BOOLEAN,
-// 		PRIMARY KEY(id)
-// 	  );
-// 	CREATE TABLE accounts(
-// 		ID UUID DEFAULT uuid_generate_v4(),
-// 		object_id VARCHAR NOT NULL,
-// 		updated_at TIMESTAMP NOT NULL,
-// 		status VARCHAR,
-// 		PRIMARY KEY(id)
-// 	);
+	CREATE TABLE transactions(
+		id uuid DEFAULT uuid_generate_v4(),
+		object_id VARCHAR NOT NULL,
+		updated_at TIMESTAMP NOT NULL,
+		account_id VARCHAR,
+		title VARCHAR,
+		value FLOAT,
+		isValidated BOOLEAN,
+		PRIMARY KEY(id)
+	  );
+	CREATE TABLE accounts(
+		ID UUID DEFAULT uuid_generate_v4(),
+		object_id VARCHAR NOT NULL,
+		updated_at TIMESTAMP NOT NULL,
+		status VARCHAR,
+		PRIMARY KEY(id)
+	);
 
-// 	INSERT INTO accounts (
-// 		object_id,
-// 		updated_at,
-// 		status
-// 	  )
-// 	VALUES(
-// 		'5c8a32f9-29c7-413c-91a8-1a363ef7e6b5',
-// 		'2021-01-01T00:00:00Z',
-// 		'VALIDATED'
-// 	  );
+	INSERT INTO accounts (
+		object_id,
+		updated_at,
+		status
+	  )
+	VALUES(
+		'5c8a32f9-29c7-413c-91a8-1a363ef7e6b5',
+		'2021-01-01T00:00:00Z',
+		'VALIDATED'
+	  );
 
-// 	INSERT INTO transactions (
-// 		object_id,
-// 		account_id,
-// 		updated_at,
-// 		value,
-// 		isValidated
-// 	  )
-// 	VALUES(
-// 		'9283b948-a140-4993-9c41-d5475fda5671',
-// 		'5c8a32f9-29c7-413c-91a8-1a363ef7e6b5',
-// 		'2021-01-01T00:00:00Z',
-// 		10,
-// 		true
-// 	  ),(
-// 		'6d3a330d-7204-4561-b523-9fa0d518d184',
-// 		'5c8a32f9-29c7-413c-91a8-1a363ef7e6b5',
-// 		'2021-01-01T00:00:00Z',
-// 		NULL,
-// 		false
-// 	  );
-// 	  `
+	INSERT INTO transactions (
+		object_id,
+		account_id,
+		updated_at,
+		value,
+		isValidated
+	  )
+	VALUES(
+		'9283b948-a140-4993-9c41-d5475fda5671',
+		'5c8a32f9-29c7-413c-91a8-1a363ef7e6b5',
+		'2021-01-01T00:00:00Z',
+		10,
+		true
+	  ),(
+		'6d3a330d-7204-4561-b523-9fa0d518d184',
+		'5c8a32f9-29c7-413c-91a8-1a363ef7e6b5',
+		'2021-01-01T00:00:00Z',
+		NULL,
+		false
+	  );
+	  `
 
-// 	if _, err := dbpool.Exec(context.Background(), createTablesSql); err != nil {
-// 		log.Fatalf("Could not create tables: %s", err)
-// 	}
+	if _, err := dbpool.Exec(context.Background(), createTablesSql); err != nil {
+		log.Fatalf("Could not create tables: %s", err)
+	}
 
-// 	//Run tests
-// 	code := m.Run()
+	//Run tests
+	code := m.Run()
 
-// 	// You can't defer this because os.Exit doesn't care for defer
-// 	if err := pool.Purge(resource); err != nil {
-// 		log.Fatalf("Could not purge resource: %s", err)
-// 	}
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
 
-// 	os.Exit(code)
-// }
+	os.Exit(code)
+}
 
 func TestReadFromDbWithDockerDb(t *testing.T) {
 	transactions := app.Table{
