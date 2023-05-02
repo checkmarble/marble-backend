cbd75ba285cf4244b90503046a6e146010dea4f3
pdelange <pascal.delange@polytechnique.org>
2023-05-02 14:50:43 +0200

    make logging work with gcloud

--- main.go
+++ main.go
@@ -38,6 +38,48 @@ func getEnvWithDefault(k string, def string) string {
 	return v
 }
 
+func loggerAttributeReplacer(groups []string, a slog.Attr) slog.Attr {
+	// Rename "msg" to "message" so that stackdriver logging can parse it as the main message
+	if a.Key == "msg" {
+		a.Key = "message"
+		return a
+	}
+
+	// Rename "level" to "severity" and convert the value so that stackdriver can properly parse it to a stackdriver severity
+	if a.Key == slog.LevelKey {
+		a.Key = "severity"
+
+		level := a.Value.Any().(slog.Level)
+
+		const (
+			LevelDebug   = slog.LevelDebug
+			LevelInfo    = slog.LevelInfo
+			LevelWarning = slog.LevelWarn
+			LevelError   = slog.LevelError
+		)
+
+		const (
+			gcpLevelDebug   = "DEBUG"
+			gcpLevelInfo    = "INFO"
+			gcpLevelWarning = "WARNING"
+			gcpLevelError   = "ERROR"
+		)
+
+		switch {
+		case level < LevelInfo:
+			a.Value = slog.StringValue(gcpLevelDebug)
+		case level < LevelWarning:
+			a.Value = slog.StringValue(gcpLevelInfo)
+		case level < LevelError:
+			a.Value = slog.StringValue(gcpLevelWarning)
+		default:
+			a.Value = slog.StringValue(gcpLevelError)
+		}
+	}
+
+	return a
+}
+
 func main() {
 	////////////////////////////////////////////////////////////
 	// Init
@@ -54,17 +96,16 @@ func main() {
 		pgPassword = mustGetenv("PG_PASSWORD")
 	)
 
-	////////////////////////////////////////////////////////////
-	// Setup dependencies
-	////////////////////////////////////////////////////////////
+	// ////////////////////////////////////////////////////////////
+	// // Setup dependencies
+	// ////////////////////////////////////////////////////////////
+
 	var logger *slog.Logger
 	if env == "DEV" {
-		textHandler := slog.NewTextHandler(os.Stderr)
+		textHandler := slog.HandlerOptions{ReplaceAttr: loggerAttributeReplacer}.NewTextHandler(os.Stderr)
 		logger = slog.New(textHandler)
-		jsonHandler := slog.NewJSONHandler(os.Stderr)
-		logger = slog.New(jsonHandler)
 	} else {
-		jsonHandler := slog.NewJSONHandler(os.Stderr)
+		jsonHandler := slog.HandlerOptions{ReplaceAttr: loggerAttributeReplacer}.NewJSONHandler(os.Stderr)
 		logger = slog.New(jsonHandler)
 	}
 
