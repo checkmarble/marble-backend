a62f1ab54056eafa03f6601369da93fdda4d0b6d
Vivien <vivien@checkmarble.com>
2023-05-09 14:32:31 +0200

    build(backoffice): add alias @ for ./src

--- marble-backoffice/vite.config.ts
+++ marble-backoffice/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
