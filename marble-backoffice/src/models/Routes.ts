099da7620861bd226fb0a4fcc4ddebb481cd3d18
Vivien <vivien@checkmarble.com>
2023-05-11 17:12:07 +0200

    feat(backoffice): Rework login navigation with a dedicated /Login page

--- marble-backoffice/src/models/Routes.ts
+++ marble-backoffice/src/models/Routes.ts
@@ -1,9 +1,20 @@
 export enum PathFragment {
   Home = "home",
+  Login = "login",
   Organizations = "organizations",
 }
 
 export const PageLink = {
   Home: `/${PathFragment.Home}`,
+  Login: `/${PathFragment.Login}`,
+  loginWithRedirect: (redirect?: string) => {
+    const queryParams = redirect ? `?${new URLSearchParams({ redirect })}` : "";
+    return `${PageLink.Login}${queryParams}`;
+  },
   Organization: `/${PathFragment.Organizations}`,
-} as const;
+};
+
+export function isRouteRequireAuthenticatedUser(path: string) {
+  // All pages but login
+  return !path.startsWith(PageLink.Login);
+}
