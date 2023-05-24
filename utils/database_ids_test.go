573b07673927d62e652483073a79f5c192040d1c
Vivien <vivien@checkmarble.com>
2023-05-24 14:35:16 +0200

    refactor: organization short id

--- utils/database_ids_test.go
+++ utils/database_ids_test.go
@@ -20,3 +20,12 @@ func TestNewPrimaryKey(t *testing.T) {
 	asserts.NotEqual(orgId, newId)
 
 }
+
+func TestNewUUIDStartWithOrgId(t *testing.T) {
+	newId := NewPrimaryKey("12345678-ffff-ffff-ffff-ffffffffffff")
+
+	// first 8 characters are the org id
+	assert.Equal(t, newId[:8], "12345678")
+	// the rest is diffenrent
+	assert.NotEqual(t, newId[8:], "-ffff-ffff-ffff-ffffffffffff")
+}
