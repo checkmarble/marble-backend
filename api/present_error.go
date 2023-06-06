dff5ab042fd0176e802c769fe3e6480cd371e2af
Vivien <vivien@checkmarble.com>
2023-06-06 10:55:43 +0200

    feat: log error when UnAuthorizedError, ForbiddenError or NotFoundError occurs

--- api/present_error.go
+++ api/present_error.go
@@ -13,16 +13,20 @@ func presentError(w http.ResponseWriter, r *http.Request, err error) bool {
 	if err == nil {
 		return false
 	}
+
 	if errors.Is(err, BadParameterError) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	} else if errors.Is(err, UnAuthorizedError) {
+		utils.LogRequestError(r, fmt.Sprintf("UnAuthorizedError: %v", err))
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 	} else if errors.Is(err, ForbiddenError) {
+		utils.LogRequestError(r, fmt.Sprintf("ForbiddenError: %v", err))
 		http.Error(w, err.Error(), http.StatusForbidden)
 	} else if errors.Is(err, NotFoundError) {
+		utils.LogRequestError(r, fmt.Sprintf("NotFoundError: %v", err))
 		http.Error(w, err.Error(), http.StatusNotFound)
 	} else {
-		utils.LogRequestError(r, fmt.Sprintf("Unexpected Error: %s", err))
+		utils.LogRequestError(r, fmt.Sprintf("Unexpected Error: %v", err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	return true
