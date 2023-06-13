b34b597e35dd319e3fb989df9c2e7bb662cb86fe
Vivien <vivien@checkmarble.com>
2023-06-13 11:10:40 +0200

    feat(backoffice): remove fake date created

--- api/cors.go
+++ api/cors.go
@@ -7,7 +7,7 @@ import (
 func corsOption(corsAllowLocalhost bool) cors.Options {
 	allowedOrigins := []string{"https://*"}
 	if corsAllowLocalhost {
-		allowedOrigins = append(allowedOrigins, "http://localhost:3000", "http://localhost:5173")
+		allowedOrigins = append(allowedOrigins, "http://localhost:3000", "http://localhost:3001", "http://localhost:5173")
 	}
 	return cors.Options{
 		AllowedOrigins:   allowedOrigins,
