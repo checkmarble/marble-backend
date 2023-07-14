84a157539f250f06088ff0171e4f7e0a6beee434
Alexandre Ablon <ablonalexandre@gmail.com>
2023-07-14 17:29:54 +0200

    Add payload evaluator Add payload and custom list to identifiers

--- usecases/ast_eval/evaluate_ast_test.go
+++ usecases/ast_eval/evaluate_ast_test.go
@@ -1,25 +1,14 @@
 package ast_eval
-
 import (
 	"testing"
-
 	"marble/marble-backend/models/ast"
-	"marble/marble-backend/usecases/ast_eval/evaluate"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestEval(t *testing.T) {
-
-	payload := map[string]any{
-		"balance": 96,
-	}
 	environment := NewAstEvaluationEnvironment()
-	environment.AddEvaluator(ast.FUNC_VARIABLE, evaluate.Variable{Variables: payload})
-
 	root := ast.NewAstCompareBalance()
 	result, err := EvaluateAst(environment, root)
 	assert.NoError(t, err)
-
 	assert.Equal(t, true, result)
 }
