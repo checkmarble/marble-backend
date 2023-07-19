ae61d0f681c1ad0022335b8e5be3c827bb0a183f
Vivien <vivien@checkmarble.com>
2023-07-19 16:40:21 +0200

    feat: better error message when no organization id provided by user or query param

--- usecases/scenario_publications_usecase.go
+++ usecases/scenario_publications_usecase.go
@@ -13,7 +13,7 @@ type ScenarioPublicationUsecase struct {
 	scenarioPublicationsRepository  repositories.ScenarioPublicationRepository
 	scenarioReadRepository          repositories.ScenarioReadRepository
 	scenarioIterationReadRepository repositories.ScenarioIterationReadRepository
-	OrganizationIdOfContext         string
+	OrganizationIdOfContext         func() (string, error)
 	enforceSecurity                 security.EnforceSecurityScenario
 	scenarioPublisher               scenarios.ScenarioPublisher
 }
@@ -32,19 +32,29 @@ func (usecase *ScenarioPublicationUsecase) GetScenarioPublication(scenarioPublic
 }
 
 func (usecase *ScenarioPublicationUsecase) ListScenarioPublications(filters models.ListScenarioPublicationsFilters) ([]models.ScenarioPublication, error) {
-	// Enforce permissions
-	if err := usecase.enforceSecurity.ListScenarios(usecase.OrganizationIdOfContext); err != nil {
+	organizationId, err := usecase.OrganizationIdOfContext()
+	if err != nil {
 		return nil, err
 	}
 
-	return usecase.scenarioPublicationsRepository.ListScenarioPublicationsOfOrganization(nil, usecase.OrganizationIdOfContext, filters)
+	// Enforce permissions
+	if err := usecase.enforceSecurity.ListScenarios(organizationId); err != nil {
+		return nil, err
+	}
+
+	return usecase.scenarioPublicationsRepository.ListScenarioPublicationsOfOrganization(nil, organizationId, filters)
 }
 
 func (usecase *ScenarioPublicationUsecase) ExecuteScenarioPublicationAction(ctx context.Context, input models.PublishScenarioIterationInput) ([]models.ScenarioPublication, error) {
+	organizationId, err := usecase.OrganizationIdOfContext()
+	if err != nil {
+		return nil, err
+	}
+
 	var scenarioPublications []models.ScenarioPublication
-	err := usecase.transactionFactory.Transaction(models.DATABASE_MARBLE_SCHEMA, func(tx repositories.Transaction) error {
+	err = usecase.transactionFactory.Transaction(models.DATABASE_MARBLE_SCHEMA, func(tx repositories.Transaction) error {
 		// FIXME Outside of transaction until the scenario iteration write repo is migrated
-		scenarioIteration, err := usecase.scenarioIterationReadRepository.GetScenarioIteration(ctx, usecase.OrganizationIdOfContext, input.ScenarioIterationId)
+		scenarioIteration, err := usecase.scenarioIterationReadRepository.GetScenarioIteration(ctx, organizationId, input.ScenarioIterationId)
 		if err != nil {
 			return err
 		}
--- usecases/usecases_with_creds.go
+++ usecases/usecases_with_creds.go
@@ -13,7 +13,7 @@ type UsecasesWithCreds struct {
 	Usecases
 	Credentials             models.Credentials
 	Logger                  *slog.Logger
-	OrganizationIdOfContext string
+	OrganizationIdOfContext func() (string, error)
 	Context                 context.Context
 }
 
