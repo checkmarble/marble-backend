64456bc9b3cd4bb9b04873fa60b890eff6422d02
Alexandre Ablon <ablonalexandre@gmail.com>
2023-07-21 13:58:13 +0200

    Rollback some access added to API_CLIENT role Fix the test in evaluate_ast_test.go

--- usecases/ast_eval/evaluate_ast_test.go
+++ usecases/ast_eval/evaluate_ast_test.go
@@ -2,12 +2,17 @@ package ast_eval
 
 import (
 	"testing"
+
+	"marble/marble-backend/models/ast"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEval(t *testing.T) {
-	// environment := NewAstEvaluationEnvironment()
-	// root := ast.NewAstCompareBalance()
-	// result, err := EvaluateAst(environment, root)
-	// assert.NoError(t, evaluation.EvaluationError)
-	// assert.Equal(t, true, evaluation.ReturnValue)
+	environment := NewAstEvaluationEnvironment()
+	root := ast.NewAstCompareBalance()
+	result, err := EvaluateAst(environment, root)
+	assert.NoError(t, err)
+
+	assert.Equal(t, true, result)
 }
