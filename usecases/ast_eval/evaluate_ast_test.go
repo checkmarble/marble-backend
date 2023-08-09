34d8e9ede10dd40eb390adb4d303d63f8d465461
Alexandre Ablon <ablonalexandre@gmail.com>
2023-08-09 18:27:46 +0200

    Fix Or operator that was non functional and add test for it

--- usecases/ast_eval/evaluate_ast_test.go
+++ usecases/ast_eval/evaluate_ast_test.go
@@ -14,3 +14,24 @@ func TestEval(t *testing.T) {
 	assert.NoError(t, evaluation.EvaluationError)
 	assert.Equal(t, true, evaluation.ReturnValue)
 }
+
+func TestEvalAndOrFunction(t *testing.T) {
+	environment := NewAstEvaluationEnvironment()
+
+	evaluation := EvaluateAst(environment, ast.NewAstAndTrue())
+	assert.NoError(t, evaluation.EvaluationError)
+	assert.Equal(t, true, evaluation.ReturnValue)
+
+	evaluation = EvaluateAst(environment, ast.NewAstAndFalse())
+	assert.NoError(t, evaluation.EvaluationError)
+	assert.Equal(t, false, evaluation.ReturnValue)
+
+	evaluation = EvaluateAst(environment, ast.NewAstOrTrue())
+	assert.NoError(t, evaluation.EvaluationError)
+	assert.Equal(t, true, evaluation.ReturnValue)
+
+	evaluation = EvaluateAst(environment, ast.NewAstOrFalse())
+	assert.NoError(t, evaluation.EvaluationError)
+	assert.Equal(t, false, evaluation.ReturnValue)
+
+}
