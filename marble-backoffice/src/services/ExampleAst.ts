a2a094c61d04d7fa422226a3c77d1926e5f28551
Vivien <vivien@checkmarble.com>
2023-09-04 15:57:31 +0200

    feat: rules of demo scenarios respects the structure Or -> Ands

--- marble-backoffice/src/services/ExampleAst.ts
+++ marble-backoffice/src/services/ExampleAst.ts
@@ -51,92 +51,111 @@ export const exampleRuleInList: ExampleRule = {
   }),
 };
 
+function adaptLitteralAnds(ands: unknown[]): AstNode {
+  return adaptLitteralAstNode({
+    name: "Or",
+    children: [
+      {
+        name: "And",
+        children: ands,
+      },
+    ],
+  });
+}
+
 export const demoRules: ExampleRule[] = [
   {
     name: "Medium amount",
     description: "Amount is between 10k and 100k, hence medium risk",
     scoreModifier: 10,
-    formula: adaptLitteralAstNode({
-      name: "And",
-      children: [
-        {
-          name: ">",
-          children: [
-            { name: "Payload", children: [{ constant: "amount" }] },
-            { constant: 10000 },
-          ],
-        },
-        {
-          name: "<",
-          children: [
-            { name: "Payload", children: [{ constant: "amount" }] },
-            { constant: 100000 },
-          ],
-        },
-      ],
-    }),
+    formula: adaptLitteralAnds([
+      {
+        name: ">",
+        children: [
+          { name: "Payload", children: [{ constant: "amount" }] },
+          { constant: 10000 },
+        ],
+      },
+      {
+        name: "<",
+        children: [
+          { name: "Payload", children: [{ constant: "amount" }] },
+          { constant: 100000 },
+        ],
+      },
+    ]),
   },
   {
     name: "High amount",
     description: "Amount is above 100k, hence high risk",
     scoreModifier: 20,
-    formula: adaptLitteralAstNode({
-      name: ">",
-      children: [
-        { name: "Payload", children: [{ constant: "amount" }] },
-        { constant: 100000 - 1 },
-      ],
-    }),
+    formula: adaptLitteralAnds([
+      {
+        name: ">",
+        children: [
+          { name: "Payload", children: [{ constant: "amount" }] },
+          { constant: 100000 - 1 },
+        ],
+      },
+    ]),
   },
   {
     name: "Medium risk country",
     description: "Country is in the list of medium risk (european) countries",
     scoreModifier: 10,
-    formula: adaptLitteralAstNode({
-      name: "IsInList",
-      children: [
-        { name: "Payload", children: [{ constant: "bic_country" }] },
-        { constant: ["HU", "IT", "PO", "IR"] },
-      ],
-    }),
+    formula: adaptLitteralAnds([
+      {
+        name: "IsInList",
+        children: [
+          { name: "Payload", children: [{ constant: "bic_country" }] },
+          { constant: ["HU", "IT", "PO", "IR"] },
+        ],
+      },
+    ]),
   },
 
   {
     name: "High risk country",
     description: "Country is in the list of high risk (european) countries",
     scoreModifier: 20,
-    formula: adaptLitteralAstNode({
-      name: "IsInList",
-      children: [
-        { name: "Payload", children: [{ constant: "bic_country" }] },
-        { constant: ["RO", "RU", "LT"] },
-      ],
-    }),
+    formula: adaptLitteralAnds([
+      {
+        name: "IsInList",
+        children: [
+          { name: "Payload", children: [{ constant: "bic_country" }] },
+          { constant: ["RO", "RU", "LT"] },
+        ],
+      },
+    ]),
   },
   {
     name: "Low risk country",
     description: "Country is domestic (France)",
     scoreModifier: -10,
-    formula: adaptLitteralAstNode({
-      name: "=",
-      children: [
-        { name: "Payload", children: [{ constant: "bic_country" }] },
-        { constant: "FR" },
-      ],
-    }),
+    formula: adaptLitteralAnds([
+      {
+        name: "=",
+        children: [
+          { name: "Payload", children: [{ constant: "bic_country" }] },
+          { constant: "FR" },
+        ],
+      },
+    ]),
   },
   {
     name: "Frozen account",
     description: "The account is frozen",
     scoreModifier: 100,
-    formula: adaptLitteralAstNode({
-      name: "DatabaseAccess",
-      named_children: {
-        tableName: { constant: "transactions" },
-        fieldName: { constant: "is_frozen" },
-        path: { constant: ["account"] },
+    formula: adaptLitteralAnds([
+      {
+        name: "DatabaseAccess",
+        named_children: {
+          tableName: { constant: "transactions" },
+          fieldName: { constant: "is_frozen" },
+          path: { constant: ["account"] },
+        },
       },
-    }),
+    ]),
   },
 ];
 
