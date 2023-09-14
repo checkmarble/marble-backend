b3256154f881cafa7cd5d75e722c10e1fc274e4f
Alexandre Ablon <ablonalexandre@gmail.com>
2023-09-14 16:34:13 +0200

    ingested_data_read_repository test move dummy data to utils to use on other place than evaluation folder

---
+++ utils/dummy_datamodel.go
@@ -0,0 +1,93 @@
+package utils
+
+import "github.com/checkmarble/marble-backend/models"
+
+const DummyTableNameFirst models.TableName = "first"
+const DummyTableNameSecond models.TableName = "second"
+const DummyTableNameThird models.TableName = "third"
+
+const DummyFieldNameId models.FieldName = "id"
+const DummyFieldNameForBool models.FieldName = "bool_var"
+const DummyFieldNameForInt models.FieldName = "int_var"
+const DummyFieldNameForFloat models.FieldName = "float_var"
+const DummyFieldNameForTimestamp models.FieldName = "time_var"
+
+func GetDummyDataModel() models.DataModel {
+	var dummyFieldBool models.Field = models.Field{
+		DataType: 0,
+		Nullable: false,
+	}
+	var dummyFieldInt models.Field = models.Field{
+		DataType: 1,
+		Nullable: false,
+	}
+	var dummyFieldFloat models.Field = models.Field{
+		DataType: 2,
+		Nullable: false,
+	}
+
+	var dummyFieldString models.Field = models.Field{
+		DataType: 3,
+		Nullable: false,
+	}
+
+	var dummyFieldTimestamp models.Field = models.Field{
+		DataType: 4,
+		Nullable: false,
+	}
+
+	var dummyFirstLinkToSingle map[models.LinkName]models.LinkToSingle = map[models.LinkName]models.LinkToSingle{
+		models.LinkName(DummyTableNameSecond): {
+			LinkedTableName: DummyTableNameSecond,
+			ParentFieldName: DummyFieldNameId,
+			ChildFieldName:  DummyFieldNameId,
+		},
+	}
+
+	var dummySecondLinkToSingle map[models.LinkName]models.LinkToSingle = map[models.LinkName]models.LinkToSingle{
+		models.LinkName(DummyTableNameThird): {
+			LinkedTableName: DummyTableNameThird,
+			ParentFieldName: DummyFieldNameId,
+			ChildFieldName:  DummyFieldNameId,
+		},
+	}
+
+	var dummyFieldsIdOnly map[models.FieldName]models.Field = map[models.FieldName]models.Field{
+		DummyFieldNameId: dummyFieldString,
+	}
+
+	var dummyAllFields map[models.FieldName]models.Field = map[models.FieldName]models.Field{
+		DummyFieldNameId:           dummyFieldString,
+		DummyFieldNameForInt:       dummyFieldInt,
+		DummyFieldNameForFloat:     dummyFieldFloat,
+		DummyFieldNameForBool:      dummyFieldBool,
+		DummyFieldNameForTimestamp: dummyFieldTimestamp,
+	}
+
+	var dummyDataModelFirstTable models.Table = models.Table{
+		Name:          DummyTableNameFirst,
+		Fields:        dummyFieldsIdOnly,
+		LinksToSingle: dummyFirstLinkToSingle,
+	}
+
+	var dummyDataModelSecondTable models.Table = models.Table{
+		Name:          DummyTableNameSecond,
+		Fields:        dummyAllFields,
+		LinksToSingle: dummySecondLinkToSingle,
+	}
+
+	var dummyDataModelThirdTable models.Table = models.Table{
+		Name:   DummyTableNameThird,
+		Fields: dummyAllFields,
+	}
+
+	return models.DataModel{
+		Version: "1",
+		Status:  0,
+		Tables: map[models.TableName]models.Table{
+			DummyTableNameFirst:  dummyDataModelFirstTable,
+			DummyTableNameSecond: dummyDataModelSecondTable,
+			DummyTableNameThird:  dummyDataModelThirdTable,
+		},
+	}
+}
