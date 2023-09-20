78d69aa2290288eb4327361abfdede9b518c6022
Vivien <vivien@checkmarble.com>
2023-09-20 09:00:00 +0200

    refactor: remove redundancy in SqlToModel

--- repositories/sql_to_model.go
+++ repositories/sql_to_model.go
@@ -67,21 +67,7 @@ func ForEachRow(transaction TransactionPostgres, query squirrel.Sqlizer, fn func
 // executes the sql query with the given transaction and returns a list of models using the provided adapter
 func SqlToListOfModels[DBModel, Model any](transaction TransactionPostgres, query squirrel.Sqlizer, adapter func(dbModel DBModel) Model) ([]Model, error) {
 
-	sql, args, err := query.ToSql()
-	if err != nil {
-		return nil, errors.Wrap(err, "can't build sql query")
-	}
-
-	rows, err := transaction.exec.Query(transaction.ctx, sql, args...)
-	if err != nil {
-		return nil, errors.Wrap(err, "error executing sql query")
-	}
-	return pgx.CollectRows(rows, func(row pgx.CollectableRow) (Model, error) {
-		dbModel, err := pgx.RowToStructByName[DBModel](row)
-		if err != nil {
-			var zeroModel Model
-			return zeroModel, errors.Wrap(err, fmt.Sprintf("error scanning row to struct %T", dbModel))
-		}
+	return SqlToListOfModelsAdapterWithErr(transaction, query, func(dbModel DBModel) (Model, error) {
 		return adapter(dbModel), nil
 	})
 }
@@ -90,35 +76,18 @@ func SqlToListOfModels[DBModel, Model any](transaction TransactionPostgres, quer
 // If no result is returned by the query, returns nil
 func SqlToOptionalModel[DBModel, Model any](transaction TransactionPostgres, s squirrel.Sqlizer, adapter func(dbModel DBModel) Model) (*Model, error) {
 
-	modelslist, err := SqlToListOfModels(transaction, s, adapter)
-	if err != nil {
-		return nil, err
-	}
-
-	numberOfTesults := len(modelslist)
-	if numberOfTesults == 0 {
-		return nil, nil
-	}
-	var model Model = modelslist[0]
-	if numberOfTesults > 1 {
-		return nil, errors.New(fmt.Sprintf("except 1 or 0 %v, %d rows in the result", reflect.TypeOf(model), numberOfTesults))
-	}
-	return &model, nil
+	return SqlToOptionalModelAdapterWithErr(transaction, s, func(dbModel DBModel) (Model, error) {
+		return adapter(dbModel), nil
+	})
 }
 
 // executes the sql query with the given transaction and returns a models using the provided adapter
 // if no result is returned by the query, returns a NotFoundError
 func SqlToModel[DBModel, Model any](transaction TransactionPostgres, s squirrel.Sqlizer, adapter func(dbModel DBModel) Model) (Model, error) {
 
-	model, err := SqlToOptionalModel(transaction, s, adapter)
-	var zeroModel Model
-	if err != nil {
-		return zeroModel, err
-	}
-	if model == nil {
-		return zeroModel, errors.Wrap(models.NotFoundError, fmt.Sprintf("found no object of type %T", zeroModel))
-	}
-	return *model, nil
+	return SqlToModelAdapterWithErr(transaction, s, func(dbModel DBModel) (Model, error) {
+		return adapter(dbModel), nil
+	})
 }
 
 ////////////////
