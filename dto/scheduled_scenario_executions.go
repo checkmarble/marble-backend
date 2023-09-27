6d1a4bd4069575dc922e2ad95463bffb24a8568c
Vivien <vivien@checkmarble.com>
2023-09-27 11:15:49 +0200

    feat: add scenario info to /scheduled-executions endpoint

--- dto/scheduled_scenario_executions.go
+++ dto/scheduled_scenario_executions.go
@@ -7,22 +7,28 @@ import (
 )
 
 type APIScheduledExecution struct {
-	Id                       string     `json:"id"`
-	ScenarioIterationId      string     `json:"scenario_iteration_id"`
-	Status                   string     `json:"status"`
-	StartedAt                time.Time  `json:"started_at"`
-	FinishedAt               *time.Time `json:"finished_at"`
-	NumberOfCreatedDecisions int        `json:"number_of_created_decisions"`
+	Id                        string     `json:"id"`
+	ScenarioIterationId       string     `json:"scenario_iteration_id"`
+	Status                    string     `json:"status"`
+	StartedAt                 time.Time  `json:"started_at"`
+	FinishedAt                *time.Time `json:"finished_at"`
+	NumberOfCreatedDecisions  int        `json:"number_of_created_decisions"`
+	ScenarioId                string     `json:"scenario_id"`
+	ScenarioName              string     `json:"scenario_name"`
+	ScenarioTriggerObjectType string     `json:"scenario_trigger_object_type"`
 }
 
 func AdaptScheduledExecutionDto(ExecutionBatch models.ScheduledExecution) APIScheduledExecution {
 	return APIScheduledExecution{
-		Id:                       ExecutionBatch.Id,
-		ScenarioIterationId:      ExecutionBatch.ScenarioIterationId,
-		Status:                   ExecutionBatch.Status.String(),
-		StartedAt:                ExecutionBatch.StartedAt,
-		FinishedAt:               ExecutionBatch.FinishedAt,
-		NumberOfCreatedDecisions: ExecutionBatch.NumberOfCreatedDecisions,
+		Id:                        ExecutionBatch.Id,
+		ScenarioIterationId:       ExecutionBatch.ScenarioIterationId,
+		Status:                    ExecutionBatch.Status.String(),
+		StartedAt:                 ExecutionBatch.StartedAt,
+		FinishedAt:                ExecutionBatch.FinishedAt,
+		NumberOfCreatedDecisions:  ExecutionBatch.NumberOfCreatedDecisions,
+		ScenarioId:                ExecutionBatch.ScenarioId,
+		ScenarioName:              ExecutionBatch.Scenario.Name,
+		ScenarioTriggerObjectType: ExecutionBatch.Scenario.TriggerObjectType,
 	}
 }
 
