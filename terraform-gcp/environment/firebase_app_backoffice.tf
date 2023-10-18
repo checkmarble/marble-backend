525994c8217358d87da1e77b059b360245ab9114
Vivien <vivien@checkmarble.com>
2023-10-18 17:55:20 +0200

    feat: terraform import production

--- terraform-gcp/environment/firebase_app_backoffice.tf
+++ terraform-gcp/environment/firebase_app_backoffice.tf
@@ -9,6 +9,6 @@ data "google_firebase_web_app_config" "backoffice" {
 }
 
 resource "google_firebase_hosting_site" "backoffice" {
-  app_id  = local.environment.firebase.backend_app_id
+#   app_id  = local.environment.firebase.backend_app_id
   site_id = local.environment.backoffice.firebase_site_id
 }
