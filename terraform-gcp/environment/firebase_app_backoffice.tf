f409642efd3e4d0a6c1acfcbff70d07ef500736f
Vivien <vivien@checkmarble.com>
2023-10-20 11:01:03 +0200

    feat: terraform custom domain for authentication

--- terraform-gcp/environment/firebase_app_backoffice.tf
+++ terraform-gcp/environment/firebase_app_backoffice.tf
@@ -9,6 +9,14 @@ data "google_firebase_web_app_config" "backoffice" {
 }
 
 resource "google_firebase_hosting_site" "backoffice" {
-#   app_id  = local.environment.firebase.backend_app_id
   site_id = local.environment.backoffice.firebase_site_id
 }
+
+resource "google_firebase_hosting_custom_domain" "backoffice" {
+  site_id       = local.environment.backoffice.firebase_site_id
+  custom_domain = local.environment.backoffice.domain
+  # cert_preference = "GROUPED"
+  # redirect_target = "app.domain.com"
+  wait_dns_verification = false
+}
+
--- terraform-gcp/environment/firebase_app_frontend.tf
+++ terraform-gcp/environment/firebase_app_frontend.tf
@@ -7,3 +7,10 @@ resource "google_firebase_web_app" "frontend" {
 data "google_firebase_web_app_config" "frontend" {
   web_app_id = google_firebase_web_app.frontend.app_id
 }
+
+# resource "google_firebase_hosting_custom_domain" "frontend" {
+#   site_id = local.environment.frontend.firebase_site_id
+#   custom_domain = local.environment.frontend.domain
+#   # cert_preference = "GROUPED"
+#   wait_dns_verification = false
+# }
