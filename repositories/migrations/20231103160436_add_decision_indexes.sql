4e81f79e6f364844b453977fbea8e506ccf386a5
Zoé Cadé <zoe.cade@gmail.com>
2023-11-06 10:50:36 +0100

    Better down migration

--- repositories/migrations/20231103160436_add_decision_indexes.sql
+++ repositories/migrations/20231103160436_add_decision_indexes.sql
@@ -11,6 +11,10 @@ CREATE INDEX trigger_object_type_idx ON decisions(org_id, trigger_object_type, c
 
 -- +goose Down
 -- +goose StatementBegin
+CREATE INDEX scenario_id_idx ON decisions(scenario_id);
+CREATE INDEX created_at_idx ON decisions(created_at);
+CREATE INDEX outcome_idx ON decisions(outcome);
+CREATE INDEX trigger_object_type_idx ON decisions(trigger_object_type);
 DROP INDEX scenario_id_idx;
 DROP INDEX outcome_idx;
 DROP INDEX trigger_object_type_idx;
