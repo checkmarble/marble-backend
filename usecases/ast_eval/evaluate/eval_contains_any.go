24aff81963d8d71b56fd949f8fbbf5cd30feba8c
Zoé Cadé <zoe.cade@gmail.com>
2023-11-08 18:36:10 +0100

    feat: add operators ContainsAny, ContainsNone & StringNotContain

---
+++ usecases/ast_eval/evaluate/eval_contains_any.go
@@ -0,0 +1,50 @@
+package evaluate
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/checkmarble/marble-backend/models/ast"
+)
+
+type ContainsAny struct {
+	Function ast.Function
+}
+
+func NewContainsAny(f ast.Function) ContainsAny {
+	return ContainsAny{
+		Function: f,
+	}
+}
+
+func (f ContainsAny) Evaluate(arguments ast.Arguments) (any, []error) {
+	leftAny, rightAny, err := leftAndRight(arguments.Args)
+	if err != nil {
+		return MakeEvaluateError(err)
+	}
+	left, err := adaptArgumentToString(leftAny)
+	if err != nil {
+		return MakeEvaluateError(err)
+	}
+
+	right, err := adaptArgumentToListOfStrings(rightAny)
+	if err != nil {
+		return MakeEvaluateError(err)
+	}
+
+	var containsElement bool
+	for _, r := range right {
+		if strings.Contains(strings.ToLower(left), strings.ToLower(r)) {
+			containsElement = true
+			break
+		}
+	}
+
+	if f.Function == ast.FUNC_CONTAINS_ANY {
+		return containsElement, nil
+	} else if f.Function == ast.FUNC_CONTAINS_NONE {
+		return !containsElement, nil
+	} else {
+		return MakeEvaluateError(fmt.Errorf("ContainsAny does not support %s function", f.Function.DebugString()))
+	}
+}
