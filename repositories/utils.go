6d7a165c788f07169f1c0bac2710125a04d3d11b
Pascal Delange <pascal@checkmarble.com>
2023-12-18 10:53:23 +0100

    remove unused helpers

--- repositories/utils.go
+++ repositories/utils.go
@@ -11,15 +11,3 @@ func columnsNames(tablename string, fields []string) []string {
 		return fmt.Sprintf("%s.%s", tablename, f)
 	})
 }
-
-func columnsNamesWithAlias(tablename string, fields []string) []string {
-	return utils.Map(fields, func(f string) string {
-		return fmt.Sprintf("%s.%s AS %s_%s", tablename, f, tablename, f)
-	})
-}
-
-func columnsAlias(tablename string, fields []string) []string {
-	return utils.Map(fields, func(f string) string {
-		return fmt.Sprintf("%s_%s", tablename, f)
-	})
-}
