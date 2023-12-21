06af534e39133d466bfcea4667fc78041d6d90ae
Zoé Cadé <zoe.cade@gmail.com>
2023-12-21 16:12:25 +0100

    feat: speed up ingestion - second iteration

--- repositories/ingestion_repository.go
+++ repositories/ingestion_repository.go
@@ -1,14 +1,12 @@
 package repositories
 
 import (
-	"errors"
 	"fmt"
 	"log/slog"
 	"time"
 
 	"github.com/Masterminds/squirrel"
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
 
 	"github.com/checkmarble/marble-backend/models"
 )
@@ -23,29 +21,24 @@ type IngestionRepositoryImpl struct {
 func (repo *IngestionRepositoryImpl) IngestObjects(transaction Transaction, payloads []models.PayloadReader, table models.Table, logger *slog.Logger) (err error) {
 	tx := adaptClientDatabaseTransaction(transaction)
 
-	// Iterate over payloads to keep only most recent records
-	recentObjectIds, recentPayloads := repo.filterMostRecentPayloads(payloads)
+	recentObjectIds, recentPayloads := repo.mostRecentPayloadsByObjectId(payloads)
 
-	// Load all previously ingested objects
 	previouslyIngestedObjects, err := repo.loadPreviouslyIngestedObjects(tx, recentObjectIds, table.Name)
 	if err != nil {
 		return err
 	}
 
-	// Iterate over payloads and compare with previously ingested objects
 	payloadsToInsert, obsoleteIngestedObjectIds := repo.comparePayloadsToIngestedObjects(recentPayloads, previouslyIngestedObjects)
 
-	// Batch update valid_until for obsolete objects
 	if err := repo.batchUpdateValidUntilOnObsoleteObjects(tx, table.Name, obsoleteIngestedObjectIds); err != nil {
 		return err
 	}
 
-	// Batch insert the new payloads
-	if err := repo.batchInsertPayloads(tx, payloadsToInsert, table); err != nil {
+	if err := repo.batchInsertPayloadsAndEnumValues(tx, payloadsToInsert, table); err != nil {
 		return err
 	}
 
-	logger.Debug("Inserted objects in db", slog.String("type", tableNameWithSchema(tx, table.Name)), slog.Int("nb_objects", len(payloadsToInsert)))
+	logger.Info("Inserted objects in db", slog.String("type", tableNameWithSchema(tx, table.Name)), slog.Int("nb_objects", len(payloadsToInsert)))
 
 	return nil
 }
@@ -59,7 +52,7 @@ func objectIdAndUpdatedAtFromPayload(payload models.PayloadReader) (string, time
 	return objectId, updatedAt
 }
 
-func (repo *IngestionRepositoryImpl) filterMostRecentPayloads(payloads []models.PayloadReader) ([]string, []models.PayloadReader) {
+func (repo *IngestionRepositoryImpl) mostRecentPayloadsByObjectId(payloads []models.PayloadReader) ([]string, []models.PayloadReader) {
 	recentMap := make(map[string]models.PayloadReader)
 	for _, payload := range payloads {
 		objectId, updatedAt := objectIdAndUpdatedAtFromPayload(payload)
@@ -102,12 +95,7 @@ func (repo *IngestionRepositoryImpl) loadPreviouslyIngestedObjects(tx Transactio
 		Where(squirrel.Eq{"object_id": objectIds}).
 		Where(squirrel.Eq{"valid_until": "Infinity"})
 
-	previouslyIngestedObjects, err := SqlToListOfModels(tx, query, func(db DBObject) (IngestedObject, error) { return IngestedObject(db), nil })
-	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
-		return nil, err
-	}
-
-	return previouslyIngestedObjects, nil
+	return SqlToListOfModels(tx, query, func(db DBObject) (IngestedObject, error) { return IngestedObject(db), nil })
 }
 
 func (repo *IngestionRepositoryImpl) comparePayloadsToIngestedObjects(payloads []models.PayloadReader, previouslyIngestedObjects []IngestedObject) ([]models.PayloadReader, []string) {
@@ -148,62 +136,16 @@ func (repo *IngestionRepositoryImpl) batchUpdateValidUntilOnObsoleteObjects(tx T
 	return err
 }
 
-// TODO refacto the insertion of enum values into one db commit
-func generateInsertValues(tx TransactionPostgres, table models.Table, payload models.PayloadReader, columnNames []string) ([]interface{}, error) {
-	insertValues := make([]interface{}, len(columnNames))
-	i := 0
-	for _, columnName := range columnNames {
-		fieldName := models.FieldName(columnName)
-		insertValues[i], _ = payload.ReadFieldFromPayload(fieldName)
-
-		// Check for enum values
-		dataType := table.Fields[fieldName].DataType
-		if table.Fields[fieldName].IsEnum && insertValues[i] != nil && (dataType == models.String || dataType == models.Float) {
-			err := addEnumValue(tx, table.Fields[fieldName].ID, dataType, insertValues[i])
-			if err != nil {
-				return nil, fmt.Errorf("addEnumValue error: %w", err)
-			}
-		}
-		i++
-	}
-	return insertValues, nil
-}
-
-func addEnumValue(tx TransactionPostgres, fieldID string, dataType models.DataType, value interface{}) error {
-	dataTypeEnumColumnMap := map[models.DataType]string{
-		models.String: "text_value",
-		models.Float:  "float_value",
-	}
-	dataTypeEnumColumnConstraintMap := map[models.DataType]string{
-		models.String: "unique_data_model_enum_text_values_field_id_value",
-		models.Float:  "unique_data_model_enum_float_values_field_id_value",
-	}
-	column, ok1 := dataTypeEnumColumnMap[dataType]
-	constraint, ok2 := dataTypeEnumColumnConstraintMap[dataType]
-	if !ok1 || !ok2 {
-		return fmt.Errorf("addEnumValue error: data type %s can't handle enum values", dataType.String())
-	}
-
-	query := fmt.Sprintf(`
-		INSERT INTO data_model_enum_values (field_id, %s)
-		VALUES ($1, $2)
-		ON CONFLICT ON CONSTRAINT %s
-		DO UPDATE SET last_seen = NOW()
-	`, column, constraint)
-
-	_, err := tx.exec.Exec(tx.ctx, query, fieldID, value)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (repo *IngestionRepositoryImpl) batchInsertPayloads(tx TransactionPostgres, payloads []models.PayloadReader, table models.Table) error {
+func (repo *IngestionRepositoryImpl) batchInsertPayloadsAndEnumValues(tx TransactionPostgres, payloads []models.PayloadReader, table models.Table) error {
 	columnNames := models.ColumnNames(table)
 	query := NewQueryBuilder().Insert(tableNameWithSchema(tx, table.Name))
 
+	enumValues := repo.buildEnumValuesWithEnumFields(table)
+
 	for _, payload := range payloads {
-		insertValues, err := generateInsertValues(tx, table, payload, columnNames)
+		repo.collectEnumValues(payload, enumValues)
+
+		insertValues, err := repo.generateInsertValues(payload, columnNames)
 		if err != nil {
 			return fmt.Errorf("generateInsertValues error: %w", err)
 		}
@@ -211,12 +153,99 @@ func (repo *IngestionRepositoryImpl) batchInsertPayloads(tx TransactionPostgres,
 		query = query.Values(insertValues...)
 	}
 
+	err := repo.batchInsertEnumValues(tx, enumValues, table)
+	if err != nil {
+		return fmt.Errorf("batchInsertEnumValues error: %w", err)
+	}
+
 	columnNames = append(columnNames, "id")
 	query = query.Columns(columnNames...)
 
-	_, err := tx.ExecBuilder(query)
-	if err != nil {
-		return err
+	_, err = tx.ExecBuilder(query)
+
+	return err
+}
+
+type EnumValues map[string]map[any]bool
+
+func (repo *IngestionRepositoryImpl) buildEnumValuesWithEnumFields(table models.Table) EnumValues {
+	enumValues := make(EnumValues)
+	for fieldName := range table.Fields {
+		dataType := table.Fields[fieldName].DataType
+		if table.Fields[fieldName].IsEnum && (dataType == models.String || dataType == models.Float) {
+			enumValues[string(fieldName)] = make(map[any]bool)
+		}
+	}
+	return enumValues
+}
+
+func (repo *IngestionRepositoryImpl) generateInsertValues(payload models.PayloadReader, columnNames []string) ([]interface{}, error) {
+	insertValues := make([]interface{}, len(columnNames))
+	i := 0
+	for _, columnName := range columnNames {
+		fieldName := models.FieldName(columnName)
+		insertValues[i], _ = payload.ReadFieldFromPayload(fieldName)
+		i++
+	}
+	return insertValues, nil
+}
+
+func (repo *IngestionRepositoryImpl) collectEnumValues(payload models.PayloadReader, enumValues EnumValues) {
+	for fieldName := range enumValues {
+		value, _ := payload.ReadFieldFromPayload(models.FieldName(fieldName))
+
+		if value != nil && value != "" {
+			enumValues[fieldName][value] = true
+		}
+	}
+}
+
+// This has to be done in 2 queries because there cannot be multiple ON CONFLICT clauses per query
+func (repo *IngestionRepositoryImpl) batchInsertEnumValues(tx TransactionPostgres, enumValues EnumValues, table models.Table) error {
+	textQuery := NewQueryBuilder().
+		Insert("data_model_enum_values").
+		Columns("field_id", "text_value").
+		Suffix("ON CONFLICT ON CONSTRAINT unique_data_model_enum_text_values_field_id_value DO UPDATE SET last_seen = NOW()")
+
+	floatQuery := NewQueryBuilder().
+		Insert("data_model_enum_values").
+		Columns("field_id", "float_value").
+		Suffix("ON CONFLICT ON CONSTRAINT unique_data_model_enum_float_values_field_id_value DO UPDATE SET last_seen = NOW()")
+
+	// Hack to avoid empty query, which would cause an executiong error
+	var shouldInsertTextValues bool
+	var shouldInsertFloatValues bool
+
+	for fieldName, values := range enumValues {
+		fieldId := table.Fields[models.FieldName(fieldName)].ID
+		dataType := table.Fields[models.FieldName(fieldName)].DataType
+
+		for value := range values {
+			if dataType == models.String {
+				textQuery = textQuery.Values(fieldId, value)
+				if !shouldInsertTextValues {
+					shouldInsertTextValues = true
+				}
+			} else if dataType == models.Float {
+				floatQuery = floatQuery.Values(fieldId, value)
+				if !shouldInsertFloatValues {
+					shouldInsertFloatValues = true
+				}
+			}
+		}
+	}
+
+	if shouldInsertTextValues {
+		_, err := tx.ExecBuilder(textQuery)
+		if err != nil {
+			return err
+		}
+	}
+	if shouldInsertFloatValues {
+		_, err := tx.ExecBuilder(floatQuery)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
