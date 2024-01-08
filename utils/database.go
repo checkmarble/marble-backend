70cafba81efab878040d526b8551b12400916ac5
Pascal Delange <pascal@checkmarble.com>
2024-01-08 18:19:40 +0100

    rename dev env for iso front

--- utils/database.go
+++ utils/database.go
@@ -16,7 +16,7 @@ func (config PGConfig) GetConnectionString(env string) string {
 		return config.ConnectionString
 	}
 	connectionString := fmt.Sprintf("host=%s user=%s password=%s database=%s sslmode=disable", config.Hostname, config.User, config.Password, config.Database)
-	if env == "DEV" {
+	if env == "development" {
 		// Cloud Run connects to the DB through a proxy and a unix socket, so we don't need need to specify the port
 		// but we do when running locally
 		connectionString = fmt.Sprintf("%s port=%s", connectionString, config.Port)
