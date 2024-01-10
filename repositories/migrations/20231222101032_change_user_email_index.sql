bae50f89d2000128dc2855ea5bf0a23e01775543
Pascal Delange <pascal@checkmarble.com>
2024-01-10 22:24:02 +0100

    enable multiple deleted users with the same email

---
+++ repositories/migrations/20231222101032_change_user_email_index.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- +goose StatementBegin
+DROP INDEX users_email_idx;
+CREATE UNIQUE INDEX users_email_idx ON users (email) WHERE deleted_at IS NOT NULL;
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP INDEX users_email_idx;
+CREATE UNIQUE INDEX users_email_idx ON users (email);
+-- +goose StatementEnd
