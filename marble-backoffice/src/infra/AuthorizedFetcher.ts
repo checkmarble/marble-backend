526918b5f8cbbc5c3ecaf3d48fad8d2a340fb480
Pascal Delange <pascal@checkmarble.com>
2024-01-22 15:45:32 +0100

    remove BO from backend repo

--- marble-backoffice/src/infra/AuthorizedFetcher.ts
+++
@@ -1,5 +0,0 @@
-
-export interface AuthorizedFetcher {
-  authorizedJson(request: Request): Promise<unknown>;
-  authorizedBlob(request: Request): Promise<Blob>;
-}
--- marble-backoffice/src/models/SignInError.ts
+++
@@ -1,8 +0,0 @@
-export class SignInError extends Error {
-  from: Error;
-
-  constructor(message: string, from: Error) {
-    super(message, { cause: from });
-    this.from = from;
-  }
-}
