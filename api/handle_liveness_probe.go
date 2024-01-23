3a654a534c41597675829e7af8c733a6c1ab94da
Pascal Delange <pascal@checkmarble.com>
2024-01-23 14:00:24 +0100

    test: liveness probe log

--- api/handle_liveness_probe.go
+++ api/handle_liveness_probe.go
@@ -1,12 +1,14 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func HandleLivenessProbe(c *gin.Context) {
+	fmt.Println("Liveness probe ok")
 	c.JSON(http.StatusOK, gin.H{
 		"mood": "Feu flammes !",
 	})
