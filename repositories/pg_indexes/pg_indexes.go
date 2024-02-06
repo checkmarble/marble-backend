c440765506b0f0e7708a8459bbffee92a64a1902
Pascal Delange <pascal@checkmarble.com>
2024-02-06 22:42:27 +0100

    repository to read existing indexes and their status

---
+++ repositories/pg_indexes/pg_indexes.go
@@ -0,0 +1,56 @@
+package pg_indexes
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/utils"
+)
+
+var columnNamesRegex = regexp.MustCompile(`(\([a-zA-Z0-9,_\ ]+\))`)
+
+type PGIndex struct {
+	CreationInProgress bool
+	Definition         string
+	IsValid            bool
+	Name               string
+	RelationId         int
+}
+
+func parseCreateIndexStatement(sql string) models.ConcreteIndex {
+	// This function EXPECTS the input to be a correctly formatted CREATE INDEX statement
+	// coming straight from the DB. It will not work correctly if it is not one.
+	// However, we do not try to parse it all generality because that is beyond the scope of this function.
+	// Moreover, column names are enforced to be alphanumeric + "_" only at data model edition time, so
+	// we do not expect column names to be double quoted, to include special characters other than "_", etc.
+	// We also expect concrete column names only indexed (not expressions, etc.)
+	matches := columnNamesRegex.FindAllString(sql, -1)
+
+	// len(matches) is expected to be > 0 because the sql statement is expected to be a proper CREATE INDEX statement
+	indexedColumnsRaw := strings.Split(strings.Trim(matches[0], "() "), ",")
+	indexedColumnNames := utils.Map(indexedColumnsRaw, func(s string) models.FieldName {
+		// We discard the order of the index (ASC/DESC) because this is not relevant or modelized (yet) for our purposes
+		parts := strings.Split(strings.Trim(s, " "), " ")
+		// the first part of the string must be the column name
+		return models.FieldName(parts[0])
+	})
+
+	var includedColumnNames []models.FieldName
+	// if there is a second match, it is the list of included columns (optional)
+	if len(matches) > 1 {
+		names := strings.Split(strings.Trim(matches[1], "() "), ",")
+		includedColumnNames = utils.Map(names, func(s string) models.FieldName {
+			return models.FieldName(strings.Trim(s, " "))
+		})
+	}
+
+	return models.ConcreteIndex{
+		Indexed:  indexedColumnNames,
+		Included: includedColumnNames,
+	}
+}
+
+func AdaptConcretIndex(pgIndex PGIndex) models.ConcreteIndex {
+	return parseCreateIndexStatement(pgIndex.Definition)
+}
---
+++ repositories/pg_indexes/pg_indexes_test.go
@@ -0,0 +1,53 @@
+package pg_indexes
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/checkmarble/marble-backend/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseCreateIndexStatement(t *testing.T) {
+	t.Run("With 2 indexed and 4 included and WHERE condition", func(t *testing.T) {
+		asserts := assert.New(t)
+		idx := parseCreateIndexStatement(
+			`CREATE INDEX index_name ON "org-test"."Transaction" USING btree (userpublicid, eventdate DESC) INCLUDE (object_id, transactionamount, operationdirection, operationcode) WHERE (valid_until = 'infinity'::timestamp with time zone)`,
+		)
+		asserts.Equal(
+			models.ConcreteIndex{
+				Indexed:  []models.FieldName{"userpublicid", "eventdate"},
+				Included: []models.FieldName{"object_id", "transactionamount", "operationdirection", "operationcode"},
+			},
+			idx,
+		)
+	})
+
+	t.Run("With 2 indexed and 4 included", func(t *testing.T) {
+		asserts := assert.New(t)
+		idx := parseCreateIndexStatement(
+			`CREATE INDEX index_name ON "org-test"."Transaction" USING btree (userpublicid, eventdate DESC) INCLUDE (object_id, transactionamount, operationdirection, operationcode)`,
+		)
+		asserts.Equal(
+			models.ConcreteIndex{
+				Indexed:  []models.FieldName{"userpublicid", "eventdate"},
+				Included: []models.FieldName{"object_id", "transactionamount", "operationdirection", "operationcode"},
+			},
+			idx,
+		)
+	})
+
+	t.Run("With 2 indexed", func(t *testing.T) {
+		asserts := assert.New(t)
+		idx := parseCreateIndexStatement(`CREATE INDEX index_name ON Transaction (userpublicid DESC)`)
+		fmt.Println(idx)
+		asserts.Equal(
+			models.ConcreteIndex{
+				Indexed:  []models.FieldName{"userpublicid"},
+				Included: nil,
+			},
+			idx,
+		)
+	})
+
+}
