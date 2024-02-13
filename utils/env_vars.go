27dc6661372e897c19e87708d16c8435ce06a021
Thomas Lathuiliere <thomas@checkmarble.com>
2024-02-13 19:13:04 +0100

    refactor(env): add reflexivity to env vars

--- utils/env_vars.go
+++ utils/env_vars.go
@@ -1,69 +1,57 @@
 package utils
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"reflect"
 	"strconv"
 )
 
-func GetRequiredIntEnv(envVarName string) int {
-	envValue, ok := os.LookupEnv(envVarName)
-	if !ok || envValue == "" {
-		log.Fatalf("%s environment variable is required", envVarName)
+type ParserFunc func(v string) (interface{}, error)
+
+var (
+	defaultBuiltInParsers = map[reflect.Kind]ParserFunc{
+		reflect.String: func(v string) (interface{}, error) {
+			return v, nil
+		},
+		reflect.Bool: func(v string) (interface{}, error) {
+			return strconv.ParseBool(v)
+		},
+		reflect.Int: func(v string) (interface{}, error) {
+			return strconv.Atoi(v)
+		},
 	}
-	intValue, err := strconv.Atoi(envValue)
-	if err != nil {
-		panic(fmt.Sprintf("Environment variable %s is not valid. '%s' is not an integer", envVarName, envValue))
-	}
-	return intValue
+)
+
+type envVarType interface {
+	~string | ~bool | ~int
 }
 
-func GetIntEnv(envVarName string, defaultValue int) int {
-	envValue, ok := os.LookupEnv(envVarName)
+func parseEnvVar[T envVarType](envVar string, envValue string) T {
+	var envParsedValue T
+	var err error
+
+	envVarType := reflect.TypeOf(envParsedValue)
+	value, err := defaultBuiltInParsers[envVarType.Kind()](envValue)
+	if err != nil {
+		log.Fatalf("%s environment variable is not valid: '%s' cannot be converted to %T", envVar, envValue, envParsedValue)
+	}
+	return value.(T)
+}
+
+func GetEnv[T envVarType](envVar string, defaultValue T) T {
+	envValue, ok := os.LookupEnv(envVar)
 	if !ok || envValue == "" {
+		log.Printf("%s environment variable is not set, using default value '%v'", envVar, defaultValue)
 		return defaultValue
 	}
-	intValue, err := strconv.Atoi(envValue)
-	if err != nil {
-		panic(fmt.Sprintf("Environment variable %s is not valid. '%s' is not an integer", envVarName, envValue))
-	}
-	return intValue
+	return parseEnvVar[T](envVar, envValue)
 }
 
-func GetRequiredStringEnv(envVar string) string {
+func GetRequiredEnv[T envVarType](envVar string) T {
 	envValue, ok := os.LookupEnv(envVar)
 	if !ok || envValue == "" {
 		log.Fatalf("%s environment variable is required", envVar)
 	}
-	return envValue
-}
-
-func GetStringEnv(envVar string, defaultValue string) string {
-	envValue, ok := os.LookupEnv(envVar)
-	if !ok || envValue == "" {
-		log.Printf("%s environment variable is not set, using default value '%s'", envVar, defaultValue)
-		return defaultValue
-	}
-	return envValue
-}
-
-func GetRequiredBoolEnv(envVar string) bool {
-	envValue, err := strconv.ParseBool(GetRequiredStringEnv(envVar))
-	if err != nil {
-		log.Fatalf("%s environment variable is no valid. '%s' cannot be converted to bool", envVar, err)
-	}
-	return envValue
-}
-
-func GetBoolEnv(envVar string, defaultValue bool) bool {
-	stringEnvValue := GetStringEnv(envVar, "")
-	if stringEnvValue == "" {
-		return defaultValue
-	}
-	envValue, err := strconv.ParseBool(stringEnvValue)
-	if err != nil {
-		log.Fatalf("%s environment variable is not valid: '%s' cannot be converted to bool", envVar, err)
-	}
-	return envValue
+	return parseEnvVar[T](envVar, envValue)
 }
