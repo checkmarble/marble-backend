a021ce1a09f365f421962cfe1fcbe0d2f40514e3
Pascal Delange <pascal@checkmarble.com>
2024-02-20 14:57:58 +0100

    new format for error output with error code on publication

--- api/present_error.go
+++ api/present_error.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/checkmarble/marble-backend/dto"
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/utils"
 	"github.com/getsentry/sentry-go"
@@ -16,26 +17,30 @@ func presentError(c *gin.Context, err error) bool {
 	if err == nil {
 		return false
 	}
+	errString := err.Error()
+	errorResponse := dto.APIErrorResponse{
+		Message: errString,
+	}
 
 	if errors.Is(err, models.BadParameterError) {
 		utils.LogRequestInfo(c.Request, fmt.Sprintf("BadParameterError: %v", err))
-		http.Error(c.Writer, err.Error(), http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, errorResponse)
 
 	} else if errors.Is(err, models.UnAuthorizedError) {
 		utils.LogRequestInfo(c.Request, fmt.Sprintf("UnAuthorizedError: %v", err))
-		http.Error(c.Writer, err.Error(), http.StatusUnauthorized)
+		c.JSON(http.StatusUnauthorized, errorResponse)
 
 	} else if errors.Is(err, models.ForbiddenError) {
 		utils.LogRequestInfo(c.Request, fmt.Sprintf("ForbiddenError: %v", err))
-		http.Error(c.Writer, err.Error(), http.StatusForbidden)
+		c.JSON(http.StatusForbidden, errorResponse)
 
 	} else if errors.Is(err, models.NotFoundError) {
 		utils.LogRequestInfo(c.Request, fmt.Sprintf("NotFoundError: %v", err))
-		http.Error(c.Writer, err.Error(), http.StatusNotFound)
+		c.JSON(http.StatusNotFound, errorResponse)
 
 	} else if errors.Is(err, models.ConflictError) {
 		utils.LogRequestInfo(c.Request, fmt.Sprintf("ConflictError: %v", err))
-		http.Error(c.Writer, err.Error(), http.StatusConflict)
+		c.JSON(http.StatusConflict, errorResponse)
 
 	} else {
 		utils.LogRequestError(c.Request, fmt.Sprintf("Unexpected Error: %+v", err))
@@ -44,7 +49,9 @@ func presentError(c *gin.Context, err error) bool {
 		} else {
 			sentry.CaptureException(err)
 		}
-		http.Error(c.Writer, "", http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, dto.APIErrorResponse{
+			Message: "An unexpected error occurred. Please try again later, or contact support if the problem persists.",
+		})
 	}
 	return true
 }
