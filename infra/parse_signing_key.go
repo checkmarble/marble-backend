1fd6b222140962a32b59bc8e87aba11180a89772
Pascal Delange <pascal@checkmarble.com>
2024-03-12 15:18:34 +0100

    chore: jwt signing key is optional (fallback generate a new one on the fly)

--- infra/parse_signing_key.go
+++ infra/parse_signing_key.go
@@ -1,14 +1,48 @@
 package infra
 
 import (
+	"context"
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"log"
 	"strings"
+
+	"github.com/checkmarble/marble-backend/utils"
 )
 
-func MustParseSigningKey(privateKeyString string) *rsa.PrivateKey {
+func ParseOrGenerateSigningKey(ctx context.Context, privateKeyString string) *rsa.PrivateKey {
+	logger := utils.LoggerFromContext(ctx)
+	if privateKeyString == "" {
+		logger.InfoContext(ctx, "No AUTHENTICATION_JWT_SIGNING_KEY received, generating a new key pair. This is not recommended for production environments as the key will not be stable in time")
+		privateKey, err := generatePrivateKey(2048)
+		if err != nil {
+			panic("Error generating RSA private key")
+		}
+		return privateKey
+	}
+
+	return parsePrivateKey(privateKeyString)
+}
+
+func generatePrivateKey(bitSize int) (*rsa.PrivateKey, error) {
+	// Private Key generation
+	privateKey, err := rsa.GenerateKey(rand.Reader, bitSize)
+	if err != nil {
+		return nil, err
+	}
+
+	// Validate Private Key
+	err = privateKey.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func parsePrivateKey(privateKeyString string) *rsa.PrivateKey {
 	// when a multi-line env variable is passed to the docker container by docker-compose, it escapes the newlines
 	privateKeyString = strings.Replace(privateKeyString, "\\n", "\n", -1)
 	block, _ := pem.Decode([]byte(privateKeyString))
