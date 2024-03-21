2bfef8fc9478acfea2e084aca0edd2dfb891f765
Pascal Delange <pascal@checkmarble.com>
2024-03-21 22:39:20 +0100

    store marble id for transfers

---
+++ repositories/transfer_mappings.go
@@ -0,0 +1,71 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/Masterminds/squirrel"
+
+	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/repositories/dbmodels"
+)
+
+func selectTransferMappings() squirrel.SelectBuilder {
+	return NewQueryBuilder().
+		Select(dbmodels.SelectTransferMappingsColumn...).
+		From(dbmodels.TABLE_TRANSFER_MAPPINGS)
+}
+
+func (repo *MarbleDbRepository) GetTransferMapping(ctx context.Context, exec Executor, id string) (models.TransferMapping, error) {
+	if err := validateMarbleDbExecutor(exec); err != nil {
+		return models.TransferMapping{}, err
+	}
+
+	return SqlToModel(
+		ctx,
+		exec,
+		selectTransferMappings().Where(squirrel.Eq{"id": id}),
+		dbmodels.AdaptTransferMapping,
+	)
+}
+
+func (repo *MarbleDbRepository) ListTransferMappings(ctx context.Context, exec Executor, transferId string) ([]models.TransferMapping, error) {
+	if err := validateMarbleDbExecutor(exec); err != nil {
+		return nil, err
+	}
+
+	return SqlToListOfModels(
+		ctx,
+		exec,
+		selectTransferMappings().Where(squirrel.Eq{"client_transfer_id": transferId}),
+		dbmodels.AdaptTransferMapping,
+	)
+}
+
+func (repo *MarbleDbRepository) CreateTransferMapping(
+	ctx context.Context,
+	exec Executor,
+	id string,
+	transferMapping models.TransferMappingCreateInput,
+) error {
+	if err := validateMarbleDbExecutor(exec); err != nil {
+		return err
+	}
+
+	err := ExecBuilder(
+		ctx,
+		exec,
+		NewQueryBuilder().
+			Insert(dbmodels.TABLE_TRANSFER_MAPPINGS).
+			Columns(
+				"id",
+				"organization_id",
+				"client_transfer_id",
+			).
+			Values(
+				id,
+				transferMapping.OrganizationId,
+				transferMapping.TransferId,
+			),
+	)
+	return err
+}
