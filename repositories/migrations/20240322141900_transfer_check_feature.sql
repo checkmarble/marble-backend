1269560bd5f5f6eac96559a018226871921e3e48
Pascal Delange <pascal@checkmarble.com>
2024-03-22 14:48:25 +0100

    decisions trigger object to jsonb type, index object_id

--- repositories/migrations/20240322141900_transfer_check_feature.sql
+++ repositories/migrations/20240322141900_transfer_check_feature.sql
@@ -11,6 +11,12 @@ CREATE TABLE IF NOT EXISTS
 ALTER TABLE organizations
 ADD COLUMN transfer_check_scenario_id uuid REFERENCES scenarios (id) ON DELETE SET NULL;
 
+ALTER TABLE decisions
+ALTER COLUMN trigger_object
+SET DATA TYPE jsonb USING trigger_object::jsonb;
+
+CREATE INDEX IF NOT EXISTS decision_object_id_idx ON decisions (org_id, (trigger_object ->> 'object_id'));
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
@@ -18,5 +24,11 @@ DROP TABLE transfers;
 
 ALTER TABLE organizations
 DROP COLUMN transfer_check_scenario_id;
+
+ALTER TABLE decisions
+ALTER COLUMN trigger_object
+SET DATA TYPE json USING trigger_object::json;
+
+DROP INDEX decision_object_id_idx;
 
 -- +goose StatementEnd
