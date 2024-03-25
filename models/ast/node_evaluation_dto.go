7f11aec9a813dea2f86d65d01a03a744b69dc62e
Thomas Lathuiliere <thomas@checkmarble.com>
2024-03-25 17:43:50 +0100

    feat(ast_eval): add support for skipping long results

--- models/ast/node_evaluation_dto.go
+++ models/ast/node_evaluation_dto.go
@@ -3,6 +3,8 @@ package ast
 import "github.com/checkmarble/marble-backend/pure_utils"
 
 type NodeEvaluationDto struct {
+	// When too long, the ReturnValue can be omitted (ex: when function is `FUNC_CUSTOM_LIST_ACCESS``).
+	// In such cases, ReturnValue will be the constant `omittedReturnValue``
 	ReturnValue   any                          `json:"return_value"`
 	Errors        []EvaluationErrorDto         `json:"errors"`
 	Children      []NodeEvaluationDto          `json:"children,omitempty"`
@@ -10,10 +12,22 @@ type NodeEvaluationDto struct {
 }
 
 func AdaptNodeEvaluationDto(evaluation NodeEvaluation) NodeEvaluationDto {
-	return NodeEvaluationDto{
+	nodeEvaluationDto := NodeEvaluationDto{
 		ReturnValue:   evaluation.ReturnValue,
 		Errors:        pure_utils.Map(evaluation.Errors, AdaptEvaluationErrorDto),
 		Children:      pure_utils.Map(evaluation.Children, AdaptNodeEvaluationDto),
 		NamedChildren: pure_utils.MapValues(evaluation.NamedChildren, AdaptNodeEvaluationDto),
 	}
+
+	if isReturnValueOmitted(evaluation) {
+		nodeEvaluationDto.ReturnValue = omittedReturnValue
+	}
+
+	return nodeEvaluationDto
+}
+
+const omittedReturnValue = "__omitted_return_value"
+
+func isReturnValueOmitted(evaluation NodeEvaluation) bool {
+	return evaluation.Function == FUNC_CUSTOM_LIST_ACCESS
 }
