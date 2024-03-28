84e947617e66312285b090712e38d7478a029026
Pascal Delange <pascal@checkmarble.com>
2024-03-28 13:36:56 +0100

    setup sentry cron monitor for scheduled execution

--- jobs/execute_all_scheduled_scenarios.go
+++ jobs/execute_all_scheduled_scenarios.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/checkmarble/marble-backend/usecases"
 	"github.com/checkmarble/marble-backend/utils"
+	"github.com/getsentry/sentry-go"
 )
 
 // Runs every minute
@@ -13,12 +14,42 @@ func ExecuteAllScheduledScenarios(ctx context.Context, usecases usecases.Usecase
 	logger := utils.LoggerFromContext(ctx)
 	logger.InfoContext(ctx, "Start pending scheduled executions")
 
+	checkinId := sentry.CaptureCheckIn(
+		&sentry.CheckIn{
+			MonitorSlug: "scheduled-execution",
+			Status:      sentry.CheckInStatusInProgress,
+		},
+		nil,
+	)
+
 	usecasesWithCreds := GenerateUsecaseWithCredForMarbleAdmin(ctx, usecases)
 	runScheduledExecution := usecasesWithCreds.NewRunScheduledExecution()
 	err := runScheduledExecution.ExecuteAllScheduledScenarios(ctx)
 	if err != nil {
+		sentry.CaptureCheckIn(
+			&sentry.CheckIn{
+				ID:          *checkinId,
+				MonitorSlug: "scheduled-execution",
+				Status:      sentry.CheckInStatusError,
+			},
+			nil,
+		)
+		if hub := sentry.GetHubFromContext(ctx); hub != nil {
+			hub.CaptureException(err)
+		} else {
+			sentry.CaptureException(err)
+		}
 		return fmt.Errorf("error executing scheduled scenarios: %w", err)
 	}
+
+	sentry.CaptureCheckIn(
+		&sentry.CheckIn{
+			ID:          *checkinId,
+			MonitorSlug: "scheduled-execution",
+			Status:      sentry.CheckInStatusOK,
+		},
+		nil,
+	)
 
 	logger.InfoContext(ctx, "Done executing all due scenarios")
 	return nil
