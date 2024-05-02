72c3bb8f23bc1eb30dcab95bf2c2eeb8666a20fb
Thomas Lathuiliere <thomas@checkmarble.com>
2024-05-02 15:15:52 +0200

    fix(cors): add missing allowed-origin for dev environment

--- router.go
+++ router.go
@@ -27,7 +27,8 @@ func corsOption(conf AppConfiguration) cors.Config {
 
 	if conf.env == "development" {
 		allowedOrigins = append(allowedOrigins,
-			"http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:5173")
+			"http://localhost:3000", "http://localhost:3001", "http://localhost:3002",
+			"http://localhost:3003", "http://localhost:5173")
 	}
 
 	return cors.Config{
