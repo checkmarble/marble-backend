c6affc0a254fa725863833b421be6c6cffe5ef62
Pascal Delange <pascal@checkmarble.com>
2024-05-24 09:43:35 +0200

    helper function to execute jobs with tracing and sentry monitoring
    typo

--- jobs/execute_all_scheduled_scenarios.go
+++ jobs/execute_all_scheduled_scenarios.go
@@ -2,69 +2,24 @@ package jobs
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/checkmarble/marble-backend/tracing"
 	"github.com/checkmarble/marble-backend/usecases"
-	"github.com/checkmarble/marble-backend/utils"
-	"github.com/getsentry/sentry-go"
 )
 
 // Runs every minute
-func ExecuteAllScheduledScenarios(ctx context.Context, usecases usecases.Usecases, config tracing.Configuration) error {
-	telemetryRessources, err := tracing.Init(config)
-	if err != nil {
-		return fmt.Errorf("error initializing tracing: %w", err)
-	}
-	ctx = utils.StoreOpenTelemetryTracerInContext(ctx, telemetryRessources.Tracer)
-
-	logger := utils.LoggerFromContext(ctx)
-	logger.InfoContext(ctx, "Start pending scheduled executions")
-
-	checkinId := sentry.CaptureCheckIn(
-		&sentry.CheckIn{
-			MonitorSlug: "scheduled-execution",
-			Status:      sentry.CheckInStatusInProgress,
+func ExecuteAllScheduledScenarios(ctx context.Context, uc usecases.Usecases, config tracing.Configuration) error {
+	return executeWithMonitoring(
+		ctx,
+		uc,
+		config,
+		"scheduled-execution",
+		func(
+			ctx context.Context, usecases usecases.Usecases,
+		) error {
+			usecasesWithCreds := GenerateUsecaseWithCredForMarbleAdmin(ctx, usecases)
+			runScheduledExecution := usecasesWithCreds.NewRunScheduledExecution()
+			return runScheduledExecution.ExecuteAllScheduledScenarios(ctx)
 		},
-		nil,
 	)
-	fmt.Println("checkinId: ", *checkinId)
-
-	usecasesWithCreds := GenerateUsecaseWithCredForMarbleAdmin(ctx, usecases)
-	runScheduledExecution := usecasesWithCreds.NewRunScheduledExecution()
-	err = runScheduledExecution.ExecuteAllScheduledScenarios(ctx)
-	if err != nil {
-		// Known issue where Cloud Run will sometimes fail to create the unix socket to connect to CloudSQL. In this case, we don't log the error in Sentry.
-		if strings.Contains(err.Error(), "failed to connect to `host=/cloudsql/") {
-			logger.WarnContext(ctx, "Failed to create unix socket to connect to CloudSQL. Wait for the next execution of the job.")
-			return nil
-		}
-		sentry.CaptureCheckIn(
-			&sentry.CheckIn{
-				ID:          *checkinId,
-				MonitorSlug: "scheduled-execution",
-				Status:      sentry.CheckInStatusError,
-			},
-			nil,
-		)
-		if hub := sentry.GetHubFromContext(ctx); hub != nil {
-			hub.CaptureException(err)
-		} else {
-			sentry.CaptureException(err)
-		}
-		return fmt.Errorf("error executing scheduled scenarios: %w", err)
-	}
-
-	sentry.CaptureCheckIn(
-		&sentry.CheckIn{
-			ID:          *checkinId,
-			MonitorSlug: "scheduled-execution",
-			Status:      sentry.CheckInStatusOK,
-		},
-		nil,
-	)
-
-	logger.InfoContext(ctx, "Done executing all due scenarios")
-	return nil
 }
--- jobs/schedule_due_scenarios.go
+++ jobs/schedule_due_scenarios.go
@@ -2,12 +2,9 @@ package jobs
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
-	"strings"
 
 	"github.com/cockroachdb/errors"
-	"github.com/getsentry/sentry-go"
 
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/tracing"
@@ -16,56 +13,18 @@ import (
 )
 
 // Runs every hour at past 10min
-func ScheduleDueScenarios(ctx context.Context, usecases usecases.Usecases, config tracing.Configuration) error {
-	logger := utils.LoggerFromContext(ctx)
-	telemetryRessources, err := tracing.Init(config)
-	if err != nil {
-		return fmt.Errorf("error initializing tracing: %w", err)
-	}
-	ctx = utils.StoreOpenTelemetryTracerInContext(ctx, telemetryRessources.Tracer)
-
-	checkinId := sentry.CaptureCheckIn(
-		&sentry.CheckIn{
-			MonitorSlug: "scenario-scheduler",
-			Status:      sentry.CheckInStatusInProgress,
+func ScheduleDueScenarios(ctx context.Context, uc usecases.Usecases, config tracing.Configuration) error {
+	return executeWithMonitoring(
+		ctx,
+		uc,
+		config,
+		"scenario-scheduler",
+		func(
+			ctx context.Context, usecases usecases.Usecases,
+		) error {
+			return scheduledScenarios(ctx, usecases)
 		},
-		nil,
 	)
-
-	logger.InfoContext(ctx, "Start scheduling scenarios")
-
-	err = scheduledScenarios(ctx, usecases)
-	if err != nil {
-		// Known issue where Cloud Run will sometimes fail to create the unix socket to connect to CloudSQL. In this case, we don't log the error in Sentry.
-		if strings.Contains(err.Error(), "failed to connect to `host=/cloudsql/") {
-			logger.WarnContext(ctx, "Failed to create unix socket to connect to CloudSQL. Wait for the next execution of the job.")
-			return nil
-		}
-		sentry.CaptureCheckIn(
-			&sentry.CheckIn{
-				ID:          *checkinId,
-				MonitorSlug: "scenario-scheduler",
-				Status:      sentry.CheckInStatusError,
-			},
-			nil,
-		)
-		if hub := sentry.GetHubFromContext(ctx); hub != nil {
-			hub.CaptureException(err)
-		} else {
-			sentry.CaptureException(err)
-		}
-		return fmt.Errorf("failed to schedule due scenarios: %w", err)
-	}
-
-	sentry.CaptureCheckIn(
-		&sentry.CheckIn{
-			ID:          *checkinId,
-			MonitorSlug: "scenario-scheduler",
-			Status:      sentry.CheckInStatusOK,
-		},
-		nil,
-	)
-	return nil
 }
 
 func scheduledScenarios(ctx context.Context, usecases usecases.Usecases) error {
@@ -83,7 +42,7 @@ func scheduledScenarios(ctx context.Context, usecases usecases.Usecases) error {
 	runScheduledExecution := usecasesWithCreds.NewRunScheduledExecution()
 
 	for _, scenario := range scenarios {
-		logger.DebugContext(ctx, "Scheduled scenario scenario",
+		logger.DebugContext(ctx, "Scheduled scenario",
 			slog.String("scenario", scenario.Id),
 			slog.String("organization", scenario.OrganizationId),
 		)
