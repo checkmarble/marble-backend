dd9029952eb1474b9a48bf1afb9dd278f478a1cd
Pascal Delange <pascal@checkmarble.com>
2024-05-24 09:43:35 +0200

    helper function to execute jobs with tracing and sentry monitoring
    typo

---
+++ jobs/execute_with_monitoring.go
@@ -0,0 +1,72 @@
+package jobs
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/checkmarble/marble-backend/tracing"
+	"github.com/checkmarble/marble-backend/usecases"
+	"github.com/checkmarble/marble-backend/utils"
+	"github.com/getsentry/sentry-go"
+)
+
+func executeWithMonitoring(
+	ctx context.Context,
+	uc usecases.Usecases,
+	config tracing.Configuration,
+	jobName string,
+	fn func(context.Context, usecases.Usecases) error,
+) error {
+	telemetryRessources, err := tracing.Init(config)
+	if err != nil {
+		return fmt.Errorf("error initializing tracing: %w", err)
+	}
+	ctx = utils.StoreOpenTelemetryTracerInContext(ctx, telemetryRessources.Tracer)
+
+	logger := utils.LoggerFromContext(ctx)
+	logger.InfoContext(ctx, fmt.Sprintf("Start job %s", jobName))
+
+	checkinId := sentry.CaptureCheckIn(
+		&sentry.CheckIn{
+			MonitorSlug: jobName,
+			Status:      sentry.CheckInStatusInProgress,
+		},
+		nil,
+	)
+
+	err = fn(ctx, uc)
+	if err != nil {
+		// Known issue where Cloud Run will sometimes fail to create the unix socket to connect to CloudSQL. In this case, we don't log the error in Sentry.
+		if strings.Contains(err.Error(), "failed to connect to `host=/cloudsql/") {
+			logger.WarnContext(ctx, "Failed to create unix socket to connect to CloudSQL. Wait for the next execution of the job.")
+			return nil
+		}
+		sentry.CaptureCheckIn(
+			&sentry.CheckIn{
+				ID:          *checkinId,
+				MonitorSlug: jobName,
+				Status:      sentry.CheckInStatusError,
+			},
+			nil,
+		)
+		if hub := sentry.GetHubFromContext(ctx); hub != nil {
+			hub.CaptureException(err)
+		} else {
+			sentry.CaptureException(err)
+		}
+		return fmt.Errorf("error executing job %s: %w", jobName, err)
+	}
+
+	sentry.CaptureCheckIn(
+		&sentry.CheckIn{
+			ID:          *checkinId,
+			MonitorSlug: jobName,
+			Status:      sentry.CheckInStatusOK,
+		},
+		nil,
+	)
+
+	logger.InfoContext(ctx, fmt.Sprintf("Done executing job %s", jobName))
+	return nil
+}
--- jobs/ingest_data_from_csv.go
+++ jobs/ingest_data_from_csv.go
@@ -2,66 +2,26 @@ package jobs
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/checkmarble/marble-backend/tracing"
 	"github.com/checkmarble/marble-backend/usecases"
 	"github.com/checkmarble/marble-backend/utils"
-	"github.com/getsentry/sentry-go"
 )
 
-func IngestDataFromCsv(ctx context.Context, usecases usecases.Usecases, config tracing.Configuration) error {
-	telemetryRessources, err := tracing.Init(config)
-	if err != nil {
-		return fmt.Errorf("error initializing tracing: %w", err)
-	}
-	ctx = utils.StoreOpenTelemetryTracerInContext(ctx, telemetryRessources.Tracer)
-
-	checkinId := sentry.CaptureCheckIn(
-		&sentry.CheckIn{
-			MonitorSlug: "batch-ingestion",
-			Status:      sentry.CheckInStatusInProgress,
+func IngestDataFromCsv(ctx context.Context, uc usecases.Usecases, config tracing.Configuration) error {
+	return executeWithMonitoring(
+		ctx,
+		uc,
+		config,
+		"batch-ingestion",
+		func(
+			ctx context.Context, usecases usecases.Usecases,
+		) error {
+			usecasesWithCreds := GenerateUsecaseWithCredForMarbleAdmin(ctx, usecases)
+			usecase := usecasesWithCreds.NewIngestionUseCase()
+			logger := utils.LoggerFromContext(ctx)
+			logger.InfoContext(ctx, "Start ingesting data from upload logs")
+			return usecase.IngestDataFromCsv(ctx, logger)
 		},
-		nil,
 	)
-
-	usecasesWithCreds := GenerateUsecaseWithCredForMarbleAdmin(ctx, usecases)
-	usecase := usecasesWithCreds.NewIngestionUseCase()
-	logger := utils.LoggerFromContext(ctx)
-	logger.InfoContext(ctx, "Start ingesting data from upload logs")
-
-	err = usecase.IngestDataFromCsv(ctx, logger)
-	if err != nil {
-		// Known issue where Cloud Run will sometimes fail to create the unix socket to connect to CloudSQL. In this case, we don't log the error in Sentry.
-		if strings.Contains(err.Error(), "failed to connect to `host=/cloudsql/") {
-			logger.WarnContext(ctx, "Failed to create unix socket to connect to CloudSQL. Wait for the next execution of the job.")
-			return nil
-		}
-		sentry.CaptureCheckIn(
-			&sentry.CheckIn{
-				ID:          *checkinId,
-				MonitorSlug: "batch-ingestion",
-				Status:      sentry.CheckInStatusError,
-			},
-			nil,
-		)
-		if hub := sentry.GetHubFromContext(ctx); hub != nil {
-			hub.CaptureException(err)
-		} else {
-			sentry.CaptureException(err)
-		}
-		return fmt.Errorf("failed to ingest data from upload logs: %w", err)
-	}
-	logger.InfoContext(ctx, "Completed ingesting data from upload logs")
-	sentry.CaptureCheckIn(
-		&sentry.CheckIn{
-			ID:          *checkinId,
-			MonitorSlug: "batch-ingestion",
-			Status:      sentry.CheckInStatusOK,
-		},
-		nil,
-	)
-
-	return nil
 }
