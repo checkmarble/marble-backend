0e4ca74744efb0d1b2a101c44c7e2c8db095d0d9
Pascal Delange <pascal@checkmarble.com>
2024-06-28 11:34:15 +0200

    write actual usecase & handlers

--- usecases/security/enforce_security_transfer_check.go
+++ usecases/security/enforce_security_transfer_check.go
@@ -39,3 +39,57 @@ func (e *EnforceSecurityImpl) ReadTransferData(ctx context.Context, partnerId st
 		utils.EnforcePartnerAccess(e.Credentials, partnerId),
 	)
 }
+
+func (e *EnforceSecurityImpl) ReadTransferAlert(
+	ctx context.Context,
+	transferAlert models.TransferAlert,
+	senderOrReceiver string,
+) error {
+	var err error
+
+	switch senderOrReceiver {
+	case "sender":
+		err = utils.EnforcePartnerAccess(e.Credentials, transferAlert.SenderPartnerId)
+	case "receiver":
+		err = utils.EnforcePartnerAccess(e.Credentials, transferAlert.BeneficiaryPartnerId)
+	default:
+		err = errors.Newf("invalid access type %s", senderOrReceiver)
+	}
+
+	return errors.Join(
+		err,
+		e.Permission(models.TRANSFER_READ),
+		utils.EnforceOrganizationAccess(e.Credentials, transferAlert.OrganizationId),
+	)
+}
+
+func (e *EnforceSecurityImpl) UpdateTransferAlert(
+	ctx context.Context,
+	transferAlert models.TransferAlert,
+	senderOrReceiver string,
+) error {
+	var err error
+
+	switch senderOrReceiver {
+	case "sender":
+		err = utils.EnforcePartnerAccess(e.Credentials, transferAlert.SenderPartnerId)
+	case "receiver":
+		err = utils.EnforcePartnerAccess(e.Credentials, transferAlert.BeneficiaryPartnerId)
+	default:
+		err = errors.Newf("invalid access type %s", senderOrReceiver)
+	}
+
+	return errors.Join(
+		err,
+		e.Permission(models.TRANSFER_UPDATE),
+		utils.EnforceOrganizationAccess(e.Credentials, transferAlert.OrganizationId),
+	)
+}
+
+func (e *EnforceSecurityImpl) CreateTransferAlert(ctx context.Context, organizationId string, partnerId string) error {
+	return errors.Join(
+		e.Permission(models.TRANSFER_CREATE),
+		utils.EnforceOrganizationAccess(e.Credentials, organizationId),
+		utils.EnforcePartnerAccess(e.Credentials, partnerId),
+	)
+}
