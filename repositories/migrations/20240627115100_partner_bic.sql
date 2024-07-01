6e8719c0a38f1b73649a282935521280e80d28b7
Pascal Delange <pascal@checkmarble.com>
2024-07-01 10:26:08 +0200

    fix: consistency

--- repositories/migrations/20240627115100_partner_bic.sql
+++ repositories/migrations/20240627115100_partner_bic.sql
@@ -3,7 +3,7 @@
 ALTER TABLE partners
 ADD COLUMN bic VARCHAR NOT NULL DEFAULT '';
 
-CREATE INDEX partners_bic_idx ON partners (LOWER(bic));
+CREATE INDEX partners_bic_idx ON partners (UPPER(bic));
 
 -- +goose StatementEnd
 -- +goose Down
