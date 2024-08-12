d7c9972bc3a8c9f72171a2df109fcc706a8dc716
Pascal Delange <pasc.delange@gmail.com>
2024-08-12 11:43:43 +0800

    created_from_rule_id is not null
    fix

--- repositories/migrations/20240812112700_rule_snoozes_store_rule_id.sql
+++ repositories/migrations/20240812112700_rule_snoozes_store_rule_id.sql
@@ -1,7 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
+TRUNCATE rule_snoozes;
+
 ALTER TABLE rule_snoozes
-ADD COLUMN created_from_rule_id UUID REFERENCES scenario_iteration_rules (id) ON DELETE SET NULL;
+ADD COLUMN created_from_rule_id UUID NOT NULL REFERENCES scenario_iteration_rules (id) ON DELETE CASCADE;
 
 -- +goose StatementEnd
 -- +goose Down
