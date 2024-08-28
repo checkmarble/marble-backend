486e3cc931a86b98e4f7a6b5f4395223039072fa
Pascal Delange <pascal@checkmarble.com>
2024-08-28 21:55:16 +0200

    rewrite OrganizationIdFromRequest
    fix

--- utils/context_credentials.go
+++ utils/context_credentials.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/cockroachdb/errors"
 )
 
 func CredentialsFromCtx(ctx context.Context) (models.Credentials, bool) {
@@ -14,41 +15,43 @@ func CredentialsFromCtx(ctx context.Context) (models.Credentials, bool) {
 }
 
 func OrganizationIdFromRequest(request *http.Request) (organizationId string, err error) {
+	if request == nil {
+		return "", fmt.Errorf("no request passed to OrganizationIdFromRequest: %w", models.ForbiddenError)
+	}
+
 	creds, found := CredentialsFromCtx(request.Context())
 	if !found {
 		return "", fmt.Errorf("no credentials in context: %w", models.ForbiddenError)
 	}
 
 	var requestOrganizationId string
-	if request != nil {
-		requestOrganizationId = request.URL.Query().Get("organization-id")
-		if requestOrganizationId != "" {
-			if err := ValidateUuid(requestOrganizationId); err != nil {
-				return "", err
-			}
-		}
-	}
 
 	// allow organizationId to be passed in query param
+	requestOrganizationId = request.URL.Query().Get("organization-id")
 	if requestOrganizationId != "" {
+		if err := ValidateUuid(requestOrganizationId); err != nil {
+			return "", err
+		}
+
+		// technically, any user can pass an org id in query params, but it can be different from the credentials org id
+		// only for a marble admin user
 		if err := EnforceOrganizationAccess(creds, requestOrganizationId); err != nil {
 			return "", err
 		}
 		return requestOrganizationId, nil
 	}
 
+	if creds.OrganizationId == "" && creds.Role == models.MARBLE_ADMIN {
+		return "", errors.Wrap(
+			models.ForbiddenError,
+			"An organizationId must be passed in the request query params for MARBLE_ADMIN to use this endpoint")
+	}
+
 	if creds.OrganizationId == "" {
-		noMarbleAdmin := ""
-		if creds.Role == models.MARBLE_ADMIN {
-			noMarbleAdmin = "this Api is not supposed to be called with marble admin creds "
-		}
-		return "", fmt.Errorf("no organizationId in context. %s: %w", noMarbleAdmin, models.ForbiddenError)
+		return "", errors.Wrap(
+			models.ForbiddenError,
+			"Unexpected error: credentials does not grant access to any organization")
 	}
 
 	return creds.OrganizationId, nil
-}
-
-// TODO: replace me with OrganizationIdFromContext
-func OrgIDFromCtx(ctx context.Context, request *http.Request) (organizationId string, err error) {
-	return OrganizationIdFromRequest(request)
 }
