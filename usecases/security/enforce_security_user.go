5a2c2e315b2e5cd91bb62ec943e5b066ec81e684
Pascal Delange <pascal@checkmarble.com>
2024-09-11 21:31:20 +0200

    fix: permission check on user creation with role & partner

--- usecases/security/enforce_security_user.go
+++ usecases/security/enforce_security_user.go
@@ -1,14 +1,14 @@
 package security
 
 import (
-	"errors"
-
 	"github.com/checkmarble/marble-backend/models"
+
+	"github.com/cockroachdb/errors"
 )
 
 type EnforceSecurityUser interface {
 	EnforceSecurity
-	CreateUser(organizationId string) error
+	CreateUser(input models.CreateUser) error
 	UpdateUser(user models.User) error
 	DeleteUser(user models.User) error
 	ListUser() error
@@ -19,9 +19,28 @@ type EnforceSecurityUserImpl struct {
 	Credentials models.Credentials
 }
 
-func (e *EnforceSecurityUserImpl) CreateUser(organizationId string) error {
+func (e *EnforceSecurityUserImpl) CreateUser(input models.CreateUser) error {
+	var errLevel error
+	if input.Role == models.MARBLE_ADMIN && e.Credentials.Role != models.MARBLE_ADMIN {
+		errLevel = errors.Wrap(
+			models.ForbiddenError,
+			"only marble admins can create marble admins")
+	}
+
+	var errPartner error
+	if input.PartnerId != nil && (e.Credentials.Role != models.MARBLE_ADMIN ||
+		e.Credentials.PartnerId == nil ||
+		*e.Credentials.PartnerId != *input.PartnerId) {
+		errPartner = errors.Wrap(
+			models.ForbiddenError,
+			"only marble admins can create users with partner_id")
+	}
+
 	return errors.Join(
-		e.Permission(models.MARBLE_USER_CREATE), e.ReadOrganization(organizationId),
+		e.Permission(models.MARBLE_USER_CREATE),
+		e.ReadOrganization(input.OrganizationId),
+		errLevel,
+		errPartner,
 	)
 }
 
