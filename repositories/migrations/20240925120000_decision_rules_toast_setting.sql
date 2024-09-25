41ec5b95db9078e518803e37a0ffbc7438a66ff4
Pascal Delange <pascal@checkmarble.com>
2024-09-25 15:13:51 +0200

    read normalized rule name, description
    fix
    
    fix

--- repositories/migrations/20240925120000_decision_rules_toast_setting.sql
+++ repositories/migrations/20240925120000_decision_rules_toast_setting.sql
@@ -4,11 +4,27 @@ ALTER TABLE decision_rules
 SET
     (TOAST_TUPLE_TARGET = 128);
 
+ALTER TABLE decision_rules
+ALTER COLUMN name
+DROP NOT NULL;
+
+ALTER TABLE decision_rules
+ALTER COLUMN description
+DROP NOT NULL;
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE decision_rules
 SET
     (TOAST_TUPLE_TARGET = 2048);
+
+ALTER TABLE decision_rules
+ALTER COLUMN name
+SET NOT NULL;
+
+ALTER TABLE decision_rules
+ALTER COLUMN description
+SET NOT NULL;
 
 -- +goose StatementEnd
