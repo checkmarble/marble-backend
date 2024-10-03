11e83649f77dbff7176839357a5a01d0c50233e3
Pascal Delange <pascal@checkmarble.com>
2024-10-03 22:01:16 +0200

    liveness check calls the db

--- api/handle_liveness_probe.go
+++ api/handle_liveness_probe.go
@@ -3,11 +3,20 @@ package api
 import (
 	"net/http"
 
+	"github.com/checkmarble/marble-backend/usecases"
 	"github.com/gin-gonic/gin"
 )
 
-func handleLivenessProbe(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"mood": "Feu flammes !",
-	})
+func handleLivenessProbe(uc usecases.Usecases) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		usecase := uc.NewLivenessUsecase()
+		err := usecase.Liveness(c.Request.Context())
+		if presentError(c, err) {
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"mood": "Feu flammes !",
+		})
+	}
 }
