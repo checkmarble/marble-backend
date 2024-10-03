244e7342c4695515b3169730dcdbcd3c77ddbe7e
Pascal Delange <pascal@checkmarble.com>
2024-10-03 13:55:12 +0200

    update dto for scheduled exec

--- repositories/dbmodels/db_scheduled_executions.go
+++ repositories/dbmodels/db_scheduled_executions.go
@@ -7,35 +7,48 @@ import (
 )
 
 type DBScheduledExecution struct {
-	Id                       string     `db:"id"`
-	OrganizationId           string     `db:"organization_id"`
-	ScenarioId               string     `db:"scenario_id"`
-	ScenarioIterationId      string     `db:"scenario_iteration_id"`
-	Status                   string     `db:"status"`
-	StartedAt                time.Time  `db:"started_at"`
-	FinishedAt               *time.Time `db:"finished_at"`
-	NumberOfCreatedDecisions int        `db:"number_of_created_decisions"`
-	Manual                   bool       `db:"manual"`
+	Id                         string     `db:"id"`
+	OrganizationId             string     `db:"organization_id"`
+	ScenarioId                 string     `db:"scenario_id"`
+	ScenarioIterationId        string     `db:"scenario_iteration_id"`
+	Status                     string     `db:"status"`
+	StartedAt                  time.Time  `db:"started_at"`
+	FinishedAt                 *time.Time `db:"finished_at"`
+	NumberOfCreatedDecisions   *int       `db:"number_of_created_decisions"`
+	NumberOfEvaluatedDecisions *int       `db:"number_of_evaluated_decisions"`
+	NumberOfPlannedDecisions   *int       `db:"number_of_planned_decisions"`
+	Manual                     bool       `db:"manual"`
 }
 
 const TABLE_SCHEDULED_EXECUTIONS = "scheduled_executions"
 
 var ScheduledExecutionFields = []string{
-	"id", "organization_id", "scenario_id",
-	"scenario_iteration_id", "status", "started_at", "finished_at", "number_of_created_decisions", "manual",
+	"id",
+	"organization_id",
+	"scenario_id",
+	"scenario_iteration_id",
+	"status",
+	"started_at",
+	"finished_at",
+	"number_of_created_decisions",
+	"number_of_evaluated_decisions",
+	"number_of_planned_decisions",
+	"manual",
 }
 
 func AdaptScheduledExecution(db DBScheduledExecution, scenario models.Scenario) models.ScheduledExecution {
 	return models.ScheduledExecution{
-		Id:                       db.Id,
-		OrganizationId:           db.OrganizationId,
-		ScenarioId:               db.ScenarioId,
-		ScenarioIterationId:      db.ScenarioIterationId,
-		Status:                   models.ScheduledExecutionStatusFrom(db.Status),
-		StartedAt:                db.StartedAt,
-		FinishedAt:               db.FinishedAt,
-		NumberOfCreatedDecisions: db.NumberOfCreatedDecisions,
-		Scenario:                 scenario,
-		Manual:                   db.Manual,
+		Id:                         db.Id,
+		OrganizationId:             db.OrganizationId,
+		ScenarioId:                 db.ScenarioId,
+		ScenarioIterationId:        db.ScenarioIterationId,
+		Status:                     models.ScheduledExecutionStatusFrom(db.Status),
+		StartedAt:                  db.StartedAt,
+		FinishedAt:                 db.FinishedAt,
+		NumberOfCreatedDecisions:   db.NumberOfCreatedDecisions,
+		NumberOfEvaluatedDecisions: db.NumberOfEvaluatedDecisions,
+		NumberOfPlannedDecisions:   db.NumberOfPlannedDecisions,
+		Scenario:                   scenario,
+		Manual:                     db.Manual,
 	}
 }
