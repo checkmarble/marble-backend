1dd2fada25f13ae454af92b9d14e8a2fb7d97519
Franck Kambiwa <franck.kambiwa@checkmarble.com>
2024-10-30 01:52:03 +0100

    fix script create table

--- repositories/migrations/20241029013218_scenario_testrun.sql
+++ repositories/migrations/20241029013218_scenario_testrun.sql
@@ -4,7 +4,7 @@ CREATE TABLE scenario_testrun(
  id uuid DEFAULT uuid_generate_v4(),
  scenario_iteration_id uuid NOT NULL,
  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
- expires_at TIMESTAMP WITH TIME ZONE NOT NULL
+ expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
  status VARCHAR NOT NULL,
  PRIMARY KEY(id),
  CONSTRAINT fk_scenario_publications_scenario_iterations FOREIGN KEY(scenario_iteration_id) REFERENCES scenario_iterations(id) ON DELETE CASCADE
