82e6c4bc8d0de64d49ec69431df0bc4ba0a1f499
Pascal Delange <pascal@checkmarble.com>
2024-11-05 13:39:59 +0100

    use a "default" db config as default, if provided

--- repositories/dbmodels/db_organization.go
+++ repositories/dbmodels/db_organization.go
@@ -6,10 +6,11 @@ import (
 )
 
 type DBOrganizationResult struct {
-	Id                      string  `db:"id"`
-	DeletedAt               *int    `db:"deleted_at"`
-	Name                    string  `db:"name"`
-	TransferCheckScenarioId *string `db:"transfer_check_scenario_id"`
+	Id                         string  `db:"id"`
+	DeletedAt                  *int    `db:"deleted_at"`
+	Name                       string  `db:"name"`
+	TransferCheckScenarioId    *string `db:"transfer_check_scenario_id"`
+	UseMarbleDbSchemaAsDefault bool    `db:"use_marble_db_schema_as_default"`
 }
 
 const TABLE_ORGANIZATION = "organizations"
@@ -18,8 +19,9 @@ var ColumnsSelectOrganization = utils.ColumnList[DBOrganizationResult]()
 
 func AdaptOrganization(db DBOrganizationResult) (models.Organization, error) {
 	return models.Organization{
-		Id:                      db.Id,
-		Name:                    db.Name,
-		TransferCheckScenarioId: db.TransferCheckScenarioId,
+		Id:                         db.Id,
+		Name:                       db.Name,
+		TransferCheckScenarioId:    db.TransferCheckScenarioId,
+		UseMarbleDbSchemaAsDefault: db.UseMarbleDbSchemaAsDefault,
 	}, nil
 }
