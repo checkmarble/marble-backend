f169e1adf4cbeef322f274a0136e84f755f16e90
Franck Kambiwa <franck.kambiwa@checkmarble.com>
2024-11-14 16:16:30 +0100

    typo

--- api/routes.go
+++ api/routes.go
@@ -91,8 +91,7 @@ func addRoutes(r *gin.Engine, auth Authentication, tokenHandler TokenHandler, uc
 
 	router.GET("/scenario-publications", tom, handleListScenarioPublications(uc))
 	router.POST("/scenario-publications", tom, handleCreateScenarioPublication(uc))
-	router.POST("/scenarios-testrun", tom,
-		handleCreateScenarioTestRun(uc))
+	router.POST("/scenarios-testrun", tom, handleCreateScenarioTestRun(uc))
 	router.GET("/scenario-publications/preparation", tom,
 		handleGetPublicationPreparationStatus(uc))
 	router.POST("/scenario-publications/preparation", tom, handleStartPublicationPreparation(uc))
