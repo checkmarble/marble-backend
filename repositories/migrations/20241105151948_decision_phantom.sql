45bbe934914e2dd38140b8442f3994e3ac3547c1
Pascal Delange <pascal@checkmarble.com>
2024-12-12 11:05:12 +0100

    reorder migrations

--- repositories/migrations/20241105151948_decision_phantom.sql
+++ repositories/migrations/20241105151948_decision_phantom.sql
@@ -1,23 +1,22 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE phantom_decisions(
-  id uuid DEFAULT uuid_generate_v4(),
-  org_id uuid NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-  outcome decision_outcome NOT NULL,
-  scenario_id uuid NOT NULL,
-  score INT NOT NULL,
-  scenario_iteration_id uuid NOT NULL,
-  test_run_id uuid NOT NULL,
-  scenario_version INT NOT NULL,
-  PRIMARY KEY(id),
-  CONSTRAINT fk_phantom_decisions_org FOREIGN KEY(org_id) REFERENCES organizations(id) ON DELETE CASCADE,
-  CONSTRAINT fk_phantom_decisions_test_run_id FOREIGN KEY(test_run_id) REFERENCES scenario_test_run(id) ON DELETE CASCADE,
-  CONSTRAINT fk_phantom_decisions_scenario_ite_id FOREIGN KEY(scenario_iteration_id) REFERENCES scenario_iterations(id) ON DELETE CASCADE
-);
--- +goose StatementEnd
+CREATE TABLE
+  phantom_decisions (
+    id uuid DEFAULT uuid_generate_v4 (),
+    org_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    outcome decision_outcome NOT NULL,
+    scenario_id uuid NOT NULL,
+    score INT NOT NULL,
+    scenario_iteration_id uuid NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_phantom_decisions_org FOREIGN KEY (org_id) REFERENCES organizations (id) ON DELETE CASCADE,
+    CONSTRAINT fk_phantom_decisions_scenario_ite_id FOREIGN KEY (scenario_iteration_id) REFERENCES scenario_iterations (id) ON DELETE CASCADE
+  );
 
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE phantom_decisions;
+
 -- +goose StatementEnd
