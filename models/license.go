c3e362f6d67f72f7fa63e169699abbd3e08a4d39
Pascal Delange <pascal@checkmarble.com>
2025-01-09 11:15:53 +0100

    rename function

--- models/license.go
+++ models/license.go
@@ -62,51 +62,6 @@ type LicenseEntitlements struct {
 	Sanctions      bool
 }
 
-func (l *LicenseEntitlements) MergeWithFeatureAccess(f DbStoredOrganizationFeatureAccess) OrganizationFeatureAccess {
-	o := OrganizationFeatureAccess{
-		Id:             f.Id,
-		OrganizationId: f.OrganizationId,
-		TestRun:        f.TestRun,
-		Sanctions:      f.Sanctions,
-		CreatedAt:      f.CreatedAt,
-		UpdatedAt:      f.UpdatedAt,
-	}
-	// Add the feature accesses computed directly from the license entitlements
-	if l.Analytics {
-		o.Analytics = Allowed
-	}
-	if l.Webhooks {
-		o.Webhooks = Allowed
-	}
-	if l.Workflows {
-		o.Workflows = Allowed
-	}
-	if l.RuleSnoozes {
-		o.RuleSnoozes = Allowed
-	}
-	if l.UserRoles {
-		o.Roles = Allowed
-	}
-
-	// remove the feature accesses that are not allowed by the license
-	if !l.TestRun {
-		o.TestRun = Restricted
-	}
-	if !l.Sanctions {
-		o.Sanctions = Restricted
-	}
-
-	// set to "test" if the feature access overrides the license
-	if f.TestRun == Test {
-		o.TestRun = Test
-	}
-	if f.Sanctions == Test {
-		o.Sanctions = Test
-	}
-
-	return o
-}
-
 type LicenseValidation struct {
 	LicenseValidationCode
 	LicenseEntitlements
