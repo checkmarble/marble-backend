f5a04239ebb6a3274c9c23f5e584e858da68ba71
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-16 14:57:09 +0100

    Added initial migration and dbmodels for sanction checks.

---
+++ repositories/dbmodels/db_sanction_check_match.go
@@ -0,0 +1,23 @@
+package dbmodels
+
+import (
+	"time"
+
+	"github.com/checkmarble/marble-backend/utils"
+)
+
+const TABLE_SANCTION_CHECK_MATCHES = "sanction_check_matches"
+
+var SelectSanctionCheckMatchesColumn = utils.ColumnList[DBSanctionCheckMatch]()
+
+type DBSanctionCheckMatch struct {
+	Id                   string    `db:"id"`
+	SanctionCheckId      string    `db:"sanction_check_id"`
+	OpenSanctionEntityId string    `db:"opensanction_entity_id"`
+	Status               string    `db:"status"`
+	QueryIds             []string  `db:"query_ids"`
+	Payload              []byte    `db:"payload"`
+	ReviewedBy           string    `db:"reviewed_by"`
+	CreatedAt            time.Time `db:"created_at"`
+	UpdatedAt            time.Time `db:"updated_at"`
+}
