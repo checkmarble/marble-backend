857c5d9cf28ea7c493b8557ba16248f27fea982f
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-17 11:35:13 +0100

    Set up plumbing for org-level configuration.

--- usecases/sanction_check_usecase.go
+++ usecases/sanction_check_usecase.go
@@ -4,10 +4,14 @@ import (
 	"context"
 
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/repositories"
+	"github.com/checkmarble/marble-backend/usecases/executor_factory"
+	"github.com/pkg/errors"
 )
 
 type SanctionCheckProvider interface {
-	Search(context.Context, models.SanctionCheckConfig, models.OpenSanctionsQuery) (models.SanctionCheckResult, error)
+	Search(context.Context, models.OrganizationOpenSanctionsConfig, models.SanctionCheckConfig,
+		models.OpenSanctionsQuery) (models.SanctionCheckResult, error)
 }
 
 type SanctionCheckRepository interface {
@@ -15,14 +19,22 @@ type SanctionCheckRepository interface {
 }
 
 type SanctionCheckUsecase struct {
-	openSanctionsProvider SanctionCheckProvider
-	repository            SanctionCheckRepository
+	organizationRepository repositories.OrganizationRepository
+	openSanctionsProvider  SanctionCheckProvider
+	repository             SanctionCheckRepository
+	executorFactory        executor_factory.ExecutorFactory
 }
 
-func (uc SanctionCheckUsecase) Execute(ctx context.Context, cfg models.SanctionCheckConfig,
+func (uc SanctionCheckUsecase) Execute(ctx context.Context, orgId string, cfg models.SanctionCheckConfig,
 	query models.OpenSanctionsQuery,
 ) (models.SanctionCheckResult, error) {
-	matches, err := uc.openSanctionsProvider.Search(ctx, cfg, query)
+	org, err := uc.organizationRepository.GetOrganizationById(ctx,
+		uc.executorFactory.NewExecutor(), orgId)
+	if err != nil {
+		return models.SanctionCheckResult{}, errors.Wrap(err, "could not retrieve organization")
+	}
+
+	matches, err := uc.openSanctionsProvider.Search(ctx, org.OpenSanctionsConfig, cfg, query)
 	if err != nil {
 		return models.SanctionCheckResult{}, err
 	}
