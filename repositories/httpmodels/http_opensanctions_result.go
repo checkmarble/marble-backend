87a556ccc34e365a02daca3d16eeb6f1b97e1e1a
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-20 13:01:45 +0100

    Start writing results and matches to database.

--- repositories/httpmodels/http_opensanctions_result.go
+++ repositories/httpmodels/http_opensanctions_result.go
@@ -1,6 +1,8 @@
 package httpmodels
 
 import (
+	"bytes"
+	"encoding/json"
 	"maps"
 	"slices"
 
@@ -12,42 +14,63 @@ type HTTPOpenSanctionsResult struct {
 		Total struct {
 			Value int `json:"value"`
 		} `json:"total"`
-		Results []struct {
-			Id         string   `json:"id"`
-			Schema     string   `json:"schema"`
-			Datasets   []string `json:"datasets"`
-			Properties struct {
-				Name []string `json:"name"`
-			} `json:"properties"`
-		} `json:"results"`
+		Results []json.RawMessage `json:"results"`
 	} `json:"responses"`
 }
 
-func AdaptOpenSanctionsResult(result HTTPOpenSanctionsResult) (models.SanctionCheckExecution, error) {
+type HTTPOpenSanctionResultResult struct {
+	Id         string   `json:"id"`
+	Schema     string   `json:"schema"`
+	Datasets   []string `json:"datasets"`
+	Properties struct {
+		Name []string `json:"name"`
+	} `json:"properties"`
+}
+
+func AdaptOpenSanctionsResult(query models.OpenSanctionsQuery, result HTTPOpenSanctionsResult) (models.SanctionCheckExecution, error) {
 	// TODO: Replace with actual processing of responses
 	partial := false
 	matches := make(map[string]models.SanctionCheckExecutionMatch)
+	matchToQueryId := make(map[string][]string)
 
-	for _, resp := range result.Responses {
+	for queryId, resp := range result.Responses {
 		if resp.Total.Value > len(resp.Results) {
 			partial = true
 		}
 
 		for _, match := range resp.Results {
-			if _, ok := matches[match.Id]; !ok {
+			var parsed HTTPOpenSanctionResultResult
+
+			if err := json.NewDecoder(bytes.NewReader(match)).Decode(&parsed); err != nil {
+				return models.SanctionCheckExecution{}, err
+			}
+
+			if _, ok := matches[parsed.Id]; !ok {
 				entity := models.SanctionCheckExecutionMatch{
-					Id:       match.Id,
-					Schema:   match.Schema,
-					Datasets: match.Datasets,
-					Names:    match.Properties.Name,
+					Raw:      match,
+					EntityId: parsed.Id,
+					Schema:   parsed.Schema,
+					Datasets: parsed.Datasets,
 				}
 
-				matches[match.Id] = entity
+				matchToQueryId[parsed.Id] = append(matchToQueryId[parsed.Id], queryId)
+
+				matches[parsed.Id] = entity
+			} else {
+				matchToQueryId[parsed.Id] = append(matchToQueryId[parsed.Id], queryId)
 			}
+		}
+
+		for entityId, queryIds := range matchToQueryId {
+			result := matches[entityId]
+			result.QueryIds = queryIds
+
+			matches[entityId] = result
 		}
 	}
 
 	output := models.SanctionCheckExecution{
+		Query:   query,
 		Partial: partial,
 		Count:   len(matches),
 		Matches: slices.Collect(maps.Values(matches)),
--- repositories/sanction_check_repository.go
+++ repositories/sanction_check_repository.go
@@ -2,19 +2,60 @@ package repositories
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/repositories/dbmodels"
 	"github.com/checkmarble/marble-backend/utils"
 )
 
 func (*MarbleDbRepository) InsertResults(ctx context.Context, exec Executor,
-	matches models.SanctionCheckExecution,
+	decision models.DecisionWithRuleExecutions,
 ) (models.SanctionCheckExecution, error) {
-	if err := validateMarbleDbExecutor(exec); err != nil {
-		return matches, err
-	}
-
 	utils.LoggerFromContext(ctx).Debug("SANCTION CHECK: inserting matches in database")
 
-	return matches, nil
+	if err := validateMarbleDbExecutor(exec); err != nil {
+		return *decision.SanctionCheckExecution, err
+	}
+	sql := NewQueryBuilder().
+		Insert(dbmodels.TABLE_SANCTION_CHECKS).Columns(
+		"decision_id",
+		"search_input",
+		"search_datasets",
+		"search_threshold",
+		"is_partial",
+	).Values(
+		decision.DecisionId,
+		decision.SanctionCheckExecution.Query,
+		decision.SanctionCheckExecution.Query.OrgConfig.Datasets,
+		decision.SanctionCheckExecution.Query.OrgConfig.MatchThreshold,
+		decision.SanctionCheckExecution.Partial,
+	).Suffix(fmt.Sprintf("RETURNING %s", strings.Join(dbmodels.SelectSanctionChecksColumn, ",")))
+
+	result, err := SqlToModel(ctx, exec, sql, dbmodels.AdaptSanctionCheck)
+	if err != nil {
+		return models.SanctionCheckExecution{}, err
+	}
+
+	if len(decision.SanctionCheckExecution.Matches) == 0 {
+		return result, nil
+	}
+
+	matchSql := NewQueryBuilder().Insert(dbmodels.TABLE_SANCTION_CHECK_MATCHES).
+		Columns("sanction_check_id", "opensanction_entity_id", "query_ids", "payload").
+		Suffix(fmt.Sprintf("RETURNING %s", strings.Join(dbmodels.SelectSanctionCheckMatchesColumn, ",")))
+
+	for _, match := range decision.SanctionCheckExecution.Matches {
+		matchSql = matchSql.Values(result.Id, match.EntityId, match.QueryIds, match.Raw)
+	}
+
+	matches, err := SqlToListOfModels(ctx, exec, matchSql, dbmodels.AdaptSanctionCheckMatch)
+	if err != nil {
+		return models.SanctionCheckExecution{}, err
+	}
+
+	result.Matches = matches
+
+	return result, nil
 }
