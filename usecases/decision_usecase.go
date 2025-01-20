2d25e45d730908dddd5ee7e4c498ba92a24f9270
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-20 13:01:45 +0100

    Start writing results and matches to database.

--- usecases/decision_usecase.go
+++ usecases/decision_usecase.go
@@ -448,7 +448,7 @@ func (usecase *DecisionUsecase) CreateDecision(
 
 		if decision.SanctionCheckExecution != nil {
 			if _, err := usecase.sanctionCheckUsecase.repository.InsertResults(ctx, tx,
-				*decision.SanctionCheckExecution); err != nil {
+				decision); err != nil {
 				return models.DecisionWithRuleExecutions{},
 					errors.Wrap(err, "could not store sanction check execution")
 			}
