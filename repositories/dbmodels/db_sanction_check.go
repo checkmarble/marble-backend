fccc2d6f429437c4f24f000f0825a7333b5e75c4
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-20 10:15:56 +0100

    Bump migration version after merge.

--- repositories/dbmodels/db_sanction_check.go
+++ repositories/dbmodels/db_sanction_check.go
@@ -1,9 +1,13 @@
 package dbmodels
 
 import (
+	"bytes"
+	"encoding/json"
 	"time"
 
+	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/utils"
+	"github.com/pkg/errors"
 )
 
 const TABLE_SANCTION_CHECKS = "sanction_checks"
@@ -16,10 +20,33 @@ type DBSanctionCheck struct {
 	Status          string    `db:"status"`
 	SearchInput     []byte    `db:"search_input"`
 	SearchDatasets  []string  `db:"search_datasets"`
-	SearchThreshold float64   `db:"search_threshold"`
+	SearchThreshold int       `db:"search_threshold"`
 	IsManual        bool      `db:"is_manual"`
-	RequestedBy     string    `db:"requested_by"`
+	IsPartial       bool      `db:"is_partial"`
+	RequestedBy     *string   `db:"requested_by"`
 	IsArchived      bool      `db:"is_archived"`
 	CreatedAt       time.Time `db:"created_at"`
 	UpdatedAt       time.Time `db:"updated_at"`
+}
+
+func AdaptSanctionCheck(dto DBSanctionCheck) (models.SanctionCheckExecution, error) {
+	cfg := models.OrganizationOpenSanctionsConfig{
+		MatchThreshold: &dto.SearchThreshold,
+		Datasets:       dto.SearchDatasets,
+	}
+
+	var query models.OpenSanctionsQuery
+
+	err := json.NewDecoder(bytes.NewReader(dto.SearchInput)).Decode(&query)
+	if err != nil {
+		return models.SanctionCheckExecution{},
+			errors.Wrap(err, "could not unmarshal sanction check query input")
+	}
+
+	return models.SanctionCheckExecution{
+		Query:     query,
+		OrgConfig: cfg,
+		Count:     0,
+		Partial:   dto.IsPartial,
+	}, nil
 }
