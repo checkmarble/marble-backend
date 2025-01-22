26ab8120b7f16a5d01cfb4ec4c979bee5d043915
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-22 19:09:47 +0100

    Added example simple usecase test, mocking enforcers and repositories.

---
+++ utils/testing.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"reflect"
+
+	"github.com/go-faker/faker/v4"
+)
+
+func FakeStruct[T any]() (T, []any) {
+	var object T
+
+	_ = faker.FakeData(&object)
+
+	return object, StructToMockRow(object)
+}
+
+func FakeStructRow[T any]() []any {
+	_, row := FakeStruct[T]()
+
+	return row
+}
+
+func StructToMockRow[T any](object T) []any {
+	f := reflect.ValueOf(object)
+	t := reflect.TypeOf(object)
+
+	if f.Kind() != reflect.Struct {
+		panic("StructToMockRow should only be used on structs")
+	}
+
+	slice := make([]any, 0)
+
+	for i := 0; i < f.NumField(); i++ {
+		sf := f.Field(i)
+
+		switch sf.Kind() {
+		case reflect.Struct:
+			switch t.Field(i).Anonymous {
+			case true:
+				slice = append(slice, StructToMockRow(sf.Interface())...)
+			default:
+				slice = append(slice, sf.Interface())
+			}
+		default:
+			slice = append(slice, sf.Interface())
+		}
+	}
+
+	return slice
+}
