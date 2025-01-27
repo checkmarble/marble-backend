6df9a6e82a0c8ecf1a2e083a29ea203191c708e6
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-27 14:34:24 +0100

    Added refine endpoint (still with static query).

--- models/sanction_check.go
+++ models/sanction_check.go
@@ -3,94 +3,47 @@ package models
 import (
 	"encoding/json"
 	"time"
-
-	"github.com/adhocore/gronx"
-	"github.com/cockroachdb/errors"
 )
 
-var OPEN_SANCTIONS_OUTDATED_DATASET_LEEWAY = 1 * time.Hour
+type SanctionCheckStatus int
 
-type OpenSanctionsUpstreamDataset struct {
-	Version    string    `json:"version"`
-	Name       string    `json:"name"`
-	LastExport time.Time `json:"last_export"`
-	Schedule   string    `json:"-"`
+const (
+	SanctionStatusConfirmedHit SanctionCheckStatus = iota
+	SanctionStatusInReview
+	SanctionStatusError
+	SanctionStatusUnknown
+)
+
+func SanctionCheckStatusFrom(s string) SanctionCheckStatus {
+	switch s {
+	case "confirmed_hit":
+		return SanctionStatusConfirmedHit
+	case "in_review":
+		return SanctionStatusInReview
+	case "error":
+		return SanctionStatusError
+	}
+
+	return SanctionStatusUnknown
 }
 
-type OpenSanctionCheckFilter map[string][]string
-
-type OpenSanctionsDataset struct {
-	Upstream OpenSanctionsUpstreamDataset `json:"upstream"`
-	Version  string                       `json:"version"`
-	UpToDate bool                         `json:"up_to_date"`
-
-	// TODO: this is not the date at which the data was pulled, it is when the data was published
-	LastExport time.Time `json:"-"`
-}
-
-type TimeProvider func() time.Time
-
-// CheckIsUpToDate marks a dataset as outdated if it was not updated in a
-// reasonable window of time after the upstream dataset was.
-//
-// Considering that for an upstream update time of T, the duration for which
-// we consider the dataset as "up to date" is:
-//
-//   - Grace Period = Time until the next scheduled update + Leeway
-//
-// For example, with a leeway of 1 hour, if a dataset is set to be pulled every
-// two hours, and the upstream dataset is updated at 7am, we will consider the
-// dataset as outdated if it is not updated at 9am.
-//
-//   - Outdated if (now() > Local Dataset Export Date + Grace Period)
-//
-// Since the upstream dataset is continuously updated, this rule alone is not
-// enough, so we also consider a dataset as oudated if the upstream export date
-// is after that of the local dataset + the update formula above.
-//
-//   - Outdated if (Local Dataset Export Date + Grace Period < Upstream Dataset Export Date)
-//
-// The local dataset is always considered up to date if its version matches
-// that of its upstream counterpart.
-func (dataset *OpenSanctionsDataset) CheckIsUpToDate(tp TimeProvider) error {
-	if dataset.Upstream.Version == dataset.Version {
-		(*dataset).UpToDate = true
-		return nil
+func (scs SanctionCheckStatus) String() string {
+	switch scs {
+	case SanctionStatusConfirmedHit:
+		return "confirmed_hit"
+	case SanctionStatusInReview:
+		return "in_review"
+	case SanctionStatusError:
+		return "error"
 	}
 
-	if !gronx.New().IsValid(dataset.Upstream.Schedule) {
-		return errors.New("could not parse dataset schedule")
-	}
-
-	// TODO: this check is not very relevant, since we do not have the date the data was pulled.
-	tickAfterLastUpdate, _ := gronx.NextTickAfter(dataset.Upstream.Schedule, dataset.LastExport, false)
-
-	if tickAfterLastUpdate.Add(OPEN_SANCTIONS_OUTDATED_DATASET_LEEWAY).Before(dataset.Upstream.LastExport) {
-		(*dataset).UpToDate = false
-		return nil
-	}
-
-	tickAfterLastChange, _ := gronx.NextTickAfter(dataset.Upstream.Schedule, dataset.Upstream.LastExport, false)
-
-	if tp().After(tickAfterLastChange.Add(OPEN_SANCTIONS_OUTDATED_DATASET_LEEWAY)) {
-		(*dataset).UpToDate = false
-		return nil
-	}
-
-	(*dataset).UpToDate = true
-
-	return nil
-}
-
-type OpenSanctionsQuery struct {
-	Queries   OpenSanctionCheckFilter
-	OrgConfig OrganizationOpenSanctionsConfig
+	return "unknown"
 }
 
 type SanctionCheck struct {
 	Id          string
 	DecisionId  string
-	Status      string
+	Status      SanctionCheckStatus
 	Query       json.RawMessage
 	OrgConfig   OrganizationOpenSanctionsConfig
 	IsManual    bool
@@ -117,6 +70,10 @@ type SanctionCheckMatchUpdate struct {
 	MatchId    string
 	ReviewerId UserId
 	Status     string
+}
+
+type SanctionCheckRefineRequest struct {
+	DecisionId string
 }
 
 type SanctionCheckMatchComment struct {
