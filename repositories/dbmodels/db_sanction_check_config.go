57bc04f2607dc8717a36dfef0fea1de308a2a373
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-29 16:16:20 +0100

    Implement query AST, with a new function, StringConcat.

--- repositories/dbmodels/db_sanction_check_config.go
+++ repositories/dbmodels/db_sanction_check_config.go
@@ -1,24 +1,36 @@
 package dbmodels
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 
+	"github.com/checkmarble/marble-backend/dto"
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/utils"
+	"github.com/pkg/errors"
 )
 
 const TABLE_SANCTION_CHECK_CONFIGS = "sanction_check_configs"
 
 type DBSanctionCheckConfigs struct {
-	Id                  string    `db:"id"`
-	ScenarioIterationId string    `db:"scenario_iteration_id"`
-	Enabled             bool      `db:"enabled"`
-	Datasets            []string  `db:"datasets"`
-	TriggerRule         []byte    `db:"trigger_rule"`
-	ForcedOutcome       *string   `db:"forced_outcome"`
-	ScoreModifier       int       `db:"score_modifier"`
-	UpdatedAt           time.Time `db:"updated_at"`
+	Id                  string                     `db:"id"`
+	ScenarioIterationId string                     `db:"scenario_iteration_id"`
+	Enabled             bool                       `db:"enabled"`
+	Datasets            []string                   `db:"datasets"`
+	TriggerRule         []byte                     `db:"trigger_rule"`
+	Query               DBSanctionCheckConfigQuery `db:"query"`
+	ForcedOutcome       *string                    `db:"forced_outcome"`
+	ScoreModifier       int                        `db:"score_modifier"`
+	UpdatedAt           time.Time                  `db:"updated_at"`
+}
+
+type DBSanctionCheckConfigQuery struct {
+	Name json.RawMessage `json:"name"`
+}
+
+type DBSanctionCheckConfigQueryInput struct {
+	Name dto.NodeDto `json:"name"`
 }
 
 var SanctionCheckConfigColumnList = utils.ColumnList[DBSanctionCheckConfigs]()
@@ -28,6 +40,11 @@ func AdaptSanctionCheckConfig(db DBSanctionCheckConfigs) (models.SanctionCheckCo
 	if err != nil {
 		return models.SanctionCheckConfig{}, fmt.Errorf(
 			"unable to unmarshal formula ast expression: %w", err)
+	}
+
+	query, err := AdaptSanctionCheckConfigQuery(db.Query)
+	if err != nil {
+		return models.SanctionCheckConfig{}, errors.Wrap(err, "unable to unmarshal query formula")
 	}
 
 	var forcedOutcome models.Outcome = models.UnsetForcedOutcome
@@ -40,6 +57,7 @@ func AdaptSanctionCheckConfig(db DBSanctionCheckConfigs) (models.SanctionCheckCo
 		Enabled:     db.Enabled,
 		Datasets:    db.Datasets,
 		TriggerRule: triggerRuleAst,
+		Query:       query,
 		Outcome: models.SanctionCheckOutcome{
 			ForceOutcome:  forcedOutcome,
 			ScoreModifier: db.ScoreModifier,
@@ -47,4 +65,17 @@ func AdaptSanctionCheckConfig(db DBSanctionCheckConfigs) (models.SanctionCheckCo
 	}
 
 	return scc, nil
+}
+
+func AdaptSanctionCheckConfigQuery(db DBSanctionCheckConfigQuery) (models.SanctionCheckConfigQuery, error) {
+	nameAst, err := AdaptSerializedAstExpression(db.Name)
+	if err != nil {
+		return models.SanctionCheckConfigQuery{}, err
+	}
+
+	model := models.SanctionCheckConfigQuery{
+		Name: *nameAst,
+	}
+
+	return model, nil
 }
---
+++ usecases/ast_eval/evaluate/eval_string_concat_test.go
@@ -0,0 +1,51 @@
+package evaluate
+
+import (
+	"context"
+	"testing"
+
+	"github.com/checkmarble/marble-backend/models/ast"
+	"github.com/checkmarble/marble-backend/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringConcat(t *testing.T) {
+	tts := []struct {
+		in            []any
+		out           string
+		withSeparator bool
+		separator     *string
+		error         bool
+	}{
+		{[]any{"abc", "def"}, "abcdef", false, nil, false},
+		{[]any{42, "abc", 12}, "42abc12", false, nil, false},
+		{[]any{42, "abc", 12}, "42 abc 12", true, nil, false},
+		{[]any{42, "abc", 12}, "42-abc-12", true, utils.Ptr("-"), false},
+		{[]any{42, "abc", true}, "42 abc 12", true, nil, true},
+	}
+
+	eval := StringConcat{}
+
+	for _, tt := range tts {
+		namedArgs := map[string]any{}
+
+		if tt.withSeparator {
+			namedArgs["with_separator"] = true
+		}
+		if tt.separator != nil {
+			namedArgs["separator"] = *tt.separator
+		}
+
+		result, err := eval.Evaluate(context.TODO(), ast.Arguments{
+			Args: tt.in, NamedArgs: namedArgs,
+		})
+
+		switch tt.error {
+		case true:
+			assert.NotEmpty(t, err)
+		default:
+			assert.Empty(t, err)
+			assert.Equal(t, tt.out, result)
+		}
+	}
+}
