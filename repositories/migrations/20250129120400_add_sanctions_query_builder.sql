1467b786c61f57b71a785f2febb1744b3b3af54e
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-01-30 09:28:46 +0100

    Add on delete cascade between sanction check configs and scenario iterations (for when drafts are deleted).

--- repositories/migrations/20250129120400_add_sanctions_query_builder.sql
+++ repositories/migrations/20250129120400_add_sanctions_query_builder.sql
@@ -2,6 +2,13 @@
 
 alter table sanction_check_configs add column query jsonb not null default '{}';
 
+alter table sanction_check_configs
+drop constraint fk_scenario_iteration,
+add constraint fk_scenario_iteration
+    foreign key (scenario_iteration_id)
+    references scenario_iterations (id)
+    on delete cascade;
+
 -- +goose Down
 
 alter table sanction_check_configs drop column query;
