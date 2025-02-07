00996bab4b0aaf9394eed6d7112221bce6f762f4
Pascal Delange <pascal@checkmarble.com>
2025-02-07 14:05:41 +0100

    format migrations
    add file

--- repositories/migrations/20250204103900_create_sanction_check_files.sql
+++ repositories/migrations/20250204103900_create_sanction_check_files.sql
@@ -1,18 +1,14 @@
 -- +goose Up
-
-create table sanction_check_files (
-    id uuid primary key default uuid_generate_v4(),
-    sanction_check_id uuid not null,
-    bucket_name text not null,
-    file_reference text not null,
-    file_name text,
-    created_at timestamp with time zone not null default now(),
-
-    constraint fk_sanction_check_match
-        foreign key (sanction_check_id)
-        references sanction_checks (id)
-);
+create table
+    sanction_check_files (
+        id uuid primary key default uuid_generate_v4 (),
+        sanction_check_id uuid not null,
+        bucket_name text not null,
+        file_reference text not null,
+        file_name text,
+        created_at timestamp with time zone not null default now(),
+        constraint fk_sanction_check_match foreign key (sanction_check_id) references sanction_checks (id)
+    );
 
 -- +goose Down
-
 drop table sanction_check_files;
