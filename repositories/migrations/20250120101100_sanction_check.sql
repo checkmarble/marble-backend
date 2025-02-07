a23f47cb9016f2090e51fb27517f7e61b9190666
Pascal Delange <pascal@checkmarble.com>
2025-02-07 14:05:41 +0100

    format migrations
    add file

--- repositories/migrations/20250120101100_sanction_check.sql
+++ repositories/migrations/20250120101100_sanction_check.sql
@@ -1,84 +1,65 @@
 -- +goose Up
 -- +goose StatementBegin
-
-create table sanction_checks (
-    id uuid default uuid_generate_v4(),
-    decision_id uuid not null,
-
-    status text check (status in ('confirmed_hit', 'no_hit', 'in_review', 'error')) default 'in_review',
-    search_input jsonb,
-    search_datasets text[],
-    search_threshold integer,
-    is_manual bool default false,
-    requested_by uuid null,
-    is_partial bool default false,
-
-    is_archived bool default false,
-    created_at timestamp with time zone default now(),
-    updated_at timestamp with time zone default now(),
-
-    primary key (id),
-    constraint fk_decision
-        foreign key (decision_id)
-        references decisions (id),
-    constraint fk_user
-        foreign key (requested_by)
-        references users (id)
-);
+create table
+    sanction_checks (
+        id uuid default uuid_generate_v4 (),
+        decision_id uuid not null,
+        status text check (status in ('confirmed_hit', 'no_hit', 'in_review', 'error')) default 'in_review',
+        search_input jsonb,
+        search_datasets text[],
+        search_threshold integer,
+        is_manual bool default false,
+        requested_by uuid null,
+        is_partial bool default false,
+        is_archived bool default false,
+        created_at timestamp with time zone default now(),
+        updated_at timestamp with time zone default now(),
+        primary key (id),
+        constraint fk_decision foreign key (decision_id) references decisions (id),
+        constraint fk_user foreign key (requested_by) references users (id)
+    );
 
 create index idx_sanction_checks_decision_id on sanction_checks (decision_id);
 
-create table sanction_check_matches (
-    id uuid default uuid_generate_v4(),
-    sanction_check_id uuid not null,
-
-    opensanction_entity_id text not null,
-    status text check (status in ('pending', 'confirmed_hit', 'no_hit')) default 'pending',
-    query_ids text[],
-    payload jsonb,
-    reviewed_by uuid,
-
-    created_at timestamp with time zone default now(),
-    updated_at timestamp with time zone default now(),
-
-    primary key (id),
-    constraint fk_sanction_check
-        foreign key (sanction_check_id)
-        references sanction_checks (id),
-    constraint fk_user
-        foreign key (reviewed_by)
-        references users (id)
-);
+create table
+    sanction_check_matches (
+        id uuid default uuid_generate_v4 (),
+        sanction_check_id uuid not null,
+        opensanction_entity_id text not null,
+        status text check (status in ('pending', 'confirmed_hit', 'no_hit')) default 'pending',
+        query_ids text[],
+        payload jsonb,
+        reviewed_by uuid,
+        created_at timestamp with time zone default now(),
+        updated_at timestamp with time zone default now(),
+        primary key (id),
+        constraint fk_sanction_check foreign key (sanction_check_id) references sanction_checks (id),
+        constraint fk_user foreign key (reviewed_by) references users (id)
+    );
 
 create index idx_sanction_check_matches_sanction_check_id on sanction_check_matches (sanction_check_id);
 
-create table sanction_check_match_comments (
-    id uuid default uuid_generate_v4(),
-    sanction_check_match_id uuid not null,
-
-    commented_by uuid not null,
-    comment text,
-
-    created_at timestamp with time zone default now(),
-
-    primary key (id),
-    constraint fk_sanction_check_match
-        foreign key (sanction_check_match_id)
-        references sanction_check_matches (id),
-    constraint fk_user
-        foreign key (commented_by)
-        references users (id)
-);
+create table
+    sanction_check_match_comments (
+        id uuid default uuid_generate_v4 (),
+        sanction_check_match_id uuid not null,
+        commented_by uuid not null,
+        comment text,
+        created_at timestamp with time zone default now(),
+        primary key (id),
+        constraint fk_sanction_check_match foreign key (sanction_check_match_id) references sanction_check_matches (id),
+        constraint fk_user foreign key (commented_by) references users (id)
+    );
 
 create index idx_sanction_check_match_comments_sanction_check_match_id on sanction_check_match_comments (sanction_check_match_id);
 
 -- +goose StatementEnd
-
 -- +goose Down
 -- +goose StatementBegin
-
 drop table sanction_checks;
+
 drop table sanction_check_matches;
+
 drop table sanction_check_match_comments;
 
 -- +goose StatementEnd
