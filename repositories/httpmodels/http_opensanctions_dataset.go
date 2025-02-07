7171a7edd3530d32c69eebb0c520aa6568633d2f
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-02-07 12:50:17 +0100

    Build the proper sectioned tree of sanction lists.

--- repositories/httpmodels/http_opensanctions_dataset.go
+++ repositories/httpmodels/http_opensanctions_dataset.go
@@ -1,21 +1,64 @@
 package httpmodels
 
 import (
+	"maps"
+	"slices"
+
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/pure_utils"
 )
+
+// TODO: determine which lists we want.
+var VALID_DATASETS = []string{"sanctions", "crime", "debarment", "securities", "regulatory", "peps"}
 
 type HTTPOpenSanctionCatalogResponse struct {
 	Datasets []HTTPOpenSanctionCatalogDataset `json:"datasets"`
 }
 
 type HTTPOpenSanctionCatalogDataset struct {
-	Name  string `json:"name"`
-	Title string `json:"title"`
+	Name     string   `json:"name"`
+	Title    string   `json:"title"`
+	Children []string `json:"children"`
 }
 
-func AdaptOpenSanctionCatalogDataset(dataset HTTPOpenSanctionCatalogDataset) models.OpenSanctionsCatalogDataset {
-	return models.OpenSanctionsCatalogDataset{
-		Name:  dataset.Name,
-		Title: dataset.Title,
+func AdaptOpenSanctionCatalog(datasets []HTTPOpenSanctionCatalogDataset) models.OpenSanctionsCatalog {
+	tmpDatasets := make(map[string]HTTPOpenSanctionCatalogDataset)
+	tmpSections := make([]*HTTPOpenSanctionCatalogDataset, 0)
+
+	sections := make(map[string]*models.OpenSanctionsCatalogSection)
+
+	for _, dataset := range datasets {
+		tmpDatasets[dataset.Name] = dataset
+
+		if slices.Contains(VALID_DATASETS, dataset.Name) && len(dataset.Children) > 0 {
+			section := models.OpenSanctionsCatalogSection{
+				Name:     dataset.Name,
+				Title:    dataset.Title,
+				Datasets: make([]models.OpenSanctionsCatalogDataset, 0),
+			}
+
+			sections[dataset.Name] = &section
+			tmpSections = append(tmpSections, &dataset)
+		}
+	}
+
+	for _, section := range tmpSections {
+		for _, child := range section.Children {
+			if dataset, ok := tmpDatasets[child]; ok {
+				sections[section.Name].Datasets = append(
+					sections[section.Name].Datasets, models.OpenSanctionsCatalogDataset{
+						Name:  dataset.Name,
+						Title: dataset.Title,
+					})
+			}
+		}
+	}
+
+	f := func(section *models.OpenSanctionsCatalogSection) models.OpenSanctionsCatalogSection {
+		return *section
+	}
+
+	return models.OpenSanctionsCatalog{
+		Sections: slices.Collect(maps.Values(pure_utils.MapValues(sections, f))),
 	}
 }
