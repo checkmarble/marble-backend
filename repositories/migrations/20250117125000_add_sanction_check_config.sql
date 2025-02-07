87ef29f7a12bdaff0679323d74d54a001af0b922
Pascal Delange <pascal@checkmarble.com>
2025-02-07 14:05:41 +0100

    format migrations
    add file

--- repositories/migrations/20250117125000_add_sanction_check_config.sql
+++ repositories/migrations/20250117125000_add_sanction_check_config.sql
@@ -1,26 +1,21 @@
 -- +goose Up
 -- +goose StatementBegin
-
-create table sanction_check_configs (
-    id uuid primary key default uuid_generate_v4(),
-    scenario_iteration_id uuid unique,
-    enabled boolean not null default true,
-    trigger_rule jsonb not null,
-    datasets text[] default array[]::text[],
-    forced_outcome text null check (forced_outcome in (NULL, 'review', 'block_and_review', 'decline')),
-    score_modifier int default 0,
-    updated_at timestamp with time zone not null default CURRENT_TIMESTAMP,
-
-    constraint fk_scenario_iteration
-        foreign key (scenario_iteration_id)
-        references scenario_iterations (id)
-);
+create table
+    sanction_check_configs (
+        id uuid primary key default uuid_generate_v4 (),
+        scenario_iteration_id uuid unique,
+        enabled boolean not null default true,
+        trigger_rule jsonb not null,
+        datasets text[] default array[]::text[],
+        forced_outcome text null check (forced_outcome in (NULL, 'review', 'block_and_review', 'decline')),
+        score_modifier int default 0,
+        updated_at timestamp with time zone not null default CURRENT_TIMESTAMP,
+        constraint fk_scenario_iteration foreign key (scenario_iteration_id) references scenario_iterations (id)
+    );
 
 -- +goose StatementEnd
-
 -- +goose Down
 -- +goose StatementBegin
-
 drop table sanction_check_configs;
 
 -- +goose StatementEnd
