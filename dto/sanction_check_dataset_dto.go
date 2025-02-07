5a839b914f1de694e9969bc0c2aedba89fdeec88
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-02-07 12:50:17 +0100

    Build the proper sectioned tree of sanction lists.

--- dto/sanction_check_dataset_dto.go
+++ dto/sanction_check_dataset_dto.go
@@ -6,16 +6,44 @@ import (
 	"github.com/checkmarble/marble-backend/models"
 )
 
+type OpenSanctionsCatalog struct {
+	Sections []OpenSanctionsCatalogSection `json:"sections"`
+}
+
+type OpenSanctionsCatalogSection struct {
+	Name     string                        `json:"name"`
+	Title    string                        `json:"title"`
+	Datasets []OpenSanctionsCatalogDataset `json:"datasets"`
+}
+
 type OpenSanctionsCatalogDataset struct {
 	Name  string `json:"name"`
 	Title string `json:"title"`
 }
 
-func AdaptOpenSanctionsDatalogDataset(model models.OpenSanctionsCatalogDataset) OpenSanctionsCatalogDataset {
-	return OpenSanctionsCatalogDataset{
-		Name:  model.Name,
-		Title: model.Title,
+func AdaptOpenSanctionsCatalog(model models.OpenSanctionsCatalog) OpenSanctionsCatalog {
+	catalog := OpenSanctionsCatalog{
+		Sections: make([]OpenSanctionsCatalogSection, len(model.Sections)),
 	}
+
+	for idx, s := range model.Sections {
+		section := OpenSanctionsCatalogSection{
+			Name:     s.Name,
+			Title:    s.Title,
+			Datasets: make([]OpenSanctionsCatalogDataset, len(s.Datasets)),
+		}
+
+		for idx, d := range s.Datasets {
+			section.Datasets[idx] = OpenSanctionsCatalogDataset{
+				Name:  d.Name,
+				Title: d.Title,
+			}
+		}
+
+		catalog.Sections[idx] = section
+	}
+
+	return catalog
 }
 
 type OpenSanctionsDatasetFreshness struct {
