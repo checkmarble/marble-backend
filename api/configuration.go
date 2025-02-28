d118c78fe7d49db7aa52568db52456dd0a6ff9d7
Pascal Delange <pascal@checkmarble.com>
2025-02-28 17:33:29 +0100

    some breaking changes on env var urls expected on the backend

--- api/configuration.go
+++ api/configuration.go
@@ -3,15 +3,15 @@ package api
 import "time"
 
 type Configuration struct {
-	Env                  string
-	AppName              string
-	Port                 string
-	MarbleAppHost        string
-	MarbleBackofficeHost string
-	RequestLoggingLevel  string
-	TokenLifetimeMinute  int
-	SegmentWriteKey      string
-	BatchTimeout         time.Duration
-	DecisionTimeout      time.Duration
-	DefaultTimeout       time.Duration
+	Env                 string
+	AppName             string
+	Port                string
+	MarbleAppUrl        string
+	MarbleBackofficeUrl string
+	RequestLoggingLevel string
+	TokenLifetimeMinute int
+	SegmentWriteKey     string
+	BatchTimeout        time.Duration
+	DecisionTimeout     time.Duration
+	DefaultTimeout      time.Duration
 }
