d8cc607fe7c1bf86fb6bc85aff7506ca3b002278
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-03-05 13:28:02 +0100

    Better set up of error reporting for public API.

--- pubapi/response.go
+++ pubapi/response.go
@@ -1,30 +1,20 @@
 package pubapi
 
 import (
-	"errors"
+	"io"
 	"net/http"
 
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/pure_utils"
+	"github.com/cockroachdb/errors"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
-
-	"github.com/go-playground/locales/en"
-	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
-	entrans "github.com/go-playground/validator/v10/translations/en"
+	"github.com/jackc/pgx/v5"
+
+	ut "github.com/go-playground/universal-translator"
 )
 
 var validationTranslator ut.Translator
-
-func init() {
-	if validator, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		en := en.New()
-		uni := ut.New(en, en)
-		validationTranslator, _ = uni.GetTranslator("en")
-		_ = entrans.RegisterDefaultTranslations(validator, validationTranslator)
-	}
-}
 
 type baseResponse[T any] struct {
 	Data  *T               `json:"data,omitempty"`
@@ -87,8 +77,43 @@ func (resp baseErrorResponse) WithError(err error) baseErrorResponse {
 		})
 
 	default:
-		resp.Error.err = err
-		resp.Error.Message = err.Error()
+		switch {
+		case errors.Is(err, io.EOF):
+			resp.Error.err = ErrMissingPayload
+			resp.Error.Message = ErrMissingPayload.Error()
+
+		case
+			errors.Is(err, models.NotFoundError),
+			errors.Is(err, pgx.ErrNoRows):
+
+			resp.Error.err = err
+			resp.Error.Message = "requested resource was not found"
+
+		case
+			errors.Is(err, ErrFeatureDisabled),
+			errors.Is(err, ErrNotConfigured),
+			errors.Is(err, ErrInvalidId),
+			errors.Is(err, ErrInvalidPayload),
+			errors.Is(err, ErrMissingPayload):
+
+			resp.Error.err = err
+			resp.Error.Message = err.Error()
+
+		case
+			errors.Is(err, models.UnprocessableEntityError),
+			errors.Is(err, ErrUnprocessableEntity):
+
+			resp.Error.status = http.StatusUnprocessableEntity
+			resp.Error.Code = ErrUnprocessableEntity.Error()
+
+		default:
+			resp.Error.err = err
+			resp.Error.Message = ErrInternalServerError.Error()
+
+			if message := errors.FlattenDetails(err); message != "" {
+				resp.Error.Message = message
+			}
+		}
 	}
 
 	return resp
@@ -126,11 +151,22 @@ func (resp baseErrorResponse) Serve(c *gin.Context, statuses ...int) {
 		switch {
 		case
 			errors.Is(resp.Error.err, ErrInvalidId),
-			errors.Is(resp.Error.err, ErrInvalidPayload):
+			errors.Is(resp.Error.err, ErrInvalidPayload),
+			errors.Is(resp.Error.err, models.BadParameterError):
 			status = http.StatusBadRequest
 
-		case errors.Is(resp.Error.err, models.NotFoundError):
+		case
+			errors.Is(resp.Error.err, models.UnAuthorizedError),
+			errors.Is(resp.Error.err, models.ForbiddenError):
+			status = http.StatusUnauthorized
+
+		case
+			errors.Is(resp.Error.err, models.NotFoundError),
+			errors.Is(resp.Error.err, pgx.ErrNoRows):
 			status = http.StatusNotFound
+
+		case errors.Is(resp.Error.err, models.ConflictError):
+			status = http.StatusConflict
 
 		case errors.Is(resp.Error.err, ErrFeatureDisabled):
 			status = http.StatusPaymentRequired
