b62c4b6964e5cc485a23a688cac0744e6cff201c
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-03-25 11:39:10 +0100

    Implement object annotations.

---
+++ repositories/migrations/20250325084800_create_entity_annotations.sql
@@ -0,0 +1,25 @@
+-- +goose Up
+
+create table entity_annotations (
+    id uuid default uuid_generate_v4(),
+    org_id uuid not null,
+    object_type text not null,
+    object_id text not null,
+    annotation_type text not null,
+    payload jsonb not null,
+    attached_by uuid,
+    created_at timestamp with time zone not null default now(),
+    deleted_at timestamp with time zone default null,
+
+    primary key (id),
+    foreign key (org_id) references organizations (id),
+    foreign key (attached_by) references users (id)
+);
+
+create index idx_entity_annotations
+    on entity_annotations (org_id, object_type, object_id, annotation_type)
+    where deleted_at is null;
+
+-- +goose Down
+
+drop table entity_annotations;
---
+++ usecases/entity_annotations_usecase.go
@@ -0,0 +1,166 @@
+package usecases
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+
+	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/repositories"
+	"github.com/checkmarble/marble-backend/usecases/executor_factory"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
+
+type EntityAnnotationRepository interface {
+	GetEntityAnnotationById(
+		ctx context.Context,
+		exec repositories.Executor,
+		req models.AnnotationByIdRequest,
+	) ([]models.EntityAnnotation, error)
+	GetEntityAnnotations(ctx context.Context, exec repositories.Executor,
+		req models.EntityAnnotationRequest) ([]models.EntityAnnotation, error)
+	CreateEntityAnnotation(ctx context.Context, exec repositories.Executor,
+		req models.CreateEntityAnnotationRequest) (models.EntityAnnotation, error)
+}
+
+type EntityAnnotationUsecase struct {
+	repository                 EntityAnnotationRepository
+	dataModelRepository        repositories.DataModelRepository
+	ingestedDataReadRepository repositories.IngestedDataReadRepository
+	blobRepository             repositories.BlobRepository
+	bucketUrl                  string
+
+	executorFactory    executor_factory.ExecutorFactory
+	transactionFactory executor_factory.TransactionFactory
+}
+
+func (uc EntityAnnotationUsecase) List(ctx context.Context, req models.EntityAnnotationRequest) ([]models.EntityAnnotation, error) {
+	if err := uc.checkObject(ctx, req.OrgId, req.ObjectType, req.ObjectId); err != nil {
+		return nil, errors.Wrap(models.NotFoundError, err.Error())
+	}
+
+	return uc.repository.GetEntityAnnotations(ctx, uc.executorFactory.NewExecutor(), req)
+}
+
+func (uc EntityAnnotationUsecase) Attach(ctx context.Context,
+	req models.CreateEntityAnnotationRequest,
+) (models.EntityAnnotation, error) {
+	if err := uc.checkObject(ctx, req.OrgId, req.ObjectType, req.ObjectId); err != nil {
+		return models.EntityAnnotation{}, errors.Wrap(models.NotFoundError, err.Error())
+	}
+
+	return uc.repository.CreateEntityAnnotation(ctx, uc.executorFactory.NewExecutor(), req)
+}
+
+func (uc EntityAnnotationUsecase) AttachFile(ctx context.Context,
+	req models.CreateEntityAnnotationRequest,
+	files []multipart.FileHeader,
+) (models.EntityAnnotation, error) {
+	if err := uc.checkObject(ctx, req.OrgId, req.ObjectType, req.ObjectId); err != nil {
+		return models.EntityAnnotation{}, errors.Wrap(models.NotFoundError, err.Error())
+	}
+
+	metadata := make([]models.EntityAnnotationFilePayloadFile, len(files))
+
+	for idx, file := range files {
+		key := fmt.Sprintf("annotations/%s/%s/%s", req.OrgId, req.ObjectType, uuid.NewString())
+
+		if err := uc.writeFileAnnotationToBlobStorage(ctx, file, key); err != nil {
+			return models.EntityAnnotation{}, err
+		}
+
+		metadata[idx] = models.EntityAnnotationFilePayloadFile{
+			Id:       uuid.NewString(),
+			Key:      key,
+			Filename: file.Filename,
+		}
+	}
+
+	fp, ok := req.Payload.(models.EntityAnnotationFilePayload)
+	if !ok {
+		return models.EntityAnnotation{}, errors.New("could not understand request")
+	}
+
+	req.Payload = models.EntityAnnotationFilePayload{
+		Caption: fp.Caption,
+		Bucket:  uc.bucketUrl,
+		Files:   metadata,
+	}
+
+	return uc.repository.CreateEntityAnnotation(ctx, uc.executorFactory.NewExecutor(), req)
+}
+
+func (uc EntityAnnotationUsecase) GetFileDownloadUrl(ctx context.Context,
+	req models.AnnotationByIdRequest, partId string,
+) (string, error) {
+	files, err := uc.repository.GetEntityAnnotationById(ctx, uc.executorFactory.NewExecutor(), req)
+	if err != nil {
+		return "", err
+	}
+	if len(files) != 1 {
+		return "", errors.New("requested file not found")
+	}
+
+	var fp models.EntityAnnotationFilePayload
+
+	if err := json.Unmarshal(files[0].Payload, &fp); err != nil {
+		return "", errors.New("could not parse stored annotation payload")
+	}
+
+	for _, part := range fp.Files {
+		if part.Id == partId {
+			return uc.blobRepository.GenerateSignedUrl(ctx, uc.bucketUrl, part.Key)
+		}
+	}
+
+	return "", errors.New("could not find requested file part")
+}
+
+func (uc EntityAnnotationUsecase) checkObject(ctx context.Context, orgId, objectType, objectId string) error {
+	dataModel, err := uc.dataModelRepository.GetDataModel(ctx, uc.executorFactory.NewExecutor(), orgId, false)
+	if err != nil {
+		return err
+	}
+
+	table, ok := dataModel.Tables[objectType]
+	if !ok {
+		return errors.New("unknown object type")
+	}
+
+	db, err := uc.executorFactory.NewClientDbExecutor(ctx, orgId)
+	if err != nil {
+		return err
+	}
+
+	if _, err := uc.ingestedDataReadRepository.QueryIngestedObject(ctx, db, table, objectId); err != nil {
+		return errors.New("unknown object")
+	}
+
+	return nil
+}
+
+func (uc EntityAnnotationUsecase) writeFileAnnotationToBlobStorage(ctx context.Context, file multipart.FileHeader, key string) error {
+	writer, err := uc.blobRepository.OpenStream(ctx, uc.bucketUrl, key, file.Filename)
+	if err != nil {
+		return err
+	}
+
+	defer writer.Close()
+
+	r, err := file.Open()
+	if err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(writer, r); err != nil {
+		return err
+	}
+	if err := writer.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
