8237f7720d8ab1fc4e351acc6b466cb3cb85111b
Pascal Delange <pascal@checkmarble.com>
2025-04-17 14:46:41 +0800

    chore: improve migrations by not making them create a postgres user

--- repositories/migrations/20241105143100_drop_analytics_schema.sql
+++ repositories/migrations/20241105143100_drop_analytics_schema.sql
@@ -15,28 +15,4 @@ begin
 end
 $$;
 
-DO $$ BEGIN
-      CREATE USER analytics;
-
-EXCEPTION
-      WHEN duplicate_object THEN RAISE NOTICE '%, skipping',
-      SQLERRM USING ERRCODE = SQLSTATE;
-
-END $$;
-
-GRANT
-SELECT
-      ON ALL TABLES IN SCHEMA analytics TO analytics;
-
-GRANT USAGE ON SCHEMA analytics TO analytics;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA analytics
-GRANT
-SELECT
-      ON TABLES TO analytics;
-
-ALTER ROLE analytics
-SET
-      search_path TO analytics;
-
 -- +goose StatementEnd
