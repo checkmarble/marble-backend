7e69031adeb30ae662771e55ca98fb6604a46684
Pascal Delange <pascal@checkmarble.com>
2025-04-19 10:02:08 +0800

    rework dto for client data with annotations

--- models/client_data_object.go
+++ models/client_data_object.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 )
 
@@ -15,7 +14,6 @@ type PivotObject struct {
 	IsIngested        bool
 	PivotObjectData   ClientObjectDetail
 	NumberOfDecisions int
-	Annotations       GroupedEntityAnnotations
 }
 
 // PivotType corresponds to the type of entity that is materialized by a pivot value.
@@ -56,40 +54,21 @@ func PivotTypeFromString(s string) PivotType {
 	}
 }
 
-// This struct is used as a DTO, but instead of using struct tags directly they are set on an anonymous struct below in the MarshalJSON method.
-// This is so we can set the RelatedObjects and Data to empty slices/maps if they are nil, and avoid return a null array/object.
 type ClientObjectDetail struct {
 	Metadata       ClientObjectMetadata
 	Data           map[string]any
 	RelatedObjects []RelatedObject
-}
-
-func (c ClientObjectDetail) MarshalJSON() ([]byte, error) {
-	if c.RelatedObjects == nil {
-		c.RelatedObjects = make([]RelatedObject, 0)
-	}
-	if c.Data == nil {
-		c.Data = make(map[string]any)
-	}
-	return json.Marshal(struct {
-		Metadata       ClientObjectMetadata `json:"metadata,omitzero"`
-		Data           map[string]any       `json:"data"`
-		RelatedObjects []RelatedObject      `json:"related_objects"`
-	}{
-		Metadata:       c.Metadata,
-		Data:           c.Data,
-		RelatedObjects: c.RelatedObjects,
-	})
+	Annotations    GroupedEntityAnnotations
 }
 
 type RelatedObject struct {
-	LinkName string             `json:"link_name"`
-	Detail   ClientObjectDetail `json:"related_object_detail"` //nolint:tagliatelle
+	LinkName string
+	Detail   ClientObjectDetail
 }
 
 type ClientObjectMetadata struct {
-	ValidFrom  time.Time `json:"valid_from"`
-	ObjectType string    `json:"object_type"`
+	ValidFrom  time.Time
+	ObjectType string
 }
 
 type StringOrNumber struct {
