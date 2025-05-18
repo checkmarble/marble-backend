9004d8cf093c67dd31a1ea3e73ac2583f5aedf0b
Pascal Delange <pascal@checkmarble.com>
2025-05-18 22:56:24 +0200

    [WIP] improve indexes to query decisions

---
+++ repositories/migrations/20250518225500_improve_indexes_on_decisions.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- +goose StatementBegin
+-- empty placeholder for migration, while I start writing the PR body
+select
+    1;
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+-- +goose StatementEnd
