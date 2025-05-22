6968e140de7f955f0c41e4e6f51a33077e88ccc2
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-05-22 11:13:21 +0200

    Reimplement batch executions endpoints for the V1 public API.

--- models/scheduled_scenario_executions.go
+++ models/scheduled_scenario_executions.go
@@ -13,6 +13,7 @@ type ScheduledExecution struct {
 	OrganizationId             string
 	ScenarioId                 string
 	ScenarioIterationId        string
+	ScenarioVersion            *int
 	Status                     ScheduledExecutionStatus
 	StartedAt                  time.Time
 	FinishedAt                 *time.Time
