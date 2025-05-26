aba3e1aa5a834de227d9726f83f572552f4a25f0
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-05-26 14:24:47 +0200

    Replace migration moving pg_trgm to check if we are extension owner.

--- repositories/migrations/20250424120000_add_fuzzystrmatch_extension.sql
+++ repositories/migrations/20250424120000_add_fuzzystrmatch_extension.sql
@@ -1,7 +1,29 @@
 -- +goose Up
+-- +goose StatementBegin
+
 CREATE EXTENSION if not exists fuzzystrmatch SCHEMA public;
-ALTER EXTENSION pg_trgm SET SCHEMA public;
+
+do $$
+declare
+  extension_name text := 'pg_trgm';
+begin
+  perform true
+  from pg_user pgu
+  inner join pg_extension pge on pge.extowner = pgu.usesysid
+  where
+    pgu.usename = current_user and
+    pge.extname = extension_name;
+
+  if found then
+    execute 'alter extension ' || quote_ident(extension_name) || ' set schema public';
+  else
+    raise notice 'WARN: could not install the %s extension into the public schema because we are not the owner', extension_name;
+  end if;
+end;
+$$ language plpgsql;
+
+-- +goose StatementEnd
 
 -- +goose Down
+
 DROP EXTENSION if exists fuzzystrmatch;
-ALTER EXTENSION pg_trgm SET SCHEMA marble;
