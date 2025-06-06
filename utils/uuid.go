9dcb5e9e41f411c032c7124d7bc319b7b8b2ae68
Pascal Delange <pascal@checkmarble.com>
2025-06-06 22:56:16 +0200

    move helper

--- utils/uuid.go
+++ utils/uuid.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/cockroachdb/errors"
 	"github.com/google/uuid"
 )
 
@@ -17,4 +18,16 @@ func ValidateUuid(uuidParam string) error {
 
 func ByteUuid(str string) [16]byte {
 	return [16]byte(uuid.MustParse(str))
+}
+
+func ParseSliceUUID(slice []string) ([]uuid.UUID, error) {
+	parsed := make([]uuid.UUID, len(slice))
+	for i, item := range slice {
+		parsedItem, err := uuid.Parse(item)
+		if err != nil {
+			return nil, errors.Wrap(models.BadParameterError, "failed to parse UUID in slice")
+		}
+		parsed[i] = parsedItem
+	}
+	return parsed, nil
 }
