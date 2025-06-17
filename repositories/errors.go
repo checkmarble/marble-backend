2a90ef1f2cc20ffc95fb5f2dd87ea82ad593edaf
Pascal Delange <pascal@checkmarble.com>
2025-06-17 14:42:40 +0200

    retry deadlocks, reorder & refactor case side effects

--- repositories/errors.go
+++ repositories/errors.go
@@ -10,3 +10,13 @@ func IsUniqueViolationError(err error) bool {
 	var pgxErr *pgconn.PgError
 	return errors.As(err, &pgxErr) && pgxErr.Code == pgerrcode.UniqueViolation
 }
+
+func IsDeadlockError(err error) bool {
+	var pgxErr *pgconn.PgError
+	return errors.As(err, &pgxErr) && pgxErr.Code == pgerrcode.DeadlockDetected
+}
+
+func IsSerializationFailureError(err error) bool {
+	var pgxErr *pgconn.PgError
+	return errors.As(err, &pgxErr) && pgxErr.Code == pgerrcode.SerializationFailure
+}
