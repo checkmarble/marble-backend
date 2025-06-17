06f44d586865a3bd24d4ffb6aa9ae880b4c13b29
Pascal Delange <pascal@checkmarble.com>
2025-06-17 22:41:53 +0200

    PR suggestions

---
+++ models/uuid.go
@@ -0,0 +1,20 @@
+package models
+
+import "github.com/google/uuid"
+
+type UnmarshallingUuid struct {
+	_uuid uuid.UUID
+}
+
+func (u *UnmarshallingUuid) Uuid() uuid.UUID {
+	return u._uuid
+}
+
+func (u *UnmarshallingUuid) UnmarshalParam(param string) error {
+	parsed, err := uuid.Parse(param)
+	if err != nil {
+		return err
+	}
+	u._uuid = parsed
+	return nil
+}
