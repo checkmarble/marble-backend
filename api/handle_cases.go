673e31733f18ce0ad1b5c1f0258327b84b6266d7
Pascal Delange <pascal@checkmarble.com>
2025-06-20 16:40:38 +0200

    minimal working example of query to vertex AI - factorize case data retrieval

--- api/handle_cases.go
+++ api/handle_cases.go
@@ -644,3 +644,18 @@ func handleGetCaseDataForCopilot(uc usecases.Usecases) func(c *gin.Context) {
 		c.Status(http.StatusOK)
 	}
 }
+
+func handleCreateCaseReview(uc usecases.Usecases) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+		caseId := c.Param("case_id")
+
+		usecase := usecasesWithCreds(ctx, uc).NewAiAgentUsecase()
+		review, err := usecase.CreateCaseReview(ctx, caseId)
+		if presentError(ctx, c, err) {
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{"review": review})
+	}
+}
