b2f03eee2a12ef86cd808fd579f3c25bcd7904e5
Léo JI <leo.ji@checkmarble.com>
2025-07-02 16:18:52 +0200

    feat(metrics): enhance metrics collection with frequency support

--- usecases/scheduled_execution/metrics_collection_job.go
+++ usecases/scheduled_execution/metrics_collection_job.go
@@ -17,10 +17,6 @@ import (
 // const METRIC_COLLECTION_WORKER_INTERVAL = 24 * time.Hour // Run daily
 const METRICS_COLLECTION_WORKER_INTERVAL = 10 * time.Second // Run every minute for testing
 
-type MetricsCollectionUsecase interface {
-	CollectMetrics(ctx context.Context) (models.MetricsPayload, error)
-}
-
 func NewMetricsCollectionPeriodicJob() *river.PeriodicJob {
 	return river.NewPeriodicJob(
 		river.PeriodicInterval(METRICS_COLLECTION_WORKER_INTERVAL),
@@ -33,7 +29,7 @@ func NewMetricsCollectionPeriodicJob() *river.PeriodicJob {
 				},
 			}
 		},
-		&river.PeriodicJobOpts{RunOnStart: true}, // Don't run immediately on startup
+		&river.PeriodicJobOpts{RunOnStart: false},
 	)
 }
 
@@ -57,39 +53,49 @@ func NewMetricCollectionWorker(
 	}
 }
 
+// Work executes the metrics collection job by collecting both global and organization-specific metrics
+// from a time range defined by watermarks, then updates the watermark to track progress
 func (w MetricCollectionWorker) Work(ctx context.Context, job *river.Job[models.MetricsCollectionArgs]) error {
 	logger := utils.LoggerFromContext(ctx)
-	logger.InfoContext(ctx, "Starting metrics collection job")
+	logger.DebugContext(ctx, "Starting metrics collection job")
+
+	now := time.Now()
+
+	// Take a watermark for the "from" time
+	// TODO: Get the from time from watermark or a default value if not exists (-> create a function for this)
+	from := time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC)
 
 	// Create the metric collection usecase
-	metrics, err := w.collectMetrics(ctx)
+	metrics, err := w.collectMetrics(ctx, from, now)
 	if err != nil {
 		logger.ErrorContext(ctx, "Failed to collect metrics", "error", err)
 		return err
 	}
 
-	// Debug: Print the collected metrics
-	logger.InfoContext(ctx, "Collected metrics", "metrics", metrics)
+	logger.DebugContext(ctx, "Collected metrics", "metrics", metrics)
+
+	// TODO: Update the watermarks with now value
+	logger.DebugContext(ctx, "Updating watermarks", "new_timestamp", now)
 
 	// TODO: Store or send the metrics somewhere
 	// For now, just log the number of metrics collected
-	logger.InfoContext(ctx, "Metric collection completed", "metrics_count", len(metrics.Metrics))
+	logger.DebugContext(ctx, "Metric collection completed", "metrics_count", len(metrics.Metrics))
 
 	return nil
 }
 
-func (w *MetricCollectionWorker) collectMetrics(ctx context.Context) (models.MetricsPayload, error) {
+func (w *MetricCollectionWorker) collectMetrics(ctx context.Context, from time.Time, to time.Time) (models.MetricsPayload, error) {
 	metrics := make([]models.MetricData, 0)
 
 	// Collect global metrics
-	globalMetrics, err := w.collectGlobalMetrics(ctx)
+	globalMetrics, err := w.collectGlobalMetrics(ctx, from, to)
 	if err != nil {
 		return models.MetricsPayload{}, err
 	}
 	metrics = slices.Concat(metrics, globalMetrics)
 
 	// Collect organization-specific metrics
-	orgMetrics, err := w.collectOrganizationMetrics(ctx)
+	orgMetrics, err := w.collectOrganizationMetrics(ctx, from, to)
 	if err != nil {
 		return models.MetricsPayload{}, err
 	}
@@ -105,13 +111,17 @@ func (w *MetricCollectionWorker) collectMetrics(ctx context.Context) (models.Met
 	return payload, nil
 }
 
-func (w *MetricCollectionWorker) collectGlobalMetrics(ctx context.Context) ([]models.MetricData, error) {
+// Collects global metrics from all collectors
+// If a collector fails, it will log a warning and continue to the next collector (don't fail the whole function)
+func (w *MetricCollectionWorker) collectGlobalMetrics(ctx context.Context, from time.Time, to time.Time) ([]models.MetricData, error) {
 	metrics := make([]models.MetricData, 0)
+	logger := utils.LoggerFromContext(ctx)
 
 	for _, collector := range w.collectors.GetGlobalCollectors() {
-		value, err := collector.Collect(ctx)
+		value, err := collector.Collect(ctx, from, to)
 		if err != nil {
-			return []models.MetricData{}, err
+			logger.WarnContext(ctx, "Failed to collect global metrics", "error", err)
+			continue
 		}
 		metrics = slices.Concat(metrics, value)
 	}
@@ -119,8 +129,11 @@ func (w *MetricCollectionWorker) collectGlobalMetrics(ctx context.Context) ([]mo
 	return metrics, nil
 }
 
-func (w *MetricCollectionWorker) collectOrganizationMetrics(ctx context.Context) ([]models.MetricData, error) {
+// Collects organization metrics from all collectors, fetching all organizations from the database first
+// If a collector fails, it will log a warning and continue to the next collector (don't fail the whole function)
+func (w *MetricCollectionWorker) collectOrganizationMetrics(ctx context.Context, from time.Time, to time.Time) ([]models.MetricData, error) {
 	metrics := make([]models.MetricData, 0)
+	logger := utils.LoggerFromContext(ctx)
 
 	orgs, err := w.getListOfOrganizations(ctx)
 	if err != nil {
@@ -129,9 +142,10 @@ func (w *MetricCollectionWorker) collectOrganizationMetrics(ctx context.Context)
 
 	for _, org := range orgs {
 		for _, collector := range w.collectors.GetCollectors() {
-			value, err := collector.Collect(ctx, org.Id)
+			value, err := collector.Collect(ctx, org.Id, from, to)
 			if err != nil {
-				return []models.MetricData{}, err
+				logger.WarnContext(ctx, "Failed to collect organization metrics", "error", err)
+				continue
 			}
 			metrics = slices.Concat(metrics, value)
 		}
@@ -140,6 +154,8 @@ func (w *MetricCollectionWorker) collectOrganizationMetrics(ctx context.Context)
 	return metrics, nil
 }
 
+// Fetches all organizations from the database
+// NOTE: Add caching to avoid fetching the same organizations every time (but how can we invalidate the cache?)
 func (w *MetricCollectionWorker) getListOfOrganizations(ctx context.Context) ([]models.Organization, error) {
 	orgs, err := w.organizationRepository.AllOrganizations(ctx, w.executorFactory.NewExecutor())
 	if err != nil {
