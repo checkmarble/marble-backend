656935d5895937c885d35984bd57e2fb7c4e5123
Léo JI <leo.ji@checkmarble.com>
2025-07-03 16:26:12 +0200

    refactor(watermark): rename OffloadingWatermark to Watermark and update related methods for improved clarity and consistency

--- repositories/decisions_repository.go
+++ repositories/decisions_repository.go
@@ -980,10 +980,15 @@ func (repo *MarbleDbRepository) GetOffloadableDecisionRules(ctx context.Context,
 	}
 
 	if req.Watermark == nil {
-		req.Watermark = &models.OffloadingWatermark{
+		req.Watermark = &models.Watermark{
 			WatermarkTime: time.Time{},
-			WatermarkId:   uuid.UUID{}.String(),
+			WatermarkId:   utils.Ptr(uuid.UUID{}.String()),
 		}
+	}
+
+	// HELP: Not sure about raising the error here
+	if req.Watermark.WatermarkId == nil {
+		return nil, errors.New("watermark id is required")
 	}
 
 	sql := NewQueryBuilder().
@@ -1003,7 +1008,7 @@ func (repo *MarbleDbRepository) GetOffloadableDecisionRules(ctx context.Context,
 				Where(squirrel.And{
 					squirrel.Eq{"org_id": req.OrgId},
 					squirrel.Lt{"created_at": req.DeleteBefore},
-					squirrel.Expr("(created_at, id) > (?, ?)", req.Watermark.WatermarkTime, req.Watermark.WatermarkId),
+					squirrel.Expr("(created_at, id) > (?, ?)", req.Watermark.WatermarkTime, *req.Watermark.WatermarkId),
 				}).
 				OrderBy("created_at, id").
 				Limit(uint64(req.BatchSize)),
