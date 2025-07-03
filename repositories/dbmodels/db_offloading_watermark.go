cc811ef5770aba7e9fcb99cc668e296e74894ecb
Léo JI <leo.ji@checkmarble.com>
2025-07-03 16:26:12 +0200

    refactor(watermark): rename OffloadingWatermark to Watermark and update related methods for improved clarity and consistency

--- repositories/dbmodels/db_offloading_watermark.go
+++ repositories/dbmodels/db_offloading_watermark.go
@@ -1,32 +1,35 @@
 package dbmodels
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/utils"
 )
 
-type DbOffloadingWatermark struct {
-	OrgId         string    `db:"org_id"`
-	TableName     string    `db:"table_name"`
-	WatermarkTime time.Time `db:"watermark_time"`
-	WatermarkId   string    `db:"watermark_id"`
-	CreatedAt     time.Time `db:"created_at"`
-	UpdatedAt     time.Time `db:"updated_at"`
+type DbWatermark struct {
+	OrgId         *string          `db:"org_id"`
+	Type          string           `db:"type"`
+	WatermarkTime time.Time        `db:"watermark_time"`
+	WatermarkId   *string          `db:"watermark_id"`
+	CreatedAt     time.Time        `db:"created_at"` //nolint:tagliatelle
+	UpdatedAt     time.Time        `db:"updated_at"`
+	Params        *json.RawMessage `db:"params"`
 }
 
-const TABLE_OFFLOADING_WATERMARKS = "offloading_watermarks"
+const TABLE_WATERMARKS = "watermarks"
 
-var SelectOffloadingWatermarkColumn = utils.ColumnList[DbOffloadingWatermark]()
+var SelectWatermarkColumn = utils.ColumnList[DbWatermark]()
 
-func AdaptOffloadingWatermark(db DbOffloadingWatermark) (models.OffloadingWatermark, error) {
-	return models.OffloadingWatermark{
+func AdaptWatermark(db DbWatermark) (models.Watermark, error) {
+	return models.Watermark{
 		OrgId:         db.OrgId,
-		TableName:     db.TableName,
+		Type:          models.WatermarkType(db.Type),
 		WatermarkTime: db.WatermarkTime,
 		WatermarkId:   db.WatermarkId,
 		CreatedAt:     db.CreatedAt,
 		UpdatedAt:     db.UpdatedAt,
+		Params:        db.Params,
 	}, nil
 }
