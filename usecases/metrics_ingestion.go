b68d31ec697c719ad3a7e2152acdfc19e59092d1
Léo JI <leo.ji@checkmarble.com>
2025-07-07 12:02:44 +0200

    refacto(metrics_ingestion): remove ingection checking, let storage to do that

--- usecases/metrics_ingestion.go
+++ usecases/metrics_ingestion.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/checkmarble/marble-backend/utils"
-	"github.com/google/uuid"
 )
 
 type MetricsIngestionUsecase struct{}
@@ -15,30 +14,13 @@ func NewMetricsIngestionUsecase() MetricsIngestionUsecase {
 }
 
 func (u *MetricsIngestionUsecase) IngestMetrics(ctx context.Context, metrics models.MetricsCollection) error {
-	logger := utils.LoggerFromContext(ctx)
-
-	// Check if the collection is already ingested
-	alreadyIngested, err := u.isCollectionAlreadyIngested(ctx, metrics.CollectionID)
-	if err != nil {
-		return err
-	}
-	if alreadyIngested {
-		logger.InfoContext(ctx, "Collection already ingested", "collection_id", metrics.CollectionID)
-		return nil
-	}
-
 	// Ingest the collection
-	err = u.ingestCollection(ctx, metrics)
+	err := u.ingestCollection(ctx, metrics)
 	if err != nil {
 		return err
 	}
 
 	return nil
-}
-
-func (u *MetricsIngestionUsecase) isCollectionAlreadyIngested(_ context.Context, _ uuid.UUID) (bool, error) {
-	// TODO: Implement the logic to check if the collection is already ingested
-	return false, nil
 }
 
 func (u *MetricsIngestionUsecase) ingestCollection(ctx context.Context, collection models.MetricsCollection) error {
