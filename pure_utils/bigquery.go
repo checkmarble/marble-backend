1b4f6e9415733a4e43c23cf67fdd29c219a66dd5
Léo JI <leo.ji@checkmarble.com>
2025-07-10 11:25:54 +0200

    refactor(metrics_collection): update utility functions for BigQuery compatibility in metrics adaptation

--- pure_utils/bigquery.go
+++ pure_utils/bigquery.go
@@ -1,23 +1,27 @@
+// Package pure_utils provides utility functions for various data transformations.
+// Functions prefixed with "BQ" are specifically for Google Cloud BigQuery type conversions.
 package pure_utils
 
 import "cloud.google.com/go/bigquery"
 
-func NullStringFromPtr(ptr *string) bigquery.NullString {
+// BQNullStringFromPtr converts a string pointer to a BigQuery NullString.
+// Returns a valid NullString if ptr is not nil, otherwise returns an invalid NullString.
+func BQNullStringFromPtr(ptr *string) bigquery.NullString {
 	value := bigquery.NullString{}
 	if ptr != nil {
 		value.StringVal = *ptr
 		value.Valid = true
 	}
-
 	return value
 }
 
-func NullFloat64FromPtr(ptr *float64) bigquery.NullFloat64 {
+// BQNullFloat64FromPtr converts a float64 pointer to a BigQuery NullFloat64.
+// Returns a valid NullFloat64 if ptr is not nil, otherwise returns an invalid NullFloat64.
+func BQNullFloat64FromPtr(ptr *float64) bigquery.NullFloat64 {
 	value := bigquery.NullFloat64{}
 	if ptr != nil {
 		value.Float64 = *ptr
 		value.Valid = true
 	}
-
 	return value
 }
