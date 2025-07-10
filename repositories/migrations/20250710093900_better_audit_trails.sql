4b5faa2923e97a2f0b57dd0f445ebd5a57bae238
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-07-10 15:24:53 +0200

    Add previous value on UPDATES.

--- repositories/migrations/20250710093900_better_audit_trails.sql
+++ repositories/migrations/20250710093900_better_audit_trails.sql
@@ -3,7 +3,8 @@
 
 alter table "audit"."audit_events"
     add column if not exists org_id uuid,
-    add column if not exists api_key_id uuid;
+    add column if not exists api_key_id uuid,
+    add column if not exists previous_data jsonb;
 
 update "audit"."audit_events" events
 set org_id = sq.organization_id
@@ -20,8 +21,8 @@ begin
         insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "created_at")
         values ('DELETE', current_setting('custom.current_org_id', true)::uuid, current_setting('custom.current_user_id', true), current_setting('custom.current_api_key_id', true)::uuid, TG_TABLE_NAME, old.id, to_jsonb(OLD), now());
     elsif (TG_OP = 'UPDATE') then
-        insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "created_at")
-        values ('UPDATE', current_setting('custom.current_org_id', true)::uuid, current_setting('custom.current_user_id', true), current_setting('custom.current_api_key_id', true)::uuid, TG_TABLE_NAME, new.id, to_jsonb(NEW), now());
+        insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "previous_data", "created_at")
+        values ('UPDATE', current_setting('custom.current_org_id', true)::uuid, current_setting('custom.current_user_id', true), current_setting('custom.current_api_key_id', true)::uuid, TG_TABLE_NAME, new.id, to_jsonb(NEW), to_jsonb(OLD), now());
     elsif (TG_OP = 'INSERT') then
         insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "created_at")
         values ('INSERT', current_setting('custom.current_org_id', true)::uuid, current_setting('custom.current_user_id', true), current_setting('custom.current_api_key_id', true)::uuid, TG_TABLE_NAME, new.id, to_jsonb(NEW), now());
