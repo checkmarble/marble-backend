33bc33f318a7ab3e79ea1858af1323551f280103
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-07-22 18:18:51 +0200

    Implement case auto-assignment algorithm across all inboxes.

--- repositories/auto_assignment_repository.go
+++ repositories/auto_assignment_repository.go
@@ -5,12 +5,55 @@ import (
 
 	"github.com/Masterminds/squirrel"
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/pure_utils"
 	"github.com/checkmarble/marble-backend/repositories/dbmodels"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 )
 
-func (repo *MarbleDbRepository) FindAutoAssignableUsers(ctx context.Context, exec Executor, orgId string, inboxId uuid.UUID, limit int) ([]models.UserWithCaseCount, error) {
+func (repo *MarbleDbRepository) FindAutoAssignableUsers(ctx context.Context, exec Executor, orgId string, limit int) ([]models.UserWithCaseCount, error) {
+	sql := `
+		select u.*, count(distinct c.id) as case_count
+		from inbox_users iu
+		inner join users u on u.id = iu.user_id
+		left join cases c on c.assigned_to = u.id
+		where
+		  auto_assignable = true and
+		  not exists (
+		    select 1
+		    from user_unavailabilities uu
+		    where
+				uu.org_id = $1 and
+				uu.user_id = iu.user_id and
+				now() between uu.from_date and uu.until_date
+	  	  )
+		group by u.id
+		having count(distinct c.id) < $2
+		order by case_count
+	`
+
+	rows, err := exec.Query(ctx, sql, orgId, limit)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	dbUsers, err := pgx.CollectRows(rows, pgx.RowToStructByName[dbmodels.DbAssignableUserWithCaseCount])
+	if err != nil {
+		return nil, err
+	}
+
+	users, err := pure_utils.MapErr(dbUsers, dbmodels.AdaptAssignableUserWithCaseCount)
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (repo *MarbleDbRepository) FindNextAutoAssignableUserForInbox(ctx context.Context, exec Executor, orgId string, inboxId uuid.UUID, limit int) (*models.User, error) {
 	sql := `
 		select u.*, count(*) filter (where c.id is not null) as case_count
 		from inbox_users iu
@@ -29,51 +72,40 @@ func (repo *MarbleDbRepository) FindAutoAssignableUsers(ctx context.Context, exe
 	  	  )
 		group by u.id
 		having count(*) filter (where c.id is not null) < $3
-		order by case_count
+		order by case_count, id asc
+		limit 1
 	`
 
 	rows, err := exec.Query(ctx, sql, inboxId, orgId, limit)
-
 	if err != nil {
 		return nil, err
 	}
 
-	defer rows.Close()
-
-	users := make([]models.UserWithCaseCount, 0)
-
-	for rows.Next() {
-		dbUser, err := pgx.RowToStructByName[dbmodels.DbAssignableUserWithCaseCount](rows)
-		if err != nil {
-			return nil, err
-		}
-
-		user, err := dbmodels.AdaptAssignableUserWithCaseCount(dbUser)
-		if err != nil {
-			return nil, err
-		}
-
-		users = append(users, user)
-	}
-
-	if err := rows.Err(); err != nil {
+	row, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[dbmodels.DbAssignableUserWithCaseCount])
+	if err != nil {
 		return nil, err
 	}
 
-	return users, nil
+	user, err := dbmodels.AdaptAssignableUserWithCaseCount(row)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user.User, nil
 }
 
-func (repo *MarbleDbRepository) FindAssignableCases(ctx context.Context, exec Executor, inboxId uuid.UUID, limit int) ([]models.Case, error) {
+func (repo *MarbleDbRepository) FindAutoAssignableCases(ctx context.Context, exec Executor, limit int) ([]models.Case, error) {
 	sql := NewQueryBuilder().
-		Select(dbmodels.SelectCaseColumn...).
-		From(dbmodels.TABLE_CASES).
+		Select(columnsNames("c", dbmodels.SelectCaseColumn)...).
+		From(dbmodels.TABLE_CASES+" c").
+		InnerJoin(dbmodels.TABLE_INBOXES+" i on i.id = c.inbox_id").
 		Where(squirrel.Eq{
-			"inbox_id":      inboxId,
-			"assigned_to":   nil,
-			"snoozed_until": nil,
+			"i.auto_assign_enabled": true,
+			"c.assigned_to":         nil,
+			"c.snoozed_until":       nil,
 		}).
-		Where("status != ?", models.CaseClosed).
-		OrderBy("created_at").
+		Where("c.status != ?", models.CaseClosed).
+		OrderBy("c.created_at").
 		Limit(uint64(limit))
 
 	return SqlToListOfModels(ctx, exec, sql, dbmodels.AdaptCase)
