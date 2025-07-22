fcdd6b8858ffa8b9ac00a92b69ad20df8b0ef833
Pascal Delange <pascal@checkmarble.com>
2025-07-22 11:55:15 +0200

    routes to get existing case reviews, enqueue a new review, and create a new one sychronously

--- api/routes.go
+++ api/routes.go
@@ -232,7 +232,9 @@ func addRoutes(r *gin.Engine, conf Configuration, uc usecases.Usecases, auth uti
 	router.POST("/cases/:case_id/escalate", tom, handleEscalateCase(uc))
 
 	router.GET("/cases/:case_id/data_for_investigation", timeoutMiddleware(conf.BatchTimeout), handleGetCaseDataForCopilot(uc))
-	router.POST("cases/:case_id/review", timeoutMiddleware(conf.BatchTimeout), handleCreateCaseReview(uc))
+	router.POST("/cases/:case_id/review", timeoutMiddleware(conf.BatchTimeout), handleCreateCaseReview(uc))
+	router.GET("/cases/:case_id/review", tom, handleGetCaseReview(uc))
+	router.POST("/cases/:case_id/review/enqueue", tom, handleEnqueueCaseReview(uc))
 
 	router.GET("/inboxes/:inbox_id", tom, handleGetInboxById(uc))
 	router.GET("/inboxes/:inbox_id/metadata", tom, handleGetInboxMetadataById(uc))
