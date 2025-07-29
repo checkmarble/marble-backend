b4cf07adafce363fc3ed7800614ae3507b51f0ea
Léo JI <leo.ji@checkmarble.com>
2025-07-29 15:08:57 +0200

    remove migration

--- repositories/migrations/20250721090708_create_index_screening_org_id_created_at.sql
+++
@@ -1,6 +0,0 @@
--- +goose NO TRANSACTION
--- +goose Up
-CREATE INDEX CONCURRENTLY sanction_checks_created_at_idx ON sanction_checks (created_at DESC);
-
--- +goose Down
-DROP INDEX CONCURRENTLY sanction_checks_created_at_idx;
