a2f43459fa162354df05fd6f92a00d41bc3cec0b
leo-marble <leo.ji@checkmarble.com>
2025-08-11 16:21:34 +0200

    Update 20250808122214_add_index_created_at_ai_case_review.sql
    Add concurrently when creating the index

--- repositories/migrations/20250808122214_add_index_created_at_ai_case_review.sql
+++ repositories/migrations/20250808122214_add_index_created_at_ai_case_review.sql
@@ -1,6 +1,6 @@
 -- +goose NO TRANSACTION
 -- +goose Up
-CREATE INDEX idx_ai_case_reviews_created_at ON ai_case_reviews (created_at DESC);
+CREATE INDEX concurrently idx_ai_case_reviews_created_at ON ai_case_reviews (created_at DESC);
 
 -- +goose Down
 DROP INDEX idx_ai_case_reviews_created_at;
