665b0e593d386d3917652024a036a5bf50fcf722
Pascal Delange <pascal@checkmarble.com>
2025-09-05 14:30:34 +0200

    simplify case listing query

--- repositories/case_repository.go
+++ repositories/case_repository.go
@@ -24,16 +24,61 @@ func (repo *MarbleDbRepository) ListOrganizationCases(
 		return nil, err
 	}
 
-	orderCond := orderConditionForCases(pagination)
+	orderCondition := fmt.Sprintf("c.boost is null %[1]s, c.%[2]s %[1]s, c.id %[1]s", pagination.Order, pagination.Sorting)
 
-	coreQuery := casesCoreQueryWithRank(pagination)
-	filteredCoreQuery := applyCaseFilters(coreQuery, filters)
-
-	subquery := squirrel.StatementBuilder.
-		Select(casesWithRankColumns()...).
-		FromSelect(filteredCoreQuery, "s").
+	query := NewQueryBuilder().
+		Select(dbmodels.SelectCaseColumn...).
+		Column(
+			fmt.Sprintf(
+				"(SELECT array_agg(row(%s) ORDER BY cc.created_at) AS contributors FROM %s AS cc WHERE cc.case_id=c.id)",
+				strings.Join(columnsNames("cc", dbmodels.SelectCaseContributorColumn), ","),
+				dbmodels.TABLE_CASE_CONTRIBUTORS,
+			),
+		).
+		Column(
+			fmt.Sprintf(
+				"(SELECT array_agg(row(%s) ORDER BY ct.created_at) AS tags FROM %s AS ct WHERE ct.case_id=c.id AND ct.deleted_at IS NULL)",
+				strings.Join(columnsNames("ct", dbmodels.SelectCaseTagColumn), ","),
+				dbmodels.TABLE_CASE_TAGS,
+			),
+		).
+		Column(fmt.Sprintf("(SELECT count(distinct d.id) FROM %s AS d WHERE d.case_id = c.id AND d.org_id=c.org_id) AS decisions_count", dbmodels.TABLE_DECISIONS)).
+		From(dbmodels.TABLE_CASES + " AS c").
+		OrderBy(orderCondition).
 		Limit(uint64(pagination.Limit))
 
+		// Apply filters
+	query = query.Where(squirrel.Eq{"c.org_id": filters.OrganizationId})
+
+	if len(filters.Statuses) > 0 {
+		query = query.Where(squirrel.Eq{"c.status": filters.Statuses})
+	}
+	if !filters.StartDate.IsZero() {
+		query = query.Where(squirrel.GtOrEq{"c.created_at": filters.StartDate})
+	}
+	if !filters.EndDate.IsZero() {
+		query = query.Where(squirrel.LtOrEq{"c.created_at": filters.EndDate})
+	}
+	if len(filters.InboxIds) > 0 {
+		query = query.Where(squirrel.Eq{"c.inbox_id": filters.InboxIds})
+	}
+	if filters.Name != "" {
+		query = query.Where("c.name % ?", filters.Name)
+	}
+	if !filters.IncludeSnoozed {
+		query = query.Where(squirrel.Or{
+			squirrel.Eq{"snoozed_until": nil},
+			squirrel.LtOrEq{"snoozed_until": time.Now()},
+		})
+	}
+	if filters.ExcludeAssigned {
+		query = query.Where(squirrel.Eq{"assigned_to": nil})
+	}
+	if filters.AssigneeId != "" {
+		query = query.Where(squirrel.Eq{"assigned_to": filters.AssigneeId})
+	}
+
+	// Apply pagination, by fetching the offset case (error if not found)
 	var offsetCase models.Case
 	if pagination.OffsetId != "" {
 		var err error
@@ -45,20 +90,47 @@ func (repo *MarbleDbRepository) ListOrganizationCases(
 			return []models.Case{}, errors.Wrap(err, "Error fetching offset case")
 		}
 	}
-
-	paginatedSubquery, err := applyCasesPagination(subquery, pagination, offsetCase)
+	var err error
+	query, err = applyCasesPagination(query, pagination, offsetCase)
 	if err != nil {
 		return []models.Case{}, err
 	}
-	queryWithJoinedFields := selectCasesWithJoinedFields(paginatedSubquery, orderCond)
 
-	return SqlToListOfRow(ctx, exec, queryWithJoinedFields, func(row pgx.CollectableRow) (models.Case, error) {
+	// Then, fetch the cases
+	return SqlToListOfRow(ctx, exec, query, func(row pgx.CollectableRow) (models.Case, error) {
 		db, err := pgx.RowToStructByPos[dbmodels.DBCaseWithContributorsAndTags](row)
 		if err != nil {
 			return models.Case{}, err
 		}
 		return dbmodels.AdaptCaseWithContributorsAndTags(db)
 	})
+}
+
+func applyCasesPagination(query squirrel.SelectBuilder, p models.PaginationAndSorting, offsetCase models.Case) (squirrel.SelectBuilder, error) {
+	if p.OffsetId == "" {
+		return query, nil
+	}
+
+	var offsetFieldVal any
+	switch p.Sorting {
+	case models.CasesSortingCreatedAt:
+		offsetFieldVal = offsetCase.CreatedAt
+	default:
+		// only pagination by created_at is allowed for now
+		return query, fmt.Errorf("invalid sorting field: %w", models.BadParameterError)
+	}
+
+	queryConditionBefore := fmt.Sprintf("(c.boost IS NULL, c.%s, c.id) < (?, ?, ?)", p.Sorting)
+	queryConditionAfter := fmt.Sprintf("(c.boost IS NULL, c.%s, c.id) > (?, ?, ?)", p.Sorting)
+	args := []any{offsetCase.Boost == nil, offsetFieldVal, p.OffsetId}
+
+	if p.Order == models.SortingOrderDesc {
+		query = query.Where(queryConditionBefore, args...)
+	} else {
+		query = query.Where(queryConditionAfter, args...)
+	}
+
+	return query, nil
 }
 
 func (repo *MarbleDbRepository) GetCaseById(ctx context.Context, exec Executor, caseId string) (models.Case, error) {
@@ -291,106 +363,6 @@ func (repo *MarbleDbRepository) SoftDeleteCaseTag(ctx context.Context, exec Exec
 	return err
 }
 
-func casesWithRankColumns() []string {
-	var columnAlias []string
-	columnAlias = append(columnAlias, dbmodels.SelectCaseColumn...)
-
-	columns := columnsNames("s", columnAlias)
-	return columns
-}
-
-func casesCoreQueryWithRank(pagination models.PaginationAndSorting) squirrel.SelectBuilder {
-	orderCondition := fmt.Sprintf("c.boost is null %[1]s, c.%[2]s %[1]s, c.id %[1]s", pagination.Order, pagination.Sorting)
-
-	return squirrel.StatementBuilder.
-		Select(dbmodels.SelectCaseColumn...).
-		From(dbmodels.TABLE_CASES + " AS c").
-		OrderBy(orderCondition)
-}
-
-func applyCaseFilters(query squirrel.SelectBuilder, filters models.CaseFilters) squirrel.SelectBuilder {
-	query = query.Where(squirrel.Eq{"c.org_id": filters.OrganizationId})
-
-	if len(filters.Statuses) > 0 {
-		query = query.Where(squirrel.Eq{"c.status": filters.Statuses})
-	}
-	if !filters.StartDate.IsZero() {
-		query = query.Where(squirrel.GtOrEq{"c.created_at": filters.StartDate})
-	}
-	if !filters.EndDate.IsZero() {
-		query = query.Where(squirrel.LtOrEq{"c.created_at": filters.EndDate})
-	}
-	if len(filters.InboxIds) > 0 {
-		query = query.Where(squirrel.Eq{"c.inbox_id": filters.InboxIds})
-	}
-	if filters.Name != "" {
-		query = query.Where("c.name % ?", filters.Name)
-	}
-	if !filters.IncludeSnoozed {
-		query = query.Where(squirrel.Or{
-			squirrel.Eq{"snoozed_until": nil},
-			squirrel.LtOrEq{"snoozed_until": time.Now()},
-		})
-	}
-	if filters.ExcludeAssigned {
-		query = query.Where(squirrel.Eq{"assigned_to": nil})
-	}
-	if filters.AssigneeId != "" {
-		query = query.Where(squirrel.Eq{"assigned_to": filters.AssigneeId})
-	}
-	return query
-}
-
-func applyCasesPagination(query squirrel.SelectBuilder, p models.PaginationAndSorting, offsetCase models.Case) (squirrel.SelectBuilder, error) {
-	if p.OffsetId == "" {
-		return query, nil
-	}
-
-	var offsetFieldVal any
-	switch p.Sorting {
-	case models.CasesSortingCreatedAt:
-		offsetFieldVal = offsetCase.CreatedAt
-	default:
-		// only pagination by created_at is allowed for now
-		return query, fmt.Errorf("invalid sorting field: %w", models.BadParameterError)
-	}
-
-	queryConditionBefore := fmt.Sprintf("(s.boost IS NULL, s.%s, s.id) < (?, ?, ?)", p.Sorting)
-	queryConditionAfter := fmt.Sprintf("(s.boost IS NULL, s.%s, s.id) > (?, ?, ?)", p.Sorting)
-	args := []any{offsetCase.Boost == nil, offsetFieldVal, p.OffsetId}
-
-	if p.Order == models.SortingOrderDesc {
-		query = query.Where(queryConditionBefore, args...)
-	} else {
-		query = query.Where(queryConditionAfter, args...)
-	}
-
-	return query, nil
-}
-
-func selectCasesWithJoinedFields(query squirrel.SelectBuilder, orderCond string) squirrel.SelectBuilder {
-	return squirrel.StatementBuilder.
-		Select(columnsNames("c", dbmodels.SelectCaseColumn)...).
-		Column(
-			fmt.Sprintf(
-				"(SELECT array_agg(row(%s) ORDER BY cc.created_at) AS contributors FROM %s AS cc WHERE cc.case_id=c.id)",
-				strings.Join(columnsNames("cc", dbmodels.SelectCaseContributorColumn), ","),
-				dbmodels.TABLE_CASE_CONTRIBUTORS,
-			),
-		).
-		Column(
-			fmt.Sprintf(
-				"(SELECT array_agg(row(%s) ORDER BY ct.created_at) AS tags FROM %s AS ct WHERE ct.case_id=c.id AND ct.deleted_at IS NULL)",
-				strings.Join(columnsNames("ct", dbmodels.SelectCaseTagColumn), ","),
-				dbmodels.TABLE_CASE_TAGS,
-			),
-		).
-		Column(fmt.Sprintf("(SELECT count(distinct d.id) FROM %s AS d WHERE d.case_id = c.id AND d.org_id=c.org_id) AS decisions_count", dbmodels.TABLE_DECISIONS)).
-		FromSelect(query, "c").
-		OrderBy(orderCond).
-		PlaceholderFormat(squirrel.Dollar)
-}
-
 func (repo *MarbleDbRepository) CreateDbCaseFile(ctx context.Context, exec Executor,
 	createCaseFileAttributes models.CreateDbCaseFileInput,
 ) error {
@@ -471,10 +443,6 @@ func (repo *MarbleDbRepository) GetCasesWithPivotValue(ctx context.Context, exec
 		Limit(100)
 
 	return SqlToListOfModels(ctx, exec, sql, dbmodels.AdaptCase)
-}
-
-func orderConditionForCases(p models.PaginationAndSorting) string {
-	return fmt.Sprintf("c.boost is null %[1]s, c.%[2]s %[1]s, c.id %[1]s", p.Order, p.Sorting)
 }
 
 func (repo *MarbleDbRepository) EscalateCase(ctx context.Context, exec Executor, id, inboxId string) error {
