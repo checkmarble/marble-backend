29a4a3c31eb1f8fdf789a6ed7d5c1c025a09896e
Pascal Delange <pascal@checkmarble.com>
2025-09-05 21:45:52 +0200

    Drop two FKs on decisions table
    fix migration

--- repositories/migrations/20250905163100_various_schema_improvements.sql
+++ repositories/migrations/20250905163100_various_schema_improvements.sql
@@ -56,6 +56,14 @@ DROP NOT NULL,
 ALTER COLUMN scenario_version
 DROP NOT NULL;
 
+-- remove FK on scenario_iteration_id on decisions
+ALTER TABLE decisions
+DROP CONSTRAINT decisions_scenario_iteration_id_fkey;
+
+-- and on pivot_id
+ALTER TABLE decisions
+DROP CONSTRAINT decisions_pivot_id_fkey;
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
@@ -102,5 +110,11 @@ ALTER COLUMN scenario_description
 SET NOT NULL,
 ALTER COLUMN scenario_version
 SET NOT NULL;
+
+ALTER TABLE decisions
+ADD CONSTRAINT decisions_scenario_iteration_id_fkey FOREIGN KEY (scenario_iteration_id) REFERENCES scenario_iterations (id);
+
+ALTER TABLE decisions
+ADD CONSTRAINT decisions_pivot_id_fkey FOREIGN KEY (pivot_id) REFERENCES data_model_pivots (id);
 
 -- +goose StatementEnd
