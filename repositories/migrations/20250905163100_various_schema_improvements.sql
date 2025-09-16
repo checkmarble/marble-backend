beccfd5b0e3d381711f7f1a5953a678953a35f17
Pascal Delange <pascal@checkmarble.com>
2025-09-16 16:04:06 +0200

    group settings in migration

--- repositories/migrations/20250905163100_various_schema_improvements.sql
+++ repositories/migrations/20250905163100_various_schema_improvements.sql
@@ -2,51 +2,45 @@
 -- +goose StatementBegin
 ALTER TABLE decisions
 SET
-    (autovacuum_vacuum_insert_scale_factor = 0.02);
+    (
+        autovacuum_vacuum_insert_scale_factor = 0.02,
+        autovacuum_analyze_scale_factor = 0.01
+    );
 
 ALTER TABLE decision_rules
 SET
-    (autovacuum_vacuum_insert_scale_factor = 0.02);
-
-ALTER TABLE decisions
-SET
-    (autovacuum_analyze_scale_factor = 0.01);
-
-ALTER TABLE decision_rules
-SET
-    (autovacuum_analyze_scale_factor = 0.01);
+    (
+        autovacuum_vacuum_insert_scale_factor = 0.02,
+        autovacuum_analyze_scale_factor = 0.01
+    );
 
 ALTER TABLE webhook_events
 SET
-    (autovacuum_vacuum_insert_scale_factor = 0.02);
-
-ALTER TABLE webhook_events
-SET
-    (autovacuum_analyze_scale_factor = 0.01);
-
-ALTER TABLE phantom_decisions
-SET
-    (autovacuum_vacuum_insert_scale_factor = 0.02);
+    (
+        autovacuum_vacuum_insert_scale_factor = 0.02,
+        autovacuum_analyze_scale_factor = 0.01
+    );
 
 ALTER TABLE phantom_decisions
 SET
-    (autovacuum_analyze_scale_factor = 0.01);
+    (
+        autovacuum_vacuum_insert_scale_factor = 0.02,
+        autovacuum_analyze_scale_factor = 0.01
+    );
 
 ALTER TABLE sanction_checks
 SET
-    (autovacuum_vacuum_insert_scale_factor = 0.02);
-
-ALTER TABLE sanction_checks
-SET
-    (autovacuum_analyze_scale_factor = 0.01);
-
-ALTER TABLE decisions_to_create
-SET
-    (autovacuum_vacuum_insert_scale_factor = 0.02);
+    (
+        autovacuum_vacuum_insert_scale_factor = 0.02,
+        autovacuum_analyze_scale_factor = 0.01
+    );
 
 ALTER TABLE decisions_to_create
 SET
-    (autovacuum_analyze_scale_factor = 0.01);
+    (
+        autovacuum_vacuum_insert_scale_factor = 0.02,
+        autovacuum_analyze_scale_factor = 0.01
+    );
 
 ALTER TABLE decisions
 ALTER COLUMN scenario_name
@@ -68,40 +62,22 @@ DROP CONSTRAINT decisions_pivot_id_fkey;
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE decisions
-RESET (autovacuum_vacuum_insert_scale_factor);
+RESET (autovacuum_vacuum_insert_scale_factor, autovacuum_analyze_scale_factor);
 
 ALTER TABLE decision_rules
-RESET (autovacuum_vacuum_insert_scale_factor);
-
-ALTER TABLE decisions
-RESET (autovacuum_analyze_scale_factor);
-
-ALTER TABLE decision_rules
-RESET (autovacuum_analyze_scale_factor);
+RESET (autovacuum_vacuum_insert_scale_factor, autovacuum_analyze_scale_factor);
 
 ALTER TABLE webhook_events
-RESET (autovacuum_vacuum_insert_scale_factor);
-
-ALTER TABLE webhook_events
-RESET (autovacuum_analyze_scale_factor);
+RESET (autovacuum_vacuum_insert_scale_factor, autovacuum_analyze_scale_factor);
 
 ALTER TABLE phantom_decisions
-RESET (autovacuum_vacuum_insert_scale_factor);
-
-ALTER TABLE phantom_decisions
-RESET (autovacuum_analyze_scale_factor);
+RESET (autovacuum_vacuum_insert_scale_factor, autovacuum_analyze_scale_factor);
 
 ALTER TABLE sanction_checks
-RESET (autovacuum_vacuum_insert_scale_factor);
-
-ALTER TABLE sanction_checks
-RESET (autovacuum_analyze_scale_factor);
+RESET (autovacuum_vacuum_insert_scale_factor, autovacuum_analyze_scale_factor);
 
 ALTER TABLE decisions_to_create
-RESET (autovacuum_vacuum_insert_scale_factor);
-
-ALTER TABLE decisions_to_create
-RESET (autovacuum_analyze_scale_factor);
+RESET (autovacuum_vacuum_insert_scale_factor, autovacuum_analyze_scale_factor);
 
 ALTER TABLE decisions
 ALTER COLUMN scenario_name
