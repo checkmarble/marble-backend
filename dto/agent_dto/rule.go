f83ab3d587ff33e79f32d1c36455fcfbc5ecd47f
Léo JI <leo.ji@outlook.fr>
2025-09-18 18:04:50 +0200

    chore: update llmberjack dependency and clean up unused code
    - Updated the version of `github.com/checkmarble/llmberjack` in `go.mod` and `go.sum`.
    - Removed the unused `AdaptRuleDto` function from `dto/agent_dto/rule.go`.
    - Added a new method `PrintForAgent` in `models/ast/ast_node.go` to provide a human-readable representation of AST nodes.
    - Simplified the `AiRuleDescription` function in `usecases/ai_agent/ai_rule_assist.go` by removing unnecessary fields and improving variable naming.

--- dto/agent_dto/rule.go
+++ dto/agent_dto/rule.go
@@ -2,27 +2,10 @@ package agent_dto
 
 import (
 	"github.com/checkmarble/marble-backend/dto"
-	"github.com/checkmarble/marble-backend/models"
 )
 
 type Rule struct {
 	Id                   string      `json:"id"`
 	ScoreModifier        int         `json:"score_modifier"`
 	FormulaAstExpression dto.NodeDto `json:"formula_ast_expression"`
-}
-
-func AdaptRuleDto(rule models.Rule) (Rule, error) {
-	formulaAstExpression := dto.NodeDto{}
-	if rule.FormulaAstExpression != nil {
-		var err error
-		formulaAstExpression, err = dto.AdaptNodeDto(*rule.FormulaAstExpression)
-		if err != nil {
-			return Rule{}, err
-		}
-	}
-	return Rule{
-		Id:                   rule.Id,
-		ScoreModifier:        rule.ScoreModifier,
-		FormulaAstExpression: formulaAstExpression,
-	}, nil
 }
