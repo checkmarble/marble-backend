9b82a50edcda95d4036849591c5866464eecacb0
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-10-03 09:14:54 +0200

    Add specialized index for decision export.

---
+++ repositories/migrations/20251003090300_create_analytics_export_index.sql
@@ -0,0 +1,8 @@
+-- +goose Up
+
+create index if not exists idx_analytics_export
+on decisions (org_id, trigger_object_type, created_at desc, id desc);
+
+-- +goose Down
+
+drop index idx_analytics_export;
