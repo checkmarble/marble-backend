334054edfe77693148a862959933e12ad4909fe0
Léo JI <leo.ji@checkmarble.com>
2025-10-23 18:50:17 +0200

    feat(screening_monitoring): add configuration part

---
+++ usecases/security/enforce_security_screening_monitoring.go
@@ -0,0 +1,33 @@
+package security
+
+import (
+	"context"
+
+	"github.com/checkmarble/marble-backend/models"
+	"github.com/cockroachdb/errors"
+)
+
+type EnforceSecurityScreeningMonitoring interface {
+	EnforceSecurity
+	ReadScreeningMonitoringConfig(ctx context.Context, config models.ScreeningMonitoringConfig) error
+	WriteScreeningMonitoringConfig(ctx context.Context, orgId string) error
+}
+
+type EnforceSecurityScreeningMonitoringImpl struct {
+	EnforceSecurity
+	Credentials models.Credentials
+}
+
+func (e *EnforceSecurityScreeningMonitoringImpl) ReadScreeningMonitoringConfig(ctx context.Context, config models.ScreeningMonitoringConfig) error {
+	return errors.Join(
+		e.Permission(models.SCREENING_MONITORING_READ),
+		e.ReadOrganization(config.OrgId),
+	)
+}
+
+func (e *EnforceSecurityScreeningMonitoringImpl) WriteScreeningMonitoringConfig(ctx context.Context, orgId string) error {
+	return errors.Join(
+		e.Permission(models.SCREENING_MONITORING_WRITE),
+		e.ReadOrganization(orgId),
+	)
+}
