02f22b48053932861173bb7414321769c8247598
Léo JI <leo.ji@checkmarble.com>
2025-11-07 23:24:15 +0100

    feat(continuous_monitoring): add new fields

--- repositories/migrations/20251105100110_continuous_screening_config.sql
+++ repositories/migrations/20251105100110_continuous_screening_config.sql
@@ -3,12 +3,15 @@
 
 create table continuous_screening_configs (
     id uuid primary key default uuid_generate_v4(),
+    stable_id uuid not null default uuid_generate_v4(),
     org_id uuid not null,
     name text not null,
     description text,
+    algorithm text not null,
     datasets text[] not null,
     match_threshold int not null check (match_threshold between 0 and 100),
     match_limit int not null,
+    object_types text[] not null default '{}',
     created_at timestamp with time zone not null default current_timestamp,
     updated_at timestamp with time zone not null default current_timestamp,
     enabled boolean not null default true,
