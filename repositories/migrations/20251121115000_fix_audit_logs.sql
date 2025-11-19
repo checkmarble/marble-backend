e28ca23bb0cf3d402ac1d8bfc6a15084aba2a408
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-11-19 17:40:40 +0100

    Fix audit logs mixing user ID and API key ID.
    A race was present in the code setting the variables for user
    and API key ID in the SQL session. We did not clear the previous
    values.
    
    The blast radius should be quite limited, since we do not expose writeable,
    public API endpoints yet, but we should check the data.

---
+++ repositories/migrations/20251121115000_fix_audit_logs.sql
@@ -0,0 +1,26 @@
+-- +goose Up
+-- +goose StatementBegin
+
+create or replace function global_audit() returns trigger as $$
+begin
+    if nullif(current_setting('custom.current_user_id', true), '') is null and nullif(current_setting('custom.current_api_key_id', true), '') is null then
+        return null;
+    end if;
+
+    if (TG_OP = 'DELETE') then
+        insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "created_at")
+        values ('DELETE', current_setting('custom.current_org_id', true)::uuid, nullif(current_setting('custom.current_user_id', true), ''), nullif(current_setting('custom.current_api_key_id', true), '')::uuid, TG_TABLE_NAME, old.id, to_jsonb(OLD), now());
+    elsif (TG_OP = 'UPDATE') then
+        insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "previous_data", "created_at")
+        values ('UPDATE', current_setting('custom.current_org_id', true)::uuid, nullif(current_setting('custom.current_user_id', true), ''), nullif(current_setting('custom.current_api_key_id', true), '')::uuid, TG_TABLE_NAME, new.id, to_jsonb(NEW), to_jsonb(OLD), now());
+    elsif (TG_OP = 'INSERT') then
+        insert into audit.audit_events ("operation", "org_id", "user_id", "api_key_id", "table", "entity_id", "data", "created_at")
+        values ('INSERT', current_setting('custom.current_org_id', true)::uuid, nullif(current_setting('custom.current_user_id', true), ''), nullif(current_setting('custom.current_api_key_id', true), '')::uuid, TG_TABLE_NAME, new.id, to_jsonb(NEW), now());
+    end if;
+    return null;
+end;
+$$ language plpgsql;
+
+-- +goose StatementEnd
+
+-- +goose Down
