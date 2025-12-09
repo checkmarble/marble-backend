9d2a128ab0d9ca9e3cfd8240ac4d057f0bc164e5
Léo JI <leo.ji@checkmarble.com>
2025-12-09 18:15:17 +0100

    feat(continuous_screening): add models and DB migration for dataset update

---
+++ repositories/migrations/20251209155000_continuous_screening_update_tables.sql
@@ -0,0 +1,60 @@
+-- +goose Up
+-- +goose StatementBegin
+
+create table continuous_screening_dataset_updates (
+    id uuid primary key default uuid_generate_v4(),
+    dataset_name text not null,
+    version text not null,
+    delta_file_path text not null,
+    created_at timestamp with time zone not null default current_timestamp,
+
+    constraint unique_dataset_updates_name_version unique (dataset_name, version)
+);
+
+create table continuous_screening_update_jobs (
+    id uuid primary key default uuid_generate_v4(),
+    cs_dataset_update_id uuid not null,
+    cs_config_id uuid not null,
+    org_id uuid not null,
+    status text not null CHECK (status IN ('pending', 'processing', 'completed', 'failed')),
+    total_lines integer not null,
+    created_at timestamp with time zone not null default current_timestamp,
+    updated_at timestamp with time zone not null default current_timestamp,
+
+    constraint fk_cs_dataset_update foreign key (cs_dataset_update_id) references continuous_screening_dataset_updates (id) on delete cascade,
+    constraint fk_cs_config foreign key (cs_config_id) references continuous_screening_configs (id) on delete cascade,
+    constraint fk_org foreign key (org_id) references organizations (id) on delete cascade
+);
+
+create table continuous_screening_job_offsets (
+    id uuid primary key default uuid_generate_v4(),
+    cs_update_job_id uuid not null,
+    "offset" bigint not null,
+    line_processed integer not null,
+    total_lines integer not null,
+    created_at timestamp with time zone not null default current_timestamp,
+    updated_at timestamp with time zone not null default current_timestamp,
+
+    constraint fk_cs_update_job foreign key (cs_update_job_id) references continuous_screening_update_jobs (id) on delete cascade
+);
+
+create table continuous_screening_job_errors (
+    id uuid primary key default uuid_generate_v4(),
+    cs_update_job_id uuid not null,
+    details jsonb,
+    created_at timestamp with time zone not null default current_timestamp,
+
+    constraint fk_cs_update_job foreign key (cs_update_job_id) references continuous_screening_update_jobs (id) on delete cascade
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+
+drop table continuous_screening_job_errors;
+drop table continuous_screening_job_offsets;
+drop table continuous_screening_update_jobs;
+drop table continuous_screening_dataset_updates;
+
+-- +goose StatementEnd
