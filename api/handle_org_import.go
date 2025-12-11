7be97bd2e291050fe2c6a263b2bd5468d3b064de
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-12-11 19:54:09 +0100

    Add ingested objects generation.

--- api/handle_org_import.go
+++ api/handle_org_import.go
@@ -21,7 +21,7 @@ func handleOrgImport(uc usecases.Usecases) gin.HandlerFunc {
 		uc := usecasesWithCreds(ctx, uc)
 		orgImportUsecse := uc.NewOrgImportUsecase()
 
-		orgId, err := orgImportUsecse.Import(ctx, spec)
+		orgId, err := orgImportUsecse.Import(ctx, spec, c.Query("seed") == "true")
 		if presentError(ctx, c, err) {
 			return
 		}
@@ -29,5 +29,24 @@ func handleOrgImport(uc usecases.Usecases) gin.HandlerFunc {
 		c.JSON(http.StatusOK, gin.H{
 			"org_id": orgId,
 		})
+	}
+}
+
+func handleOrgSeed(uc usecases.Usecases) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		var spec dto.OrgImport
+
+		if err := c.ShouldBindJSON(&spec); presentError(ctx, c, err) {
+			return
+		}
+
+		uc := usecasesWithCreds(ctx, uc)
+		orgImportUsecse := uc.NewOrgImportUsecase()
+
+		if err := orgImportUsecse.Seed(ctx, spec, "019b087e-a3d6-7f1f-bbd7-5903c2e373da"); presentError(ctx, c, err) {
+			return
+		}
 	}
 }
