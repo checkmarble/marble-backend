bc562b06a63e76830d2355e897a20fcd8c69b559
Léo JI <leo.ji@checkmarble.com>
2025-12-16 10:43:40 +0100

    feat(continuous_screening): add stub apply delta file worker

--- usecases/continuous_screening/worker_apply_delta_file.go
+++ usecases/continuous_screening/worker_apply_delta_file.go
@@ -6,22 +6,41 @@ import (
 	"time"
 
 	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/repositories"
 	"github.com/checkmarble/marble-backend/usecases/executor_factory"
 	"github.com/checkmarble/marble-backend/utils"
+	"github.com/google/uuid"
 	"github.com/riverqueue/river"
 )
+
+type applyDeltaFileWorkerRepository interface {
+	GetEnrichedContinuousScreeningUpdateJob(
+		ctx context.Context,
+		exec repositories.Executor,
+		updateId uuid.UUID,
+	) (models.EnrichedContinuousScreeningUpdateJob, error)
+	UpdateContinuousScreeningUpdateJob(
+		ctx context.Context,
+		exec repositories.Executor,
+		updateId uuid.UUID,
+		status models.ContinuousScreeningUpdateJobStatus,
+	) error
+}
 
 type ApplyDeltaFileWorker struct {
 	river.WorkerDefaults[models.ContinuousScreeningApplyDeltaFileArgs]
 
 	executorFactory executor_factory.ExecutorFactory
+	repository      applyDeltaFileWorkerRepository
 }
 
 func NewApplyDeltaFileWorker(
 	executorFactory executor_factory.ExecutorFactory,
+	repository applyDeltaFileWorkerRepository,
 ) *ApplyDeltaFileWorker {
 	return &ApplyDeltaFileWorker{
 		executorFactory: executorFactory,
+		repository:      repository,
 	}
 }
 
@@ -38,5 +57,32 @@ func (w *ApplyDeltaFileWorker) Work(ctx context.Context, job *river.Job[models.C
 		"update_id", job.Args.UpdateId,
 		"org_id", job.Args.OrgId,
 	)
+
+	updateJob, err := w.repository.GetEnrichedContinuousScreeningUpdateJob(ctx,
+		w.executorFactory.NewExecutor(), job.Args.UpdateId)
+	if err != nil {
+		return err
+	}
+
+	logger.DebugContext(ctx, "Enriched continuous screening update job", "update_job", updateJob)
+
+	if updateJob.Status == models.ContinuousScreeningUpdateJobStatusCompleted {
+		logger.DebugContext(ctx, "Continuous screening update job already completed, skip processing")
+		return nil
+	}
+
+	// TODO: Implement the delta file update logic
+
+	err = w.repository.UpdateContinuousScreeningUpdateJob(
+		ctx,
+		w.executorFactory.NewExecutor(),
+		updateJob.Id,
+		models.ContinuousScreeningUpdateJobStatusCompleted,
+	)
+	if err != nil {
+		return err
+	}
+
+	logger.DebugContext(ctx, "Successfully updated continuous screening update job", "update_job", updateJob)
 	return nil
 }
