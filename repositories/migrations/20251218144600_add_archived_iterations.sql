ad38d8ba6a963ab52a422dbf52db9f1c7324e409
Antoine Popineau <antoine.popineau@checkmarble.com>
2025-12-22 13:41:20 +0100

    Delete data model links.

--- repositories/migrations/20251218144600_add_archived_iterations.sql
+++ repositories/migrations/20251218144600_add_archived_iterations.sql
@@ -3,14 +3,13 @@
 alter table scenario_iterations
     add column archived boolean default false;
 
-alter table data_model_tables
-    add column archived boolean default false;
-
 alter table data_model_fields
     add column archived boolean default false;
 
 alter table data_model_fields
-    drop constraint data_model_fields_table_id_name_key;
+    drop constraint if exists data_model_fields_table_id_name_key;
+
+drop index if exists data_model_fields_table_id_name_key;
 
 alter table data_model_fields
     drop constraint unique_data_model_fields_name;
@@ -20,19 +19,14 @@ create unique index data_model_fields_table_id_name
 
 -- +goose Down
 
-alter table scenario_iterations
-    drop column archived;
+delete from data_model_fields
+where archived is true;
 
-alter table data_model_tables
+alter table scenario_iterations
     drop column archived;
 
 alter table data_model_fields
     drop column archived;
-
-drop index data_model_fields_table_id_name;
-
-create unique index data_model_fields_table_id_name_key
-	on data_model_fields (table_id, name);
 
 alter table data_model_fields
     add constraint unique_data_model_fields_name
