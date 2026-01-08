5df7416bdb6cf90e8944a684e2e84708f264175c
Pascal Delange <pascal@checkmarble.com>
2026-01-08 18:30:12 +0100

    fix

--- repositories/analytics_export_repository_test.go
+++ repositories/analytics_export_repository_test.go
@@ -1,28 +1,37 @@
 package repositories
 
-// func TestAnalyticsCopyScreenings(t *testing.T) {
-// 	req := AnalyticsCopyRequest{
-// 		OrgId:         "123",
-// 		TriggerObject: "test",
-// 		EndTime:       time.Now(),
-// 		Limit:         10000,
-// 		Watermark: &models.Watermark{
-// 			WatermarkTime: time.Now(),
-// 			WatermarkId:   utils.Ptr(uuid.New().String()),
-// 		},
-// 	}
+import (
+	"testing"
+	"time"
 
-// 	inner, err := generateScreeningsExportQuery(req, nil, nil)
-// 	if err != nil {
-// 		t.Fatalf("failed to generate query: %v", err)
-// 	}
+	"github.com/checkmarble/marble-backend/models"
+	"github.com/checkmarble/marble-backend/utils"
+	"github.com/google/uuid"
+)
 
-// 	innerSql, args, err := inner.ToSql()
-// 	if err != nil {
-// 		t.Fatalf("failed to build query: %v", err)
-// 	}
+func TestAnalyticsCopyScreenings(t *testing.T) {
+	req := AnalyticsCopyRequest{
+		OrgId:         utils.TextToUUID("123"),
+		TriggerObject: "test",
+		EndTime:       time.Now(),
+		Limit:         10000,
+		Watermark: &models.Watermark{
+			WatermarkTime: time.Now(),
+			WatermarkId:   utils.Ptr(uuid.New().String()),
+		},
+	}
 
-// 	t.Error("just need the log really")
-// 	t.Logf("query: %s", innerSql)
-// 	t.Logf("args: %v", args)
-// }
+	inner, err := generateScreeningsExportQuery(req, nil)
+	if err != nil {
+		t.Fatalf("failed to generate query: %v", err)
+	}
+
+	innerSql, args, err := inner.ToSql()
+	if err != nil {
+		t.Fatalf("failed to build query: %v", err)
+	}
+
+	t.Error("just need the log really")
+	t.Logf("query: %s", innerSql)
+	t.Logf("args: %v", args)
+}
