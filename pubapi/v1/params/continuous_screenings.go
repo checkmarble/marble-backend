9a67cc50686ba9c13049d3754a9cfcaa91185828
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-02-05 13:33:27 +0100

    Remove possibility to ingest in the continuous screening endpoint.

--- pubapi/v1/params/continuous_screenings.go
+++ pubapi/v1/params/continuous_screenings.go
@@ -1,18 +1,15 @@
 package params
 
 import (
-	"encoding/json"
-
 	"github.com/checkmarble/marble-backend/models"
 	"github.com/google/uuid"
 )
 
 type CreateContinuousScreeningObjectParams struct {
-	ObjectType     string           `json:"object_type" binding:"required"`
-	ConfigStableId uuid.UUID        `json:"config_stable_id" binding:"required"`
-	ObjectId       *string          `json:"object_id" binding:"required_without_all=ObjectPayload,excluded_with=ObjectPayload"`
-	ObjectPayload  *json.RawMessage `json:"object_payload" binding:"required_without_all=ObjectId,excluded_with=ObjectId"`
-	SkipScreen     bool             `json:"skip_screen"`
+	ObjectType     string    `json:"object_type" binding:"required"`
+	ConfigStableId uuid.UUID `json:"config_stable_id" binding:"required"`
+	ObjectId       string    `json:"object_id" binding:"required"`
+	SkipScreen     bool      `json:"skip_screen"`
 }
 
 func (dto CreateContinuousScreeningObjectParams) ToModel() models.CreateContinuousScreeningObject {
@@ -20,7 +17,6 @@ func (dto CreateContinuousScreeningObjectParams) ToModel() models.CreateContinuo
 		ObjectType:     dto.ObjectType,
 		ConfigStableId: dto.ConfigStableId,
 		ObjectId:       dto.ObjectId,
-		ObjectPayload:  dto.ObjectPayload,
 		SkipScreen:     dto.SkipScreen,
 	}
 }
