a78426fca188685170b2df6aa349ce4b10d77cab
Léo JI <64692412+OrriMandarin@users.noreply.github.com>
2026-02-27 09:41:06 +0100

    fix: Wait longer for postgres in test (#1566)

--- pubapi/tests/setup_test.go
+++ pubapi/tests/setup_test.go
@@ -73,7 +73,7 @@ func setupPostgres(t *testing.T, ctx context.Context) *postgres.PostgresContaine
 		testcontainers.WithWaitStrategy(
 			wait.ForLog("database system is ready to accept connections").
 				WithOccurrence(2).
-				WithStartupTimeout(5*time.Second)),
+				WithStartupTimeout(10*time.Second)),
 	)
 
 	t.Cleanup(func() {
