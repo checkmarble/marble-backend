102c978bd0e030477603cc004a824dc45dcdd4c5
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-03-02 16:03:37 +0100

    Store score evaluations on blob storage.

--- repositories/scoring_ruleset_repository.go
+++ repositories/scoring_ruleset_repository.go
@@ -81,6 +81,24 @@ func (repo *MarbleDbRepository) GetScoringRuleset(
 	return *ruleset, nil
 }
 
+func (repo *MarbleDbRepository) GetScoringRulesetById(
+	ctx context.Context,
+	exec Executor,
+	orgId, id uuid.UUID,
+) (models.ScoringRuleset, error) {
+	if err := validateMarbleDbExecutor(exec); err != nil {
+		return models.ScoringRuleset{}, err
+	}
+
+	query := NewQueryBuilder().
+		Select(dbmodels.SelectScoringRulesetsColumns...).
+		From(dbmodels.TABLE_SCORING_RULESETS).
+		Where("org_id = ?", orgId).
+		Where("id = ?", id)
+
+	return SqlToModel(ctx, exec, query, dbmodels.AdaptScoringRuleset)
+}
+
 func (repo *MarbleDbRepository) InsertScoringRulesetVersion(
 	ctx context.Context,
 	tx Transaction,
