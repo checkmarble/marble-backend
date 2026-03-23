949c513c360a39ee82058410ecc769f0ce7028a2
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-03-23 15:54:57 +0100

    Prevent risk level scale from being changed after ruleset are created.

--- usecases/scoring/scoring_settings_usecase.go
+++ usecases/scoring/scoring_settings_usecase.go
@@ -55,5 +55,24 @@ func (uc ScoringSettingsUsecase) UpdateSettings(ctx context.Context, settings mo
 		return models.ScoringSettings{}, errors.Wrapf(models.BadParameterError, "maximum risk level outside of allowed bounds (1-%d)", MAX_RISK_LEVEL_SCALE)
 	}
 
-	return uc.repository.UpdateScoringSettings(ctx, uc.executorFactory.NewExecutor(), settings)
+	exec := uc.executorFactory.NewExecutor()
+
+	existingSettings, err := uc.repository.GetScoringSettings(ctx, exec, settings.OrgId)
+	if err != nil {
+		return models.ScoringSettings{}, err
+	}
+
+	// Some settings cannot be changed after a ruleset is created.
+	if existingSettings != nil {
+		rulesets, err := uc.repository.ListScoringRulesets(ctx, exec, settings.OrgId)
+		if err != nil {
+			return models.ScoringSettings{}, err
+		}
+
+		if len(rulesets) > 0 && existingSettings.MaxRiskLevel != settings.MaxRiskLevel {
+			return models.ScoringSettings{}, errors.Wrap(models.BadParameterError, "cannot change maximum risk level after having created rulesets")
+		}
+	}
+
+	return uc.repository.UpdateScoringSettings(ctx, exec, settings)
 }
