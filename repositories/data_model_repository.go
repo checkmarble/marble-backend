264c0712cff442ba68df8b0782225cecb5a9d90f
Léo JI <leo.ji@checkmarble.com>
2026-04-09 17:43:36 +0200

    fix: link type is not returned

--- repositories/data_model_repository.go
+++ repositories/data_model_repository.go
@@ -124,12 +124,6 @@ func (repo MarbleDbRepository) GetDataModel(
 		return models.DataModel{}, err
 	}
 
-	pivots, err := repo.ListPivots(ctx, exec, organizationID, nil, useCache)
-	if err != nil {
-		return models.DataModel{}, err
-	}
-	models.EnrichLinksWithPivotTypes(links, pivots)
-
 	dataModel := models.DataModel{
 		Tables: make(map[string]models.Table),
 	}
@@ -527,7 +521,12 @@ func (repo MarbleDbRepository) GetLinks(ctx context.Context, exec Executor, orga
 		child_table.name,
 		child_table.id,
 		child_field.name,
-		child_field.id
+		child_field.id,
+		EXISTS (
+			SELECT 1 FROM data_model_pivots p
+			WHERE p.organization_id = links.organization_id
+			AND links.id = ANY(p.path_link_ids)
+		) AS is_belongs_to
 	FROM data_model_links AS links
     	JOIN data_model_tables AS parent_table ON (links.parent_table_id = parent_table.id)
     	JOIN data_model_fields AS parent_field ON (links.parent_field_id = parent_field.id)
@@ -540,7 +539,7 @@ func (repo MarbleDbRepository) GetLinks(ctx context.Context, exec Executor, orga
 		return nil, err
 	}
 
-	links, err := pgx.CollectRows(rows, func(row pgx.CollectableRow) (models.LinkToSingle, error) {
+	return pgx.CollectRows(rows, func(row pgx.CollectableRow) (models.LinkToSingle, error) {
 		var dbLinks dbmodels.DbDataModelLink
 		if err := rows.Scan(
 			&dbLinks.Id,
@@ -554,12 +553,12 @@ func (repo MarbleDbRepository) GetLinks(ctx context.Context, exec Executor, orga
 			&dbLinks.ChildTableId,
 			&dbLinks.ChildFieldName,
 			&dbLinks.ChildFieldId,
+			&dbLinks.IsBelongsTo,
 		); err != nil {
 			return models.LinkToSingle{}, err
 		}
 		return dbmodels.AdaptLinkToSingle(dbLinks), err
 	})
-	return links, nil
 }
 
 func (repo MarbleDbRepository) DeleteDataModel(ctx context.Context, exec Executor, organizationID uuid.UUID) error {
