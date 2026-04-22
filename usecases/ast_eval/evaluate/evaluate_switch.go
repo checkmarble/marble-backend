07babce16a8b39e7d1a9027bb39c9c4faf43bbe7
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-04-22 08:36:43 +0200

    Relax mandatory "field" argument on Switch.

--- usecases/ast_eval/evaluate/evaluate_switch.go
+++ usecases/ast_eval/evaluate/evaluate_switch.go
@@ -15,33 +15,27 @@ func (p Switch) Evaluate(ctx context.Context, arguments ast.Arguments) (any, []e
 	if len(arguments.Args) == 0 {
 		errs = append(errs, errors.Wrap(ast.ErrWrongNumberOfArgument, "Switch should have at least one branch"))
 	}
-	field, ok := arguments.NamedArgs["field"]
-	if !ok {
-		errs = append(errs, errors.Wrap(ast.ErrMissingNamedArgument, "Switch should have a `field` named argument"))
+
+	if len(errs) > 0 {
+		return nil, errs
+	}
+
+	nodes, err := adaptArgumentToListOfThings[ast.ScoreComputationResult](arguments.Args)
+	if err != nil {
+		errs = append(errs, err)
 	}
 
 	if len(errs) > 0 {
 		return nil, errs
 	}
 
-	if field != nil {
-		nodes, err := adaptArgumentToListOfThings[ast.ScoreComputationResult](arguments.Args)
-		if err != nil {
-			errs = append(errs, err)
-		}
+	for idx, node := range nodes {
+		if node.Triggered {
+			node.Fallback = false
+			node.Default = false
+			node.Branch = new(idx)
 
-		if len(errs) > 0 {
-			return nil, errs
-		}
-
-		for idx, node := range nodes {
-			if node.Triggered {
-				node.Fallback = false
-				node.Default = false
-				node.Branch = new(idx)
-
-				return node, nil
-			}
+			return node, nil
 		}
 	}
 
