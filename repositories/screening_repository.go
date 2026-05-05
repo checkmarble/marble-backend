61d3d9a0a367a867a07d001dac969506f8d8fccf
Léo JI <leo.ji@checkmarble.com>
2026-05-05 17:30:52 +0200

    chore: nothing

--- repositories/screening_repository.go
+++ repositories/screening_repository.go
@@ -441,9 +441,7 @@ func (repo *MarbleDbRepository) CountScreeningsByProvider(ctx context.Context, e
 		Where(squirrel.GtOrEq{"created_at": from}).
 		Where(squirrel.Lt{"created_at": to}).
 		Where(squirrel.NotEq{"status": models.ScreeningStatusError.String()}).
-		// TODO: uncomment or replace by the right field to filter by provider
-		// GroupBy("org_id", "provider")
-		GroupBy("org_id")
+		GroupBy("org_id", "provider")
 
 	stringProviders := pure_utils.Map(providers, func(p models.ScreeningProvider) string { return string(p) })
 
