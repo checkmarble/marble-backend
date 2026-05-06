77894b50ea445fef64a60644b5e5beb96d4ae2fc
Léo JI <leo.ji@checkmarble.com>
2026-05-06 17:00:38 +0200

    replace existing metric by the new one

--- usecases/metrics_collection/collector_test.go
+++ usecases/metrics_collection/collector_test.go
@@ -65,22 +65,8 @@ func (m *MockCollectorRepository) GetMetadata(ctx context.Context, exec reposito
 	return args.Get(0).(*models.Metadata), args.Error(1)
 }
 
-func (m *MockCollectorRepository) GetEnabledConfigStableIdsByOrg(ctx context.Context, exec repositories.Executor,
-	orgIds []string,
-) (map[string][]uuid.UUID, error) {
-	args := m.Called(ctx, exec, orgIds)
-	return args.Get(0).(map[string][]uuid.UUID), args.Error(1)
-}
-
 type MockCollectorClientRepository struct {
 	mock.Mock
-}
-
-func (m *MockCollectorClientRepository) CountMonitoredObjectsByConfigStableIds(ctx context.Context, exec repositories.Executor,
-	configStableIds []uuid.UUID,
-) (int, error) {
-	args := m.Called(ctx, exec, configStableIds)
-	return args.Get(0).(int), args.Error(1)
 }
 
 func (m *MockCollectorClientRepository) IsContinuousScreeningSetup(ctx context.Context, exec repositories.Executor) (bool, error) {
@@ -413,7 +399,7 @@ func TestNewCollectorsV1(t *testing.T) {
 	// Assert
 	assert.Equal(t, "v1", collectors.version)
 	assert.Len(t, collectors.globalCollectors, 1)
-	assert.Len(t, collectors.collectors, 6)
+	assert.Len(t, collectors.collectors, 5)
 	assert.Equal(t, mockRepository, collectors.repository)
 	assert.Equal(t, mockExecutorFactory, collectors.executorFactory)
 
@@ -433,6 +419,6 @@ func TestNewCollectorsV1(t *testing.T) {
 	_, isAiCaseReviewCollector := collectors.collectors[3].(AiCaseReviewCollector)
 	assert.True(t, isAiCaseReviewCollector, "Should contain AiCaseReviewCollector")
 
-	_, isContinuousScreeningCollector := collectors.collectors[4].(ContinuousScreeningCollector)
-	assert.True(t, isContinuousScreeningCollector, "Should contain ContinuousScreeningCollector")
+	_, isMonitoredObjectActiveCollector := collectors.collectors[4].(MonitoredObjectActiveCollector)
+	assert.True(t, isMonitoredObjectActiveCollector, "Should contain MonitoredObjectActiveCollector")
 }
