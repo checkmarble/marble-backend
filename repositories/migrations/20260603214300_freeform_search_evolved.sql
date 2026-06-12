ffc066f015d701df45715a7096a5283c0b8c9f2b
Pascal Delange <pascal@checkmarble.com>
2026-06-12 11:41:44 +0200

    delete all past screening_freeform_searches entries

--- repositories/migrations/20260603214300_freeform_search_evolved.sql
+++ repositories/migrations/20260603214300_freeform_search_evolved.sql
@@ -5,6 +5,9 @@ add column search_config jsonb not null default '{}'::jsonb,
 add column is_saved boolean not null default false,
 add column result_hash bytea;
 
+-- the format of the unstructured json we save for the search config has changed, and we'll need those for display, so we might as well start from a clean table
+delete from screening_freeform_searches where true;
+
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
