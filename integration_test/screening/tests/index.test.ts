ae5804136be3adb6baef5687361286d3424eb4c8
Antoine Popineau <antoine.popineau@checkmarble.com>
2026-06-29 08:17:42 +0200

    Add simple E2E test for outgoing continuous monitoring.

--- integration_test/screening/tests/index.test.ts
+++ integration_test/screening/tests/index.test.ts
@@ -1,5 +1,11 @@
 import { StartedPostgreSqlContainer } from "@testcontainers/postgresql";
-import { startFirebase, startApi, startDatabase } from "./marble/base";
+import {
+	startFirebase,
+	startApi,
+	startDatabase,
+	startWorker,
+	startS3,
+} from "./marble/base";
 import { Network, StartedNetwork, StartedTestContainer } from "testcontainers";
 import { performInitialSetup, type SetupOutput } from "./marble/setup";
 import postgres, { Sql } from "postgres";
@@ -7,15 +13,19 @@ import { startElasticsearch, startMotiva } from "./marble/screening";
 import { testTransactionMonitoring } from "./transaction";
 import { buildMarble } from "./marble/build";
 import { uri } from "./marble/utils";
+import { testOutgoingContinuousMonitoring } from "./continuous-outgoing";
 
 var network: StartedNetwork;
 
 var fb: StartedTestContainer;
 var pg: StartedPostgreSqlContainer;
 var es: StartedTestContainer;
+var s3: StartedTestContainer;
 var motiva: StartedTestContainer;
 var api: StartedTestContainer;
-var dbConn: Sql;
+var worker: StartedTestContainer;
+
+var sql: Sql;
 
 var vars: SetupOutput;
 
@@ -23,26 +33,31 @@ beforeAll(
 	async () => {
 		network = await new Network().start();
 
-		[, pg, fb, es] = await Promise.all([
+		[, pg, fb, es, s3] = await Promise.all([
 			buildMarble(),
 			startDatabase(network),
 			startFirebase(network),
 			startElasticsearch(network),
+			startS3(network),
 		]);
 
-		dbConn = postgres(pg.getConnectionUri());
+		sql = postgres(pg.getConnectionUri());
 		motiva = await startMotiva(network);
-		api = await startApi(network, process.env.LICENSE_KEY ?? "");
 
-		vars = await performInitialSetup(dbConn, uri(network, api, 8080));
+		api = await startApi(network, process.env.LICENSE_KEY ?? "");
+		worker = await startWorker(network, process.env.LICENSE_KEY ?? "");
+
+		vars = await performInitialSetup(sql, uri(network, api, 8080));
 	},
 	15 * 60 * 1000,
 );
 
 afterAll(async () => {
-	await dbConn?.end();
+	await sql?.end();
+	await worker?.stop();
 	await api?.stop();
 	await motiva?.stop();
+	await s3?.stop();
 	await es?.stop();
 	await pg?.stop();
 	await fb?.stop();
@@ -58,4 +73,28 @@ describe("Initial setup", () => {
 	it("perform transaction monitoring screening check", async () => {
 		await testTransactionMonitoring(uri(network, api, 8080), vars.scenarioId);
 	});
+
+	it(
+		"perform outgoing continuous screening on ingestion",
+		async () => {
+			await testOutgoingContinuousMonitoring(
+				sql,
+				uri(network, api, 8080),
+				vars.continuousScreeningConfigId,
+			);
+		},
+		30 * 1000,
+	);
+
+	it(
+		"perform incoming continuous screening on dataset update",
+		async () => {
+			await testOutgoingContinuousMonitoring(
+				sql,
+				uri(network, api, 8080),
+				vars.continuousScreeningConfigId,
+			);
+		},
+		30 * 1000,
+	);
 });
