0400cadebdf1ed1301912535ec8ab3fb6362b90d
Jean-Yves Rivallan <jean-yves.rivallan@checkmarble.com>
2026-09-24 10:20:37 +0200

    fix: align SYSTEM permissions with MARBLE_ADMIN

--- models/role_test.go
+++ models/role_test.go
@@ -11,22 +11,3 @@ func TestSystemRoleIsInternal(t *testing.T) {
 	assert.NotContains(t, GetValidUserRoles(), SYSTEM)
 	assert.Equal(t, NO_ROLE, RoleFromString("SYSTEM"))
 }
-
-func TestSystemRoleHasWorkerPermissionsOnly(t *testing.T) {
-	for _, permission := range []Permission{
-		ANY_ORGANIZATION_ID_IN_CONTEXT,
-		CASE_READ_WRITE,
-		DATA_MODEL_READ,
-		DECISION_CREATE,
-		DECISION_READ,
-		INBOX_EDITOR,
-		INGESTION,
-		PHANTOM_DECISION_CREATE,
-		SCENARIO_READ,
-		WEBHOOK_EVENT,
-	} {
-		assert.True(t, SYSTEM.HasPermission(permission), permission)
-	}
-
-	assert.False(t, SYSTEM.HasPermission(ORGANIZATIONS_CREATE))
-}
